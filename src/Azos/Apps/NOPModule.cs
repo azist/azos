2adb3e148d2778e9e1fd3f1a204d98b4ee7e482e
Dmitriy Khmaladze <d@zhabis.com>
2018-10-04 20:46:00 -0400

    upd header

--- src/Azos/Apps/NOPModule.cs
+++ src/Azos/Apps/NOPModule.cs
@@ -1,4 +1,9 @@
-﻿namespace Azos.Apps
+/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+namespace Azos.Apps
 {
   /// <summary>
   /// Defines a module that does nothing
---
+++ src/FILE_LICENSE.txt
@@ -0,0 +1,5 @@
+/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
