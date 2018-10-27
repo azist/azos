7487dd0879865e3edc9c3363577a323776fb7ed5
Dmitriy Khmaladze <d@zhabis.com>
2018-10-26 20:17:56 -0400

    Fix TestBusinessLogic

---
+++ src/testing/TestBusinessLogic/_build/pre-build.cmd
@@ -0,0 +1,18 @@
+echo +++++ [TestBusinessLogic] Pre Build
+
+set SOLUTION_DIR=%1
+set PROJECT_DIR=%2
+set CONFIG=%3
+set TOOL_DIR=%SOLUTION_DIR%../out/%CONFIG%/run-core/
+
+echo +++ Solution  -  %SOLUTION_DIR%
+echo +++ Project   -  %PROJECT_DIR%
+echo +++ Config    -  %CONFIG%
+echo +++ Tool      -  %TOOL_DIR%
+
+rem ---------------------------------------------
+
+dotnet "%TOOL_DIR%ntc" "%PROJECT_DIR%WaveTestSite\Pages\*.htm" -sub -r -ext ".auto.cs" -src -c "Azos.Templatization.NHTCompiler, Azos"
+
+rem ---------------------------------------------
+echo +++++ [TestBusinessLogic] Pre Build
--- src/testing/TestBusinessLogic/pre-build.cmd
+++
@@ -1,6 +0,0 @@
-set SOLUTION_DIR=%1
-set CONFIG=%2
-set PROJECT_DIR=%~dp0
-set TOOL_DIR=%SOLUTION_DIR%\..\out\%CONFIG%\run-netf\
-
-"%TOOL_DIR%ntc" "%PROJECT_DIR%WaveTestSite\Pages\*.htm" -sub -r -ext ".auto.cs" -src -c "Azos.Templatization.NHTCompiler, Azos"
