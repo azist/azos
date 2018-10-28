104f29851cfc24fea7e0705603fa6449359cf106
Dmitriy Khmaladze <d@zhabis.com>
2018-10-28 19:23:25 -0400

    Add lic

--- src/providers/Azos.Erlang/Properties/AssemblyInfo.cs
+++ src/providers/Azos.Erlang/Properties/AssemblyInfo.cs
@@ -1,4 +1,9 @@
-﻿using System.Runtime.CompilerServices;
+/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Azos.Tests.Unit")]
 [assembly: InternalsVisibleTo("Azos.Tests.Integration")]
