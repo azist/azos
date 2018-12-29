37812879764ec8a46fb3d440d53047351d1f5e78
Dmitriy Khmaladze <d@zhabis.com>
2018-12-29 14:51:50 -0500

    Fix build

--- src/testing/TestBusinessLogic/_build/pre-build.cmd
+++ src/testing/TestBusinessLogic/_build/pre-build.cmd
@@ -12,7 +12,7 @@ echo +++ Tool      -  %TOOL_DIR%
 
 rem ---------------------------------------------
 
-dotnet "%TOOL_DIR%ntc" "%PROJECT_DIR%WaveTestSite\Pages\*.htm" -sub -r -ext ".auto.cs" -src -c "Azos.Templatization.NHTCompiler, Azos"
+dotnet "%TOOL_DIR%ntc.dll" "%PROJECT_DIR%WaveTestSite\Pages\*.htm" -sub -r -ext ".auto.cs" -src -c "Azos.Templatization.NHTCompiler, Azos"
 
 rem ---------------------------------------------
 echo +++++ [TestBusinessLogic] Pre Build
