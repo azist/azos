5276ee690c190058f819ced13be2dc2218e14ead
Dmitriy Khmaladze <d@zhabis.com>
2019-01-11 21:41:22 -0500

    Add nugets Web and Wave

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -7,8 +7,12 @@ IF %LAST% NEQ \ (SET PROJECT_HOME=%PROJECT_HOME%\)
 set AZOS_HOME=%PROJECT_HOME%AZOS\
 set OUT=%AZOS_HOME%out\nuget
 
-set VER=1.0.201
+set VER=1.0.203
 
 nuget pack Azos.nuspec -Version %VER% -OutputDirectory "%OUT%"
+nuget pack Azos.Web.nuspec -Version %VER% -OutputDirectory "%OUT%"
+nuget pack Azos.Wave.nuspec -Version %VER% -OutputDirectory "%OUT%"
 
-nuget push "%OUT%\Azos.%VER%.nupkg" %AZIST_NUGET_API_KEY% 
+rem nuget push "%OUT%\Azos.%VER%.nupkg" %AZIST_NUGET_API_KEY% 
+rem nuget push "%OUT%\Azos.Web.%VER%.nupkg" %AZIST_NUGET_API_KEY% 
+rem nuget push "%OUT%\Azos.Wave.%VER%.nupkg" %AZIST_NUGET_API_KEY% 
