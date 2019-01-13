dbba42c0bdc8ac2e48e3b44e919d3fb474ece088
Dmitriy Khmaladze <d@zhabis.com>
2019-01-12 19:29:10 -0500

    Add failing JSOn tests

---
+++ src/testing/Azos.Tests.Nub/Configuration/RootTests.cs
@@ -0,0 +1,90 @@
+/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+
+using System;
+
+using Azos.Conf;
+using Azos.Data;
+using Azos.Scripting;
+
+namespace Azos.Tests.Nub.Configuration
+{
+  [Runnable]
+  public class SameAttrSectionNamesTests
+  {
+    [Run]
+    public void ReadLaconic()
+    {
+      var src = @"
+app=3
+{
+  a=1
+  a{v=123}
+}
+";
+      var cfg = src.AsLaconicConfig(handling: ConvertErrorHandling.Throw);
+
+      Aver.AreEqual(3, cfg.ValueAsInt());
+      Aver.AreEqual(1, cfg.AttrByName("a").ValueAsInt());
+      Aver.AreEqual(123, cfg["a"].AttrByName("v").ValueAsInt());
+    }
+
+    [Run]
+    public void ReadLaconicWriteJson()
+    {
+      var src = @"
+app=3
+{
+  a=1
+  a{v=123}
+}
+";
+      var cfg = src.AsLaconicConfig(handling: ConvertErrorHandling.Throw);
+
+      Aver.AreEqual(3, cfg.ValueAsInt());
+      Aver.AreEqual(1, cfg.AttrByName("a").ValueAsInt());
+      Aver.AreEqual(123, cfg["a"].AttrByName("v").ValueAsInt());
+
+      var json = cfg.ToJSONDataMap();
+      Console.WriteLine(json);
+
+      cfg = json.AsJSONConfig(handling: ConvertErrorHandling.Throw);
+      Aver.AreEqual(3, cfg.ValueAsInt());
+      Aver.AreEqual(1, cfg.AttrByName("a").ValueAsInt());
+      Aver.AreEqual(123, cfg["a"].AttrByName("v").ValueAsInt());
+    }
+
+    [Run]
+    public void ReadXML()
+    {
+      var src = @"
+<app a=""1"">3
+  <a v=""123""/>
+</app>
+";
+      var cfg = src.AsXMLConfig(handling: ConvertErrorHandling.Throw);
+
+      Aver.AreEqual(3, cfg.ValueAsInt());
+      Aver.AreEqual(1, cfg.AttrByName("a").ValueAsInt());
+      Aver.AreEqual(123, cfg["a"].AttrByName("v").ValueAsInt());
+    }
+
+    [Run]
+    public void ReadJSON()
+    {
+      var src = @"
+{ ""app"": {""-section-value"": 3, ""a"": [1, {""v"": 123}]}}";
+      var cfg = src.AsJSONConfig(handling: ConvertErrorHandling.Throw);
+
+      Aver.AreEqual(3, cfg.ValueAsInt());
+      Aver.AreEqual(1, cfg.AttrByName("a").ValueAsInt());
+      Aver.AreEqual(123, cfg["a"].AttrByName("v").ValueAsInt());
+    }
+
+
+  }
+}
