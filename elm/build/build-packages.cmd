9febc78d0c763ee9dedbfc44ca90464374d389e6
Dmitriy Khmaladze <d@zhabis.com>
2019-01-13 19:42:21 -0500

    Build packages

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -7,7 +7,7 @@ IF %LAST% NEQ \ (SET PROJECT_HOME=%PROJECT_HOME%\)
 set AZOS_HOME=%PROJECT_HOME%AZOS\
 set OUT=%AZOS_HOME%out\nuget
 
-set VER=1.0.205
+set VER=1.0.207
 
 nuget pack Azos.nuspec -Version %VER% -OutputDirectory "%OUT%"
 nuget pack Azos.Web.nuspec -Version %VER% -OutputDirectory "%OUT%"
