0d559dbfb5e729eab01b594530b6cd9c13a1e255
Dmitriy Khmaladze <d@zhabis.com>
2019-01-17 19:57:26 -0500

    Mend package icon

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -7,18 +7,20 @@ IF %LAST% NEQ \ (SET PROJECT_HOME=%PROJECT_HOME%\)
 set AZOS_HOME=%PROJECT_HOME%AZOS\
 set OUT=%AZOS_HOME%out\nuget
 
-set VER=1.0.207
+set ICON=https://raw.githubusercontent.com/azist/azos/master/elm/design/logo/azos-logo-320x320.png
 
-nuget pack Azos.nuspec -Version %VER% -OutputDirectory "%OUT%"
-nuget pack Azos.Web.nuspec -Version %VER% -OutputDirectory "%OUT%"
-nuget pack Azos.Wave.nuspec -Version %VER% -OutputDirectory "%OUT%"
-nuget pack Azos.Media.nuspec -Version %VER% -OutputDirectory "%OUT%"
-nuget pack Azos.MongoDb.nuspec -Version %VER% -OutputDirectory "%OUT%"
-nuget pack Azos.MySql.nuspec -Version %VER% -OutputDirectory "%OUT%"
-nuget pack Azos.MsSql.nuspec -Version %VER% -OutputDirectory "%OUT%"
-nuget pack Azos.Oracle.nuspec -Version %VER% -OutputDirectory "%OUT%"
-nuget pack Azos.Sky.nuspec -Version %VER% -OutputDirectory "%OUT%"
-nuget pack Azos.Sky.MongoDb.nuspec -Version %VER% -OutputDirectory "%OUT%"
+set VER=1.0.209
+
+nuget pack Azos.nuspec -Version %VER% -OutputDirectory "%OUT%" -Properties icon="%ICON%"
+nuget pack Azos.Web.nuspec -Version %VER% -OutputDirectory "%OUT%" -Properties icon="%ICON%"
+nuget pack Azos.Wave.nuspec -Version %VER% -OutputDirectory "%OUT%" -Properties icon="%ICON%"
+nuget pack Azos.Media.nuspec -Version %VER% -OutputDirectory "%OUT%" -Properties icon="%ICON%"
+nuget pack Azos.MongoDb.nuspec -Version %VER% -OutputDirectory "%OUT%" -Properties icon="%ICON%"
+nuget pack Azos.MySql.nuspec -Version %VER% -OutputDirectory "%OUT%" -Properties icon="%ICON%"
+nuget pack Azos.MsSql.nuspec -Version %VER% -OutputDirectory "%OUT%" -Properties icon="%ICON%"
+nuget pack Azos.Oracle.nuspec -Version %VER% -OutputDirectory "%OUT%" -Properties icon="%ICON%"
+nuget pack Azos.Sky.nuspec -Version %VER% -OutputDirectory "%OUT%" -Properties icon="%ICON%"
+nuget pack Azos.Sky.MongoDb.nuspec -Version %VER% -OutputDirectory "%OUT%" -Properties icon="%ICON%"
 
  nuget push "%OUT%\Azos.%VER%.nupkg" %AZIST_NUGET_API_KEY% 
  nuget push "%OUT%\Azos.Web.%VER%.nupkg" %AZIST_NUGET_API_KEY% 
