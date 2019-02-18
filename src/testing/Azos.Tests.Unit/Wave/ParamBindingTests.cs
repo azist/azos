cf4975d44c6af7a3e271e7c04cededbf34361600
Dmitriy Khmaladze <d@zhabis.com>
2019-02-18 14:36:33 -0500

    Adding baseline unit tests for wave

--- src/testing/Azos.Tests.Unit/Wave/ParamBindingTests.cs
+++ src/testing/Azos.Tests.Unit/Wave/ParamBindingTests.cs
@@ -53,5 +53,56 @@ namespace Azos.Tests.Unit.Wave
     }
 
 
+    [Run]
+    public async Task EchoParamsWithDefaults_GET()
+    {
+      var response = await Client.GetAsync("echoparamswithdefaults?a=hello");
+      var got = (await response.Content.ReadAsStringAsync()).JSONToDataObject() as JSONDataMap;
+      Aver.IsNotNull(got);
+      Aver.AreEqual("hello", got["a"].AsString());
+      Aver.AreEqual(127, got["b"].AsInt());
+      Aver.AreEqual(true, got["c"].AsBool());
+    }
+
+    [Run]
+    public async Task EchoParamsWithDefaults_POST()
+    {
+      var content = new FormUrlEncodedContent(new[]{
+        new KeyValuePair<string, string>("a", "hello"),
+      });
+
+      var response = await Client.PostAsync("echoparamswithdefaults", content);
+      var got = (await response.Content.ReadAsStringAsync()).JSONToDataObject() as JSONDataMap;
+      Aver.IsNotNull(got);
+      Aver.AreEqual("hello", got["a"].AsString());
+      Aver.AreEqual(127, got["b"].AsInt());
+      Aver.AreEqual(true, got["c"].AsBool());
+    }
+
+
+    [Run]
+    public async Task EchoMixMap()
+    {
+      var response = await Client.GetAsync("echomixmap?a=hello&b=789&c=true&d=ok&e=false");
+      var got = (await response.Content.ReadAsStringAsync()).JSONToDataObject() as JSONDataMap;
+      Aver.IsNotNull(got);
+      Aver.AreEqual(4, got.Count);
+      Aver.AreEqual("hello", got["a"].AsString());
+      Aver.AreEqual(789, got["b"].AsInt());
+      Aver.AreEqual(true, got["c"].AsBool());
+
+      var got2 = got["got"] as JSONDataMap;
+      Aver.IsNotNull(got2);
+      Console.WriteLine(got2.ToJSON());
+
+      Aver.IsTrue( got2.Count >= 5);
+      Aver.AreEqual("hello", got2["a"].AsString());
+      Aver.AreEqual(789, got2["b"].AsInt());
+      Aver.AreEqual(true, got2["c"].AsBool());
+      Aver.AreEqual("ok", got2["d"].AsString());
+      Aver.AreEqual("false", got2["e"].AsString());
+
+    }
+
   }
 }
