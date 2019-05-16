c1095d07fb90dac2d00db1048e5d8f4bc135c6b1
Dmitriy Khmaladze <d@zhabis.com>
2019-05-16 09:59:26 -0400

    Add test ctrl for ApiDoc

---
+++ src/testing/TestBusinessLogic/WaveTestSite/Controllers/TestApiDoc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Azos.Wave.Mvc;
+
+namespace WaveTestSite.Controllers
+{
+  /// <summary>
+  /// Generates documentation for this site
+  /// </summary>
+  public class TestApiDoc : ApiDocController
+  {
+    protected override ApiDocGenerator MakeDocGenerator()
+    {
+      var gen = new ApiDocGenerator(App);
+      gen.Locations.Add(new ApiDocGenerator.ControllerLocation("TestBusinessLogic.dll", "WaveTestSite.Cont*"));
+      return gen;
+    }
+  }
+}
