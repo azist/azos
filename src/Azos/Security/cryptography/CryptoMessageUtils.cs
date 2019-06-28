889c34f59f2bc54e5a65107ec92f4e23c00f58df
Dmitriy Khmaladze <d@zhabis.com>
2019-06-28 14:40:43 -0400

    Wip

--- src/Azos/Security/cryptography/CryptoMessageUtils.cs
+++ src/Azos/Security/cryptography/CryptoMessageUtils.cs
@@ -11,9 +11,16 @@ namespace Azos.Security
   /// </summary>
   public static class CryptoMessageUtils
   {
-
+    /// <summary>
+    /// Protects message into a byte[]. Null is returned for null messages
+    /// </summary>
+    /// <param name="algorithm">Algorithm to use</param>
+    /// <param name="message">Message to protect using Json format</param>
+    /// <param name="options">Json format options</param>
+    /// <returns>Binary representation of protected message</returns>
     public static byte[] ProtectAsBuffer(this ICryptoMessageAlgorithm algorithm, object message, JsonWritingOptions options = null)
     {
+      if (message == null) return null;
       if (options==null) options = JsonWritingOptions.CompactRowsAsMap;
       var raw = JsonWriter.WriteToBuffer(message, options, Encoding.UTF8);
       var result = algorithm.NonNull(nameof(algorithm)).Protect(new ArraySegment<byte>(raw));
@@ -21,7 +28,7 @@ namespace Azos.Security
     }
 
     /// <summary>
-    /// Protects message as web-safe URI string
+    /// Protects message as web-safe URI string. Null is returned for null messages
     /// </summary>
     /// <param name="algorithm">Algorithm to use</param>
     /// <param name="message">Message to protect using Json format</param>
@@ -33,6 +40,13 @@ namespace Azos.Security
       return bin.ToWebSafeBase64();
     }
 
+
+    /// <summary>
+    /// Tries to decode/unprotect the message, returning null if the protectedMessage does not represent a valid protected message
+    /// </summary>
+    /// <param name="algorithm">Algorithm to use</param>
+    /// <param name="protectedMessage">Protected message content encoded as string</param>
+    /// <returns>Unprotected/decoded message or null if the protectedMessage is not valid</returns>
     public static IJsonDataObject Unprotect(this ICryptoMessageAlgorithm algorithm, string protectedMessage)
     {
       var raw = protectedMessage.FromWebSafeBase64();
@@ -40,14 +54,26 @@ namespace Azos.Security
       return Unprotect(algorithm, new ArraySegment<byte>(raw));
     }
 
-
+    /// <summary>
+    /// Tries to decode/unprotect the message, returning null if the protectedMessage does not represent a valid protected message
+    /// </summary>
+    /// <param name="algorithm">Algorithm to use</param>
+    /// <param name="protectedMessage">Protected message content</param>
+    /// <returns>Unprotected/decoded message or null if the protectedMessage is not valid</returns>
     public static IJsonDataObject Unprotect(this ICryptoMessageAlgorithm algorithm, ArraySegment<byte> protectedMessage)
     {
       var raw = algorithm.Unprotect(protectedMessage);
       if (raw==null) return null;
       using(var ms = new MemoryStream(raw))
       {
-        return JsonReader.DeserializeDataObject(ms, Encoding.UTF8, true);
+        try
+        {
+          return JsonReader.DeserializeDataObject(ms, Encoding.UTF8, true);
+        }
+        catch
+        {
+          return null;//corrupted message
+        }
       }
     }
   }
