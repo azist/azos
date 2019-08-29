698c6831480e1e267312a90e5da7e2ebd2b1ee22
Dmitriy Khmaladze <d@zhabis.com>
2019-08-28 20:27:03 -0400

    Add Azos.Data.Business modeling entities

---
+++ src/providers/Azos.IAM/Protocol/filters/Filter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azos.IAM.Protocol
+{
+  //////public abstract class Filter<TSaveResult> : FormModel<TSaveResult>, IFilterModel
+  //////{
+  //////}
+}
