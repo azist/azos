99c836e396a5c60aa4cb93fe6c52c120be1fd71b
Dmitriy Khmaladze <d@zhabis.com>
2019-11-09 20:46:07 -0500

    WIP Azos.Client

--- src/Azos/Client/http/HttpService.cs
+++ src/Azos/Client/http/HttpService.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading;
 using Azos.Apps;
 using Azos.Conf;
 
@@ -14,10 +15,20 @@ namespace Azos.Client
     {
     }
 
+    protected override void Destructor()
+    {
+      m_Transports.ForEach( kvp => this.DontLeak( () => kvp.Value.Dispose()) );
+      base.Destructor();
+    }
+
+    private object m_TransportLock = new object();
+    private volatile Dictionary<EndpointAssignment, HttpTransport> m_Transports = new Dictionary<EndpointAssignment, HttpTransport>();
+
     protected override IEnumerable<EndpointAssignment> DoGetEndpointsForCall(string remoteAddress, string contract, object shardKey, string network, string binding)
     {
       var shard = (int)Data.ShardingUtils.ObjectToShardingID(shardKey) & CoreConsts.ABS_HASH_MASK;
 
+      //todo add Caching for speed
       var shards = m_Endpoints.Where( ep =>
          ep.RemoteAddress.EqualsIgnoreCase(remoteAddress) &&
          ep.Contract.EqualsIgnoreCase(contract) &&
@@ -31,12 +42,29 @@ namespace Azos.Client
 
     protected override HttpTransport DoAcquireTransport(EndpointAssignment assignment, bool reserve)
     {
-      throw new NotImplementedException();
+      if (reserve)
+      {
+        return new HttpTransport(assignment);
+      }
+
+      if (m_Transports.TryGetValue(assignment, out var transport)) return transport;
+      lock(m_TransportLock)
+      {
+        if (m_Transports.TryGetValue(assignment, out transport)) return transport;
+
+        transport = new HttpTransport(assignment);
+        var dict = new Dictionary<EndpointAssignment, HttpTransport>(m_Transports);
+        dict[assignment] = transport;
+        Thread.MemoryBarrier();
+        m_Transports = dict;
+      }
+
+      return transport;
     }
 
     protected override void DoReleaseTransport(HttpTransport endpoint)
     {
-      throw new NotImplementedException();
+      //do nothing
     }
 
   }
