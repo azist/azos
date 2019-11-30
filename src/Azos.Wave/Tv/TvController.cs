555332ea64e0d02f839279fc1a98cd01d6d72c5a
Dmitriy Khmaladze <d@zhabis.com>
2019-11-30 18:05:08 -0500

    WIP add Tv

---
+++ src/Azos.Wave/Tv/TvController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Azos.Wave.Mvc;
+
+namespace Azos.Wave.Tv
+{
+  /// <summary>
+  /// Implements MVC controller that acts as a server for Tele-services (Tele-Vision)
+  /// </summary>
+  [NoCache]
+  public class TvController : Controller
+  {
+  }
+}
---
+++ src/Azos.Wave/Tv/TvProgramBody.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Azos.Apps;
+using Azos.IO.Console;
+using Azos.Platform;
+
+namespace Azos.Wave.Tv
+{
+  /// <summary>
+  /// Television program body
+  /// </summary>
+  public static class TvProgramBody
+  {
+    public static void Main(string[] args)
+    {
+      try
+      {
+        using (var app = new AzosApplication(true, args, null))
+        {
+          app.SetConsolePort(LocalConsolePort.Default);
+          System.Environment.ExitCode = runServer(app);
+        }
+      }
+      catch (Exception error)
+      {
+        ConsoleUtils.Error(error.ToMessageWithType());
+        ConsoleUtils.Warning(error.StackTrace);
+        Console.WriteLine();
+        System.Environment.ExitCode = -1;
+      }
+    }
+
+    private static int runServer(AzosApplication app)
+    {
+      var config = app.CommandArgs;
+
+      ConsoleUtils.WriteMarkupContent(typeof(TvProgramBody).GetText("Welcome.txt"));
+
+
+      if (config["?"].Exists ||
+          config["h"].Exists ||
+          config["help"].Exists)
+      {
+        ConsoleUtils.WriteMarkupContent(typeof(TvProgramBody).GetText("Help.txt"));
+        return 0;
+      }
+
+      using(var web = new WaveServer(app))
+      {
+        web.Configure(null);
+        web.Start();
+        Console.ReadLine();
+      }
+
+      return 0;
+    }
+
+ }
+}
