879ed8967c1ee113b866a1fd1914a7cb83d70d07
Dmitriy Khmaladze <d@zhabis.com>
2019-12-08 13:28:29 -0500

    Nuget push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=1.0.532.1
-set HEADLINE=Svc: Add JsonHandlerAttribute for polymorphic json support
+set VER=1.0.533
+set HEADLINE=AST SQL Expressions
 
 call vs2017-build-release %VER%
 
