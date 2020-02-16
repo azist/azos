7bee48ae922cb1cb5ac39768e2ba14a740b88d67
Dmitriy Khmaladze <d@zhabis.com>
2020-02-16 17:36:22 -0500

    Add Expressions.readme.md

--- src/Azos/Scripting/Expressions/Operators.cs
+++ src/Azos/Scripting/Expressions/Operators.cs
@@ -13,6 +13,11 @@ namespace Azos.Scripting.Expressions
   /// </summary>
   public abstract class BinaryOperator<TContext, TResult, TLeftOperand, TRightOperand> : Expression<TContext, TResult>, IBinaryOperator
   {
+    public const string CONFIG_LEFT1_SECT = "left-operand";
+    public const string CONFIG_LEFT2_SECT = "left";
+    public const string CONFIG_RIGHT1_SECT = "right-operand";
+    public const string CONFIG_RIGHT2_SECT = "right";
+
     public Expression<TContext, TLeftOperand> LeftOperand { get; set; }
     public Expression<TContext, TRightOperand> RightOperand { get; set; }
 
@@ -22,8 +27,8 @@ namespace Azos.Scripting.Expressions
     protected override void DoConfigure(IConfigSectionNode node)
     {
       base.DoConfigure(node);
-      LeftOperand = FactoryUtils.MakeAndConfigure<Expression<TContext, TLeftOperand>>(node);
-      RightOperand = FactoryUtils.MakeAndConfigure<Expression<TContext, TRightOperand>>(node);
+      LeftOperand = FactoryUtils.MakeAndConfigure<Expression<TContext, TLeftOperand>>(node[CONFIG_LEFT1_SECT, CONFIG_LEFT2_SECT]);
+      RightOperand = FactoryUtils.MakeAndConfigure<Expression<TContext, TRightOperand>>(node[CONFIG_RIGHT1_SECT, CONFIG_RIGHT2_SECT]);
     }
   }
 
@@ -32,13 +37,15 @@ namespace Azos.Scripting.Expressions
   /// </summary>
   public abstract class UnaryOperator<TContext, TResult, TOperand> : Expression<TContext, TResult>, IUnaryOperator
   {
+    public const string CONFIG_OPERAND_SECT = "operand";
+
     public Expression<TContext, TOperand> Operand { get; set; }
     IExpression IUnaryOperator.Operand => Operand;
 
     protected override void DoConfigure(IConfigSectionNode node)
     {
       base.DoConfigure(node);
-      Operand = FactoryUtils.MakeAndConfigure<Expression<TContext, TOperand>>(node);
+      Operand = FactoryUtils.MakeAndConfigure<Expression<TContext, TOperand>>(node[CONFIG_OPERAND_SECT]);
     }
   }
 
