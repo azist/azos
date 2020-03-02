fc43d53d48754cd61045a8afde4d8d1b79f46a64
Dmitriy Khmaladze <d@zhabis.com>
2020-03-02 13:19:58 -0500

    Add more cases under JazonReadTests

--- src/testing/Azos.Tests.Nub/Serialization/json/JazonReadTests.cs
+++ src/testing/Azos.Tests.Nub/Serialization/json/JazonReadTests.cs
@@ -14,6 +14,117 @@ namespace Azos.Tests.Nub.Serialization
   {
     private static readonly IJsonReaderBackend JZ = new JazonReaderBackend();
 
+    [Run]
+    public void Object_1()
+    {
+      var map = JZ.DeserializeFromJson("{ a: 1, b: 2, c: null, d: {a: 'string'}, e: [ - 1000,null, true, -2e3,{msg: 'my message'}]}", true) as JsonDataMap;
+      Aver.IsNotNull(map);
+      Aver.AreEqual(5, map.Count);
+      Aver.AreObjectsEqual(1, map["a"]);
+      Aver.AreObjectsEqual(2, map["b"]);
+      Aver.AreObjectsEqual(null, map["c"]);
+
+      var d = map["d"] as JsonDataMap;
+      Aver.IsNotNull(d);
+      Aver.AreObjectsEqual("string", d["a"]);
+
+      var e = map["e"] as JsonDataArray;
+      Aver.IsNotNull(e);
+      Aver.AreEqual(5, e.Count);
+
+      Aver.AreObjectsEqual(-1000, e[0]);
+      Aver.AreObjectsEqual(null, e[1]);
+      Aver.AreObjectsEqual(true, e[2]);
+      Aver.AreObjectsEqual(-2e3, e[3]);
+
+      var e5 = e[4] as JsonDataMap;
+      Aver.IsNotNull(e5);
+      Aver.AreEqual(1, e5.Count);
+      Aver.AreObjectsEqual("my message", e5["msg"]);
+    }
+
+    [Run]
+    public void Object_2()
+    {
+      var got = JZ.DeserializeFromJson("[1,2,3]", true) as JsonDataArray;
+      Aver.IsNotNull(got);
+      Aver.AreEqual(3, got.Count);
+      Aver.AreObjectsEqual(1, got[0]);
+      Aver.AreObjectsEqual(2, got[1]);
+      Aver.AreObjectsEqual(3, got[2]);
+    }
+
+    [Run]
+    public void Object_3()
+    {
+      var got = JZ.DeserializeFromJson("123", true);
+      Aver.IsNotNull(got);
+      Aver.AreObjectsEqual(123, got);
+    }
+
+    [Run]
+    public void Object_4()
+    {
+      var got = JZ.DeserializeFromJson("-123", true);
+      Aver.IsNotNull(got);
+      Aver.AreObjectsEqual(-123, got);
+    }
+
+    [Run]
+    public void Object_5()
+    {
+      var got = JZ.DeserializeFromJson("-123000000000", true);
+      Aver.IsNotNull(got);
+      Aver.AreObjectsEqual(-123_000_000_000, got);
+    }
+
+    [Run]
+    public void Object_6()
+    {
+      var got = JZ.DeserializeFromJson("true", true);
+      Aver.IsNotNull(got);
+      Aver.AreObjectsEqual(true, got);
+    }
+
+    [Run]
+    public void Object_7()
+    {
+      var got = JZ.DeserializeFromJson("false", true);
+      Aver.IsNotNull(got);
+      Aver.AreObjectsEqual(false, got);
+    }
+
+    [Run]
+    public void Object_8()
+    {
+      var got = JZ.DeserializeFromJson("null", true);
+      Aver.IsNull(got);
+    }
+
+    [Run]
+    public void Object_9()
+    {
+      var got = JZ.DeserializeFromJson("'string'", true);
+      Aver.IsNotNull(got);
+      Aver.AreObjectsEqual("string", got);
+    }
+
+    [Run]
+    public void Object_10()
+    {
+      var got = JZ.DeserializeFromJson("{}", true) as JsonDataMap;
+      Aver.IsNotNull(got);
+      Aver.AreObjectsEqual(0, got.Count);
+    }
+
+    [Run]
+    public void Object_11()
+    {
+      var got = JZ.DeserializeFromJson("[]", true) as JsonDataArray;
+      Aver.IsNotNull(got);
+      Aver.AreObjectsEqual(0, got.Count);
+    }
+
 
     [Run]
     public void Integers()
@@ -106,6 +217,38 @@ namespace Azos.Tests.Nub.Serialization
       Aver.IsNotNull(map);
       Aver.AreEqual(1, map.Count);
       Aver.IsTrue(map["a"].ToString().MatchPattern(@"*this string* uses \escapes that \donot \ufff??? evaluate*Mc'Cloud's ""name""*all*"));
+    }
+
+    [Run]
+    public void Comments_1()
+    {
+      var map = JZ.DeserializeFromJson(@"{ 
+       a: 1, //comment1
+      /* comment2 */
+       b: 2  // comment /* 3*/
+       ,/**/c:/* */3
+      }", true) as JsonDataMap;
+      Aver.IsNotNull(map);
+      Aver.AreEqual(3, map.Count);
+      Aver.AreObjectsEqual(1, map["a"]);
+      Aver.AreObjectsEqual(2, map["b"]);
+      Aver.AreObjectsEqual(3, map["c"]);
+    }
+
+    [Run]
+    public void Comments_2()
+    {
+      var map = JZ.DeserializeFromJson(@"{ 
+       a: 1, //comment1
+      /* ""comment2 */
+       b: 2  // commen't /* 3*/
+       ,/**/c: ""/* */3""
+      }", true) as JsonDataMap;
+      Aver.IsNotNull(map);
+      Aver.AreEqual(3, map.Count);
+      Aver.AreObjectsEqual(1, map["a"]);
+      Aver.AreObjectsEqual(2, map["b"]);
+      Aver.AreObjectsEqual("/* */3", map["c"]);
     }
 
   }
