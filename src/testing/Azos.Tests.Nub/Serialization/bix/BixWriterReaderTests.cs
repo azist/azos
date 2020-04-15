4dce7000acf9fe110bc7652c2e44153c9aaaa485
Dmitriy Khmaladze <d@zhabis.com>
2020-04-14 21:51:00 -0400

    wip tests

--- src/testing/Azos.Tests.Nub/Serialization/bix/BixWriterReaderTests.cs
+++ src/testing/Azos.Tests.Nub/Serialization/bix/BixWriterReaderTests.cs
@@ -99,75 +99,73 @@ namespace Azos.Tests.Nub.Serialization
     #region BYTE
     [Run] public void Byte_01()
     {
-      byte v = 0;
-      testScalar(v, w => w.Write(v), r => r.ReadByte());
+      byte v = byte.MinValue;
+      testScalar(v, w => w.Write(v), r => r.ReadByte(), 1);
+
+      v = byte.MaxValue;
+      testScalar(v, w => w.Write(v), r => r.ReadByte(), 1);
     }
 
-    [Run] public void Byte_02()
-    {
-      byte v = 255;
-      testScalar(v, w => w.Write(v), r => r.ReadByte());
-    }
-
-    [Run] public void Byte_03_Nullable()
+    [Run] public void Byte_02_Nullable()
     {
       byte? v = null;
-      testScalar(v, w => w.Write(v), r => r.ReadNullableByte());
+      testScalar(v, w => w.Write(v), r => r.ReadNullableByte(), 1);
+
+      v = byte.MinValue;
+      testScalar(v, w => w.Write(v), r => r.ReadNullableByte(), 2);
+
+      v = byte.MaxValue;
+      testScalar(v, w => w.Write(v), r => r.ReadNullableByte(), 2);
     }
 
-    [Run] public void Byte_04_Nullable()
-    {
-      byte? v = 255;
-      testScalar(v, w => w.Write(v), r => r.ReadNullableByte());
-    }
-
-    [Run] public void Byte_05_Collection()
+    [Run] public void Byte_03_Collection()
     {
       List<byte> v = null;
       testCollection(v, w => w.WriteCollection(v), r => r.ReadByteCollection<List<byte>>());
-    }
 
-    [Run] public void Byte_06_Collection()
-    {
-      List<byte> v = new List<byte>{0,1, 2, 3, 4};
+      v = new List<byte> { 0, 1, 2, 3, 4 };
       testCollection(v, w => w.WriteCollection(v), r => r.ReadByteCollection<List<byte>>());
     }
 
-    [Run] public void Byte_07_CollectionNullable()
+    [Run] public void Byte_04_CollectionNullable()
     {
       List<byte?> v = null;
       testCollection(v, w => w.WriteCollection(v), r => r.ReadNullableByteCollection<List<byte?>>());
-    }
 
-    [Run] public void Byte_08_CollectionNullable()
-    {
-      List<byte?> v = new List<byte?> { 0, null, 2, null, 4 };
+      v = new List<byte?> { 0, null, 2, null, 4 };
       testCollection(v, w => w.WriteCollection(v), r => r.ReadNullableByteCollection<List<byte?>>());
     }
 
-    [Run] public void Byte_09_Array()
+
+    [Run] public void Byte_05_Array()
     {
       byte[] v = null;
       testArray(v, w => w.Write(v), r => r.ReadByteArray());
+
+      v = new byte[] { 1, 2, 5, 6 };
+      testArray(v, w => w.Write(v), r => r.ReadByteArray(), 1 + 1 + 4);
     }
 
-    [Run] public void Byte_10_Array()
-    {
-      byte[] v = new byte[]{1,2,5,6};
-      testArray(v, w => w.Write(v), r => r.ReadByteArray());
-    }
-
-    [Run] public void Byte_11_ArrayNullable()
+    [Run] public void Byte_06_ArrayNullable()
     {
       byte?[] v = null;
-      testArray(v, w => w.Write(v), r => r.ReadNullableByteArray());
+      testArray(v, w => w.Write(v), r => r.ReadNullableByteArray(), 1);
+
+      v = new byte?[] { 1, null, 25 };
+      testArray(v, w => w.Write(v), r => r.ReadNullableByteArray(), 1 + 1 + 5);
     }
 
-    [Run] public void Byte_12_ArrayNullable()
+    [Run]
+    public void Byte_07_Buffer()
     {
-      byte?[] v = new byte?[]{1, null, 25};
-      testArray(v, w => w.Write(v), r => r.ReadNullableByteArray());
+      byte[] v = null;
+      testArray(v, w => w.WriteBuffer(v), r => r.ReadBuffer(), 1);
+
+      v = new byte[] { 1, 2, 5, 6 };
+      testArray(v, w => w.WriteBuffer(v), r => r.ReadBuffer(), 1 + 1 + 4);
     }
+
+
     #endregion
 
     #region BOOL
@@ -176,82 +174,59 @@ namespace Azos.Tests.Nub.Serialization
     {
       bool v = false;
       testScalar(v, w => w.Write(v), r => r.ReadBool(), 1);
-    }
-
-    [Run]
-    public void Bool_02()
-    {
-      bool v = true;
+      v = true;
       testScalar(v, w => w.Write(v), r => r.ReadBool(), 1);
     }
 
     [Run]
-    public void Bool_03_Nullable()
+    public void Bool_02_Nullable()
     {
       bool? v = null;
       testScalar(v, w => w.Write(v), r => r.ReadNullableBool(), 1);
-    }
 
-    [Run]
-    public void Bool_04_Nullable()
-    {
-      bool? v = true;
+      v = false;
+      testScalar(v, w => w.Write(v), r => r.ReadNullableBool(), 2);
+      v = true;
       testScalar(v, w => w.Write(v), r => r.ReadNullableBool(), 2);
     }
 
     [Run]
-    public void Bool_05_Collection()
+    public void Bool_03_Collection()
     {
       List<bool> v = null;
       testCollection(v, w => w.WriteCollection(v), r => r.ReadBoolCollection<List<bool>>());
-    }
 
-    [Run]
-    public void Bool_06_Collection()
-    {
-      List<bool> v = new List<bool> { false, true, true };
+      v = new List<bool> { false, true, true };
       testCollection(v, w => w.WriteCollection(v), r => r.ReadBoolCollection<List<bool>>());
     }
 
     [Run]
-    public void Bool_07_CollectionNullable()
+    public void Bool_04_CollectionNullable()
     {
       List<bool?> v = null;
       testCollection(v, w => w.WriteCollection(v), r => r.ReadNullableBoolCollection<List<bool?>>());
-    }
 
-    [Run]
-    public void Bool_08_CollectionNullable()
-    {
-      List<bool?> v = new List<bool?> { true, null, false, false, null, true };
+      v = new List<bool?> { true, null, false, false, null, true };
       testCollection(v, w => w.WriteCollection(v), r => r.ReadNullableBoolCollection<List<bool?>>());
     }
 
     [Run]
-    public void Bool_09_Array()
+    public void Bool_05_Array()
     {
       bool[] v = null;
       testArray(v, w => w.Write(v), r => r.ReadBoolArray());
-    }
 
-    [Run]
-    public void Bool_10_Array()
-    {
-      bool[] v = new bool[] { true, false, true, true };
+      v = new bool[] { true, false, true, true };
       testArray(v, w => w.Write(v), r => r.ReadBoolArray());
     }
 
     [Run]
-    public void Bool_11_ArrayNullable()
+    public void Bool_06_ArrayNullable()
     {
       bool?[] v = null;
       testArray(v, w => w.Write(v), r => r.ReadNullableBoolArray());
-    }
 
-    [Run]
-    public void Bool_12_ArrayNullable()
-    {
-      bool?[] v = new bool?[] { true, null, false, false };
+      v = new bool?[] { true, null, false, false };
       testArray(v, w => w.Write(v), r => r.ReadNullableBoolArray());
     }
     #endregion
@@ -262,90 +237,146 @@ namespace Azos.Tests.Nub.Serialization
     {
       sbyte v = 0;
       testScalar(v, w => w.Write(v), r => r.ReadSbyte(), 1);
-    }
 
-    [Run]
-    public void SByte_02()
-    {
-      sbyte v = -128;
+      v = sbyte.MinValue;
       testScalar(v, w => w.Write(v), r => r.ReadSbyte(), 1);
 
-      v = 127;
+      v = sbyte.MaxValue;
       testScalar(v, w => w.Write(v), r => r.ReadSbyte(), 1);
     }
 
     [Run]
-    public void SByte_03_Nullable()
+    public void SByte_02_Nullable()
     {
       sbyte? v = null;
       testScalar(v, w => w.Write(v), r => r.ReadNullableSbyte(), 1);
-    }
 
-    [Run]
-    public void SByte_04_Nullable()
-    {
-      sbyte? v = -128;
+      v = sbyte.MinValue;
       testScalar(v, w => w.Write(v), r => r.ReadNullableSbyte(), 2);
 
-      v = 127;
+      v = sbyte.MaxValue;
       testScalar(v, w => w.Write(v), r => r.ReadNullableSbyte(), 2);
     }
 
     [Run]
-    public void SByte_05_Collection()
+    public void SByte_03_Collection()
     {
       List<sbyte> v = null;
       testCollection(v, w => w.WriteCollection(v), r => r.ReadSbyteCollection<List<sbyte>>(), 1);
-    }
 
-    [Run]
-    public void SByte_06_Collection()
-    {
-      List<sbyte> v = new List<sbyte> { 0, -1, 127, -128, 0 };
+      v = new List<sbyte> { 0, -1, 127, -128, 0 };
       testCollection(v, w => w.WriteCollection(v), r => r.ReadSbyteCollection<List<sbyte>>(), 1+1+5);
     }
 
     [Run]
-    public void SByte_07_CollectionNullable()
+    public void SByte_04_CollectionNullable()
     {
       List<sbyte?> v = null;
       testCollection(v, w => w.WriteCollection(v), r => r.ReadNullableSbyteCollection<List<sbyte?>>(), 1);
-    }
 
-    [Run]
-    public void SByte_08_CollectionNullable()
-    {
-      List<sbyte?> v = new List<sbyte?> { 0, null, -128, null, 127 };
+      v = new List<sbyte?> { 0, null, -128, null, 127 };
       testCollection(v, w => w.WriteCollection(v), r => r.ReadNullableSbyteCollection<List<sbyte?>>(), 1+1+8);
     }
 
     [Run]
-    public void SByte_09_Array()
+    public void SByte_05_Array()
     {
       sbyte[] v = null;
       testArray(v, w => w.Write(v), r => r.ReadSbyteArray(), 1);
-    }
 
-    [Run]
-    public void SByte_10_Array()
-    {
-      sbyte[] v = new sbyte[] { 1, -2, 127, -128 };
+      v = new sbyte[] { 1, -2, 127, -128 };
       testArray(v, w => w.Write(v), r => r.ReadSbyteArray(), 1+1+4);
     }
 
     [Run]
-    public void SByte_11_ArrayNullable()
+    public void SByte_06_ArrayNullable()
     {
       sbyte?[] v = null;
       testArray(v, w => w.Write(v), r => r.ReadNullableSbyteArray(), 1);
+
+      v = new sbyte?[] { 1, null, -128, 127, 0, 9 };
+      testArray(v, w => w.Write(v), r => r.ReadNullableSbyteArray(), 1+1+11);
+    }
+    #endregion
+
+
+    #region SHORT
+    [Run]
+    public void Short_01()
+    {
+      short v = short.MinValue;
+      testScalar(v, w => w.Write(v), r => r.ReadShort(), 3);
+
+      v = short.MaxValue;
+      testScalar(v, w => w.Write(v), r => r.ReadShort(), 3);
+
+      v = 0;
+      testScalar(v, w => w.Write(v), r => r.ReadShort(), 1);
+
+      v = 256;
+      testScalar(v, w => w.Write(v), r => r.ReadShort(), 2);
     }
 
     [Run]
-    public void SByte_12_ArrayNullable()
+    public void Short_02_Nullable()
     {
-      sbyte?[] v = new sbyte?[] { 1, null, -128, 127, 0, 9 };
-      testArray(v, w => w.Write(v), r => r.ReadNullableSbyteArray(), 1+1+11);
+      short? v = null;
+      testScalar(v, w => w.Write(v), r => r.ReadNullableShort(), 1);
+
+      v = short.MinValue;
+      testScalar(v, w => w.Write(v), r => r.ReadNullableShort(), 4);
+
+      v = short.MaxValue;
+      testScalar(v, w => w.Write(v), r => r.ReadNullableShort(), 4);
+
+      v = 0;
+      testScalar(v, w => w.Write(v), r => r.ReadNullableShort(), 2);
+
+      v = 256;
+      testScalar(v, w => w.Write(v), r => r.ReadNullableShort(), 3);
     }
+
+    [Run]
+    public void Short_03_Collection()
+    {
+      List<short> v = null;
+      testCollection(v, w => w.WriteCollection(v), r => r.ReadShortCollection<List<short>>(), 1);
+
+      v = new List<short> { 0, -1, 127, -128, 0 };
+      testCollection(v, w => w.WriteCollection(v), r => r.ReadShortCollection<List<short>>(), 1 + 1 + 7);
+    }
+
+
+    [Run]
+    public void Short_04_CollectionNullable()
+    {
+      List<short?> v = null;
+      testCollection(v, w => w.WriteCollection(v), r => r.ReadNullableShortCollection<List<short?>>(), 1);
+
+      v = new List<short?> { 0, null, -130, null, 180 };
+      testCollection(v, w => w.WriteCollection(v), r => r.ReadNullableShortCollection<List<short?>>(), 1 + 1 + 10);
+    }
+
+    [Run]
+    public void Short_05_Array()
+    {
+      short[] v = null;
+      testArray(v, w => w.Write(v), r => r.ReadShortArray(), 1);
+
+      v = new short[] { 1, -12, 127, -728 };
+      testArray(v, w => w.Write(v), r => r.ReadShortArray(), 1 + 1 + 6);
+    }
+
+    [Run]
+    public void Short_06_ArrayNullable()
+    {
+      short?[] v = null;
+      testArray(v, w => w.Write(v), r => r.ReadNullableShortArray(), 1);
+
+      v = new short?[] { 1, null, -1282, 1227, 0, 9 };
+      testArray(v, w => w.Write(v), r => r.ReadNullableShortArray(), 1 + 1 + 13);
+    }
+
     #endregion
 
 
