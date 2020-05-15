ef34d5aaa9ce28a0b031b91be9727a5cbd7d565d
Dmitriy Khmaladze <d@zhabis.com>
2020-05-14 22:26:01 -0400

    Add HourList per #285

--- src/Azos/Time/HourList.cs
+++ src/Azos/Time/HourList.cs
@@ -4,129 +4,234 @@
  * See the LICENSE file in the project root for more information.
 </FILE_LICENSE>*/
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using Azos.Serialization.JSON;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-//namespace Azos.Time
-//{
-//  /// <summary>
-//  /// Efficiently represents a list of minute-aligned time spans within a day
-//  /// which is typically used for operation hours/schedules
-//  /// </summary>
-//  public struct HourList : IEquatable<HourList>, IJsonWritable, IJsonReadable
-//  {
-//    public struct Span : IEquatable<Span>, IComparable<Span>
-//    {
-//      public const int MINUTES_PER_DAY = 24 * 60;
+using Azos.Serialization.JSON;
 
-//      internal Span(int start, int duration)
-//      {
-//        StartMinute = start;
-//        DurationMinutes = duration;
-//      }
+namespace Azos.Time
+{
+  /// <summary>
+  /// Efficiently represents a list of minute-aligned time spans within a day
+  /// which is typically used for operation hours/schedules
+  /// </summary>
+  public struct HourList : IEquatable<HourList>, IJsonWritable, IJsonReadable
+  {
+    public const int MINUTES_PER_DAY = 24 * 60;
 
-//      public readonly int StartMinute;
-//      public readonly int DurationMinutes;
+    public struct Span : IEquatable<Span>, IComparable<Span>
+    {
 
-//      public int FinishMinute => StartMinute + DurationMinutes;
+      internal Span(int start, int duration)
+      {
+        StartMinute = start;
+        DurationMinutes = duration;
+      }
 
-//      public string Start
-//      {
-//        get
-//        {
-//          var hr = StartMinute / 60;
-//          var min = StartMinute % 60;
-//          return "{0:n0}:{1:n0}".Args(hr, min);
-//        }
-//      }
+      public readonly int StartMinute;
+      public readonly int DurationMinutes;
 
-//      public string Finish
-//      {
-//        get
-//        {
-//          var to = StartMinute + DurationMinutes;
-//          if (to > MINUTES_PER_DAY) to = to % MINUTES_PER_DAY;
-//          var hr = to / 60;
-//          var min = to % 60;
-//          return "{0:n0}:{1:n0}".Args(hr, min);
-//        }
-//      }
+      public int FinishMinute => StartMinute + DurationMinutes;
 
-//      public override string ToString() => Start + "-" + Finish;
-//      public override int GetHashCode() => StartMinute;
-//      public override bool Equals(object obj) => obj is Span span ? this.Equals(span) : false;
-//      public bool Equals(Span other) => this.Start == other.Start && this.DurationMinutes == other.DurationMinutes;
-//      public int CompareTo(Span other) => this.StartMinute < other.StartMinute ? -1 : this.StartMinute > other.StartMinute ? +1 : 0;
-//    }
+      public string Start
+      {
+        get
+        {
+          var hr = StartMinute / 60;
+          var min = StartMinute % 60;
+          return "{0:D}:{1:D2}".Args(hr, min);
+        }
+      }
 
-//    public static readonly char[] DELIMS = new []{',', ';'};
+      public string Finish
+      {
+        get
+        {
+          var to = StartMinute + DurationMinutes;
+          if (to > MINUTES_PER_DAY) to = to % MINUTES_PER_DAY;
+          var hr = to / 60;
+          var min = to % 60;
+          return "{0:D}:{1:D2}".Args(hr, min);
+        }
+      }
 
-//    public HourList(string data)
-//    {
-//      Data = data;
-//      m_Spans = null;
-//    }
+      public override string ToString() => Start + "-" + Finish;
+      public override int GetHashCode() => StartMinute;
+      public override bool Equals(object obj) => obj is Span span ? this.Equals(span) : false;
+      public bool Equals(Span other) => this.Start == other.Start && this.DurationMinutes == other.DurationMinutes;
+      public int CompareTo(Span other) => this.StartMinute < other.StartMinute ? -1 : this.StartMinute > other.StartMinute ? +1 : 0;
 
-//    private Span[] m_Spans;
+      public static bool operator ==(Span a, Span b) =>  a.Equals(b);
+      public static bool operator !=(Span a, Span b) => !a.Equals(b);
+    }
 
-//    public readonly string Data;
+    public static readonly char[] DELIMS = new[] { ',', ';' };
 
-//    public bool IsAssigned => Data.IsNotNullOrWhiteSpace();
+    public static HourList Parse(string v)
+    {
+      var data = parse(v);
+      if (data==null) throw new TimeoutException(StringConsts.ARGUMENT_ERROR + " Bad hour list syntax, out of order, or overlap in `{0}`".Args(v.TakeFirstChars(64)));;
+      var result = new HourList(v);
+      result.m_Spans = data;
+      return result;
+    }
 
-//    public IEnumerable<Span> Spans
-//    {
-//      get
-//      {
-//        if (m_Spans!=null) return m_Spans;
-//        m_Spans = parse(Data);
-//        if (m_Spans==null) throw new TimeoutException(StringConsts.ARGUMENT_ERROR + " Bad hour list syntax, out of order, or overlap in `{0}`".Args(Data.TakeFirstChars(64)));
-//        return m_Spans;
-//      }
-//    }
+    public static bool TryParse(string v, out HourList result)
+    {
+      var data = parse(v);
+      if (data == null)
+      {
+        result = default(HourList);
+        return false;
+      }
 
-//    private static Span[] parse(string content)
-//    {
-//      if (content.IsNullOrWhiteSpace()) return null;
+      result = new HourList(v);
+      result.m_Spans = data;
+      return true;
+    }
 
-//      var kvps = content.Split(DELIMS)
-//                      .Where(p => p.IsNotNullOrWhiteSpace())
-//                      .Select(p => p.SplitKVP('-') );
+    public HourList(string data)
+    {
+      Data = data;
+      m_Spans = null;
+    }
 
-//      var data = new List<Span>();
-//      foreach(var kvp in kvps)
-//      {
-//        var rawStart = parseComponent(kvp.Key);
-//        if (rawStart < 0) return null;
-//        var rawFinish = parseComponent(kvp.Value);
-//        if (rawFinish <0 || rawFinish < rawStart) return null;
-//        var span = new Span(rawStart, rawFinish - rawStart);
-//        data.Add(span);
-//      }
+    private Span[] m_Spans;
 
-//      //order it time
-//      var result = data.OrderBy(s => s.StartMinute).ToArray();
+    public readonly string Data;
 
-//      //check for overlap
-//      var start =0;
-//      foreach(var span in result)
-//       if (span.StartMinute < start)
-//         return null;
-//       else
-//         start = span.FinishMinute;
+    public bool IsAssigned => Data.IsNotNullOrWhiteSpace();
 
-//      return result;
-//    }
+    /// <summary>
+    /// Returns ordered set of hour spans. Throws if data is assigned but invalid
+    /// </summary>
+    public IEnumerable<Span> Spans
+    {
+      get
+      {
+        if (m_Spans != null) return m_Spans;
+        if (!IsAssigned) return Enumerable.Empty<Span>();
 
-//    private static int parseComponent(string c)
-//    {
-//      if (c.IsNullOrWhiteSpace()) return -1;
-//      return 0;
-//    }
+        m_Spans = parse(Data);
+        if (m_Spans == null) throw new TimeoutException(StringConsts.ARGUMENT_ERROR + " Bad hour list syntax, out of order, or overlap in `{0}`".Args(Data.TakeFirstChars(64)));
+        return m_Spans;
+      }
+    }
 
 
+    /// <summary>
+    /// Returns true if the time component of the specified DateTime is covered(included)
+    /// by one of the HourList spans
+    /// </summary>
+    public bool IsCovered(DateTime when)
+    {
+      var ts = (int)when.TimeOfDay.TotalMinutes;
+      return Spans.Any(s => s.StartMinute <= ts && s.FinishMinute >= ts);
+    }
 
 
-//  }
-//}
+    public override bool Equals(object obj) => obj is HourList other ? this.Equals(other) : false;
+    public override int GetHashCode() => Data==null ? 0 : Data.GetHashCode();
+    public override string ToString() => Data.Default("<unassigned>");
+    public bool Equals(HourList other) => this.Data.EqualsOrdSenseCase(other.Data);
+
+    /// <summary>
+    /// Returns true when two instances contain representations of identical spans or both are unassigned.
+    /// Throws is one of instances contain invalid data
+    /// </summary>
+    public bool IsEquivalent(HourList other) => this.IsAssigned==other.IsAssigned && (!IsAssigned || this.Spans.SequenceEqual(other.Spans));
+
+
+    public void WriteAsJson(TextWriter wri, int nestingLevel, JsonWritingOptions options = null)
+     => JsonWriter.EncodeString(wri, Data, options);
+
+    public (bool match, IJsonReadable self) ReadAsJson(object data, bool fromUI, JsonReader.DocReadOptions? options)
+    {
+      if (data != null) return (true, new HourList(data.ToString()));
+      return (false, null);
+    }
+
+
+    // 8:00-12:00,12:30pm-4:45pm,21:00-22:15
+    // 8-1:30pm;14-18.5;23-2
+    private static Span[] parse(string content)
+    {
+      if (content.IsNullOrWhiteSpace()) return null;
+
+      var kvps = content.Split(DELIMS)
+                      .Where(p => p.IsNotNullOrWhiteSpace())
+                      .Select(p => p.SplitKVP('-'));
+
+      var data = new List<Span>();
+      foreach (var kvp in kvps)
+      {
+        var rawStart = parseComponent(kvp.Key);
+        if (rawStart < 0) return null;
+        var rawFinish = parseComponent(kvp.Value);
+        if (rawFinish < 0) return null;
+        var duration = rawFinish >= rawStart ? rawFinish - rawStart : (MINUTES_PER_DAY - rawStart) + rawFinish;
+        var span = new Span(rawStart, duration);
+        data.Add(span);
+      }
+
+      //order it time
+      var result = data.OrderBy(s => s.StartMinute).ToArray();
+
+      //check for overlap
+      var start = 0;
+      foreach (var span in result)
+        if (span.StartMinute < start)
+          return null;
+        else
+          start = span.FinishMinute;
+
+      return result;
+    }
+
+    // 8:00    12:00  12:30pm 4:45pm 22:15
+    // 8       12pm 14   18.5  2
+    private static int parseComponent(string str)
+    {
+      if (str.IsNullOrWhiteSpace()) return -1;
+      str = str.Trim();
+      var result = 0;
+      if (str.EndsWith("am", StringComparison.InvariantCultureIgnoreCase))
+      {
+        str = str.Substring(0, str.Length - 2);
+        result = parseMinutes(str);
+      }
+      else if(str.EndsWith("pm", StringComparison.InvariantCultureIgnoreCase))
+      {
+        str = str.Substring(0, str.Length - 2);
+        result = parseMinutes(str);
+        if (result>=0) result += (12 * 60);
+      }
+      else result = parseMinutes(str);
+
+      return result;
+    }
+
+    private static int parseMinutes(string str)
+    {
+      if (str.IsNullOrWhiteSpace()) return -1;
+      var i = str.IndexOf(':');
+      if (i<0)//decimal
+      {
+        if (double.TryParse(str, out var result)) return (int)(result * 60);
+        return -1;
+      }
+      if (i == 0 || i == str.Length) return -1;
+
+      if (!int.TryParse(str.Substring(0, i), out var h)) return -1;
+      if (!int.TryParse(str.Substring(i+1), out var m)) return -1;
+      if (h < 0 || h > 23) return -1;
+      if (m < 0 || m > 59) return -1;
+
+      return (h * 60) + m;
+    }
+
+
+  }
+}
---
+++ src/testing/Azos.Tests.Nub/Time/HourListTests.cs
@@ -0,0 +1,201 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+
+using Azos.Data;
+using Azos.Time;
+using Azos.Scripting;
+using Azos.Serialization.JSON;
+using System.Linq;
+
+namespace Azos.Tests.Nub.Time
+{
+  [Runnable]
+  public class HourListTests
+  {
+    [Run]
+    public void Unassigned()
+    {
+      var x = new HourList();
+      Aver.IsFalse(x.IsAssigned);
+    }
+
+    [Run]
+    public void Test01()
+    {
+      var got = new HourList("1-2");
+      Aver.AreEqual(1, got.Spans.Count());
+      Aver.AreEqual(60, got.Spans.First().StartMinute);
+      Aver.AreEqual(60, got.Spans.First().DurationMinutes);
+    }
+
+    [Run]
+    public void Test02()
+    {
+      var got = new HourList("1-2.25");
+      Aver.AreEqual(1, got.Spans.Count());
+      Aver.AreEqual(60, got.Spans.First().StartMinute);
+      Aver.AreEqual(75, got.Spans.First().DurationMinutes);
+    }
+
+    [Run]
+    public void Test03()
+    {
+      var got = new HourList("1-2:15");
+      Aver.AreEqual(1, got.Spans.Count());
+      Aver.AreEqual(60, got.Spans.First().StartMinute);
+      Aver.AreEqual(75, got.Spans.First().DurationMinutes);
+    }
+
+    [Run]
+    public void Test04()
+    {
+      var got = new HourList("1am-2:15am");
+      Aver.AreEqual(1, got.Spans.Count());
+      Aver.AreEqual(60, got.Spans.First().StartMinute);
+      Aver.AreEqual(75, got.Spans.First().DurationMinutes);
+    }
+
+    [Run]
+    public void Test05()
+    {
+      var got = new HourList(" 1:00am  - 2:15");
+      Aver.AreEqual(1, got.Spans.Count());
+      Aver.AreEqual(60, got.Spans.First().StartMinute);
+      Aver.AreEqual(75, got.Spans.First().DurationMinutes);
+    }
+
+    [Run]
+    public void Test06()
+    {
+      var got = new HourList(" 1.00      -     2.25am        ");
+      Aver.AreEqual(1, got.Spans.Count());
+      Aver.AreEqual(60, got.Spans.First().StartMinute);
+      Aver.AreEqual(75, got.Spans.First().DurationMinutes);
+    }
+
+    [Run]
+    public void Test07()
+    {
+      var got = new HourList(" 1.00      -     2.25am        ");
+      Aver.AreEqual(1, got.Spans.Count());
+      var span = got.Spans.First();
+
+      "{0} {1}".SeeArgs(span.Start, span.Finish);
+      Aver.AreEqual("1:00", span.Start);
+      Aver.AreEqual("2:15", span.Finish);
+    }
+
+    [Run]
+    public void Test08()
+    {
+      var got = new HourList(" 1.00 - 23 ");
+      Aver.AreEqual(1, got.Spans.Count());
+      var span = got.Spans.First();
+      Aver.AreEqual(60, span.StartMinute);
+      Aver.AreEqual(22 * 60, span.DurationMinutes);
+
+      "{0} {1}".SeeArgs(span.Start, span.Finish);
+      Aver.AreEqual("1:00", span.Start);
+      Aver.AreEqual("23:00", span.Finish);
+    }
+
+    [Run]
+    public void Test09()
+    {
+      var got = new HourList("1-14");
+      Aver.AreEqual(1, got.Spans.Count());
+      var span = got.Spans.First();
+      Aver.AreEqual(60, span.StartMinute);
+      Aver.AreEqual(13 * 60, span.DurationMinutes);
+
+      Aver.IsFalse(got.IsCovered(new DateTime(1980, 1, 1,   0,  0, 0)));
+      Aver.IsFalse(got.IsCovered(new DateTime(1980, 1, 1,   0, 59, 59)));
+      Aver.IsTrue(got.IsCovered( new DateTime(1980, 1, 1,   1,  0, 0)));
+      Aver.IsTrue(got.IsCovered( new DateTime(1980, 1, 1,   13,59, 59)));
+      Aver.IsTrue(got.IsCovered( new DateTime(1980, 1, 1,   14, 0, 0)));
+      Aver.IsFalse(got.IsCovered(new DateTime(1980, 1, 1,   14, 1, 0)));
+      Aver.IsFalse(got.IsCovered(new DateTime(1980, 1, 1,   15, 1, 0)));
+    }
+
+    [Run]
+    public void Test10()
+    {
+      var got = new HourList("1-14");
+      Aver.AreEqual(1, got.Spans.Count());
+      var got2 = new HourList("1am-2pm");
+      Aver.IsFalse(got.Equals(got2));
+      Aver.AreNotEqual(got.GetHashCode(), got2.GetHashCode());
+      Aver.IsTrue(got.IsEquivalent(got2));
+    }
+
+    [Run]
+    public void Test11()
+    {
+      var got = new HourList("1-14");
+      Aver.AreEqual(1, got.Spans.Count());
+      var got2 = new HourList("1-14");
+      Aver.IsTrue(got.Equals(got2));
+      Aver.AreEqual(got.GetHashCode(), got2.GetHashCode());
+      Aver.IsTrue(got.IsEquivalent(got2));
+    }
+
+    [Run]
+    public void Test12()
+    {
+      var got = new HourList("1-14, 2:30pm-7:15pm, 23-24");
+      Aver.AreEqual(3, got.Spans.Count());
+      var got2 = new HourList("1:00am-2:00pm; 14.5-19.25; 11pm-24");
+      Aver.AreEqual(3, got.Spans.Count());
+      Aver.IsFalse(got.Equals(got2));
+      Aver.AreNotEqual(got.GetHashCode(), got2.GetHashCode());
+      Aver.IsTrue(got.IsEquivalent(got2));
+      "{0}   {1}".SeeArgs(got, got2);
+    }
+
+    [Run]
+    public void Test13()
+    {
+      var got = new HourList("23-12pm");
+      Aver.AreEqual(1, got.Spans.Count());
+      Aver.AreEqual(60*23, got.Spans.First().StartMinute);
+      Aver.AreEqual(60, got.Spans.First().DurationMinutes);
+      got.See();
+    }
+
+    [Run]
+    public void Test14()
+    {
+      var got = new HourList("23-1am");
+      Aver.AreEqual(1, got.Spans.Count());
+      Aver.AreEqual(60 * 23, got.Spans.First().StartMinute);
+      Aver.AreEqual(2*60, got.Spans.First().DurationMinutes);
+      got.See();
+    }
+
+    [Run]
+    public void Test15()
+    {
+      var got = new HourList("8-12, 12:30-5pm, 23-1:12am");
+      Aver.AreEqual(3, got.Spans.Count());
+      Aver.AreEqual(8 * 60, got.Spans.First().StartMinute);
+      Aver.AreEqual(4 * 60, got.Spans.First().DurationMinutes);
+
+      Aver.AreEqual(12 * 60 + 30, got.Spans.Skip(1).First().StartMinute);
+      Aver.AreEqual(4 * 60 + 30, got.Spans.Skip(1).First().DurationMinutes);
+
+      Aver.AreEqual(23 * 60, got.Spans.Skip(2).First().StartMinute);
+      Aver.AreEqual(2 * 60 + 12, got.Spans.Skip(2).First().DurationMinutes);
+
+      got.See();
+    }
+
+
+    //todo:  parse, try parse,  exceptions, JSOPn serialization, refactor exception constant
+
+  }
+}
