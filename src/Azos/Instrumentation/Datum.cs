c2131df305142c82870328c737ae1a1cc7c66c8c
Dmitriy Khmaladze <d@zhabis.com>
2020-06-13 18:56:33 -0400

    Add ArchiveConventions and tests

--- src/Azos/Instrumentation/Datum.cs
+++ src/Azos/Instrumentation/Datum.cs
@@ -21,8 +21,6 @@ namespace Azos.Instrumentation
   {
     #region CONST
     public const string UNSPECIFIED_SOURCE = "*";
-    public const string FRAMEWORK_SOURCE = "Frmwrk";
-    public const string BUSINESS_SOURCE = "BsnsLgc";
     #endregion
 
     /// <summary>
@@ -34,48 +32,60 @@ namespace Azos.Instrumentation
     #region .ctor
     protected Datum()
     {
-      m_UTCTime = Ambient.UTCNow;
+      m_StartUtc = Ambient.UTCNow;
     }
 
     protected Datum(string source)
     {
       m_Source = source;
-      m_UTCTime = Ambient.UTCNow;
+      m_StartUtc = Ambient.UTCNow;
     }
 
     protected Datum(string source, DateTime utcDateTime)
     {
       m_Source = source;
-      m_UTCTime = utcDateTime;
+      m_StartUtc = utcDateTime;
     }
     #endregion
 
     #region Fields
+    private GDID m_Gdid;
     private string m_Source;
     private int m_Count;
-    private DateTime m_UTCTime;
-    private DateTime m_UTCEndTime;
+    private DateTime m_StartUtc;
+    private DateTime m_EndUtc;
     #endregion
 
     #region Properties
+
+    /// <summary>
+    /// Global distributed ID used by distributed log warehouses. GDID.ZERO for local logging applications
+    /// </summary>
+    [Field, Field(isArow: true, backendName: "gdid")]
+    public GDID Gdid
+    {
+      get => m_Gdid;
+      set => m_Gdid = value;
+    }
+
     /// <summary>
     /// Returns UTC time stamp when event happened
     /// </summary>
     [Field, Field(isArow: true, backendName: "sts")]
-    public DateTime UTCTime
+    public DateTime StartUtc
     {
-      get => m_UTCTime;
-      protected set => m_UTCTime = value;
+      get => m_StartUtc;
+      protected set => m_StartUtc = value;
     }
 
     /// <summary>
     /// Returns UTC time stamp when event happened. This property may be gotten only if IsAggregated==true, otherwise UTCTime value is returned
     /// </summary>
     [Field, Field(isArow: true, backendName: "ets")]
-    public DateTime UTCEndTime
+    public DateTime EndUtc
     {
-      get => m_Count == 0 ? m_UTCTime : m_UTCEndTime;
-      protected set => m_UTCEndTime = value;
+      get => m_Count == 0 ? m_StartUtc : m_EndUtc;
+      protected set => m_EndUtc = value;
     }
 
 
@@ -105,6 +115,11 @@ namespace Azos.Instrumentation
     }
 
     /// <summary>
+    /// Returns archive dimensions vector which by default short-circuits to Source
+    /// </summary>
+    public virtual string ArchiveDimensions => Source;
+
+    /// <summary>
     /// True if this instance represent an unspecified source
     /// </summary>
     public bool IsUnspecifiedSource => IsUnspecifiedSourceString(Source);
@@ -118,17 +133,13 @@ namespace Azos.Instrumentation
       {
         if (m_Count <= 0) return CoreConsts.UNKNOWN;
 
-        var span = m_UTCEndTime - m_UTCTime;
+        var span = m_EndUtc - m_StartUtc;
 
         if (m_Count == 1 && span.TotalMilliseconds < 0.1) return string.Empty;
 
         var rate = m_Count / span.TotalSeconds;
 
-        if (rate > 1.0)
-          return string.Format("{0:0.00}/sec.", rate);
-
-        rate = rate * 1000;
-        return string.Format("{0:0.00}/msec.", rate);
+        return Math.Abs(rate) > 1.0 ? "{0:0.00}/sec.".Args(rate) : "{0:0.00}/msec.".Args(1000 * rate);
       }
     }
 
@@ -189,14 +200,14 @@ namespace Azos.Instrumentation
       foreach (var e in many)
       {
         cnt++;
-        if (e.UTCTime < start) start = e.UTCTime;
-        if (e.UTCTime > end) end = e.UTCTime;
+        if (e.StartUtc < start) start = e.StartUtc;
+        if (e.StartUtc > end) end = e.StartUtc;
         result.AggregateEvent(e);
       }
 
       result.m_Count = cnt;
-      result.m_UTCTime = start;
-      result.m_UTCEndTime = end;
+      result.m_StartUtc = start;
+      result.m_EndUtc = end;
       result.SummarizeAggregation();
 
       return result;
@@ -217,7 +228,7 @@ namespace Azos.Instrumentation
     /// </summary>
     public virtual void ReduceSourceDetail(int reductionLevel)
     {
-      if (reductionLevel < 0) m_Source = string.Empty;
+      if (reductionLevel < 0) m_Source = null;
     }
 
     public override string ToString()
---
+++ src/Azos/Log/ArchiveConventions.cs
@@ -0,0 +1,69 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+
+using Azos.Serialization.JSON;
+
+namespace Azos.Log
+{
+  /// <summary>
+  /// Provides helper methods for encoding and decoding ArchiveDimensions string in a terse JSON format -
+  /// a form of a convention of ArchiveDimensions use with IArchiveLoggable
+  /// </summary>
+  public static class ArchiveConventions
+  {
+    private static readonly JsonWritingOptions JSON_FORMAT = new JsonWritingOptions(JsonWritingOptions.CompactRowsAsMap)
+    {
+      MapSkipNulls =true,
+      ISODates = true
+    };
+
+    /// <summary>
+    /// Used as a convention identifier.
+    /// By this convention, all archive dimensions data must start with a hashbang: #!ad\n,
+    /// otherwise the decode function will not parse it and return null
+    /// </summary>
+    public const string AD_HASHBANG = "#!ad\n";
+
+    /// <summary>
+    /// Returns a JsonDataMap parsed out of IArchiveLoggable.ArchiveDimensions content when it is set,
+    /// or null. The function returns null for content which does not start with AD_HASHBANG
+    /// which represents this convention. Throws on invalid JSON
+    /// </summary>
+    public static JsonDataMap DecodeArchiveDimensionsMap(this IArchiveLoggable entity)
+    {
+      if (entity==null) return null;
+      var ad = entity.ArchiveDimensions;
+      return DecodeArchiveDimensionsMap(ad);
+    }
+
+    /// <summary>
+    /// Returns a JsonDataMap parsed out of ArchiveDimensions content when it is set,
+    /// or null. The function returns null for content which does not start with AD_HASHBANG
+    /// which represents this convention. Throws on invalid JSON
+    /// </summary>
+    public static JsonDataMap DecodeArchiveDimensionsMap(string archiveDimensions)
+    {
+      if (archiveDimensions.IsNullOrWhiteSpace()) return null;
+      if (!archiveDimensions.StartsWith(AD_HASHBANG, StringComparison.Ordinal)) return null;
+      var result = archiveDimensions.JsonToDataObject(true) as JsonDataMap;
+      return result;
+    }
+
+    /// <summary>
+    /// Returns a terse json object encoding into string, or null if supplied argument is null.
+    /// The encoded string is prefixed with AD_HASHBANG to mark the content as the one
+    /// encoded with this convention
+    /// </summary>
+    public static string EncodeArchiveDimensions(object archiveDimensions)
+    {
+      if (archiveDimensions == null) return null;
+      return AD_HASHBANG + archiveDimensions.ToJson(JSON_FORMAT);
+    }
+
+  }
+}
