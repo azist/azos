8bab43b93d19cd00f3c61590baf92df3058da226
Dmitriy Khmaladze <d@zhabis.com>
2020-08-03 19:07:19 -0400

    Nuget 1.4.11 push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=1.4.10
-set HEADLINE=Add IPatternStrategyTrait to Azos.Apps.Strategies; add GdidAccessor for agm
+set VER=1.4.11
+set HEADLINE=Log Chronicles RTM
 call vs2017-build-release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
