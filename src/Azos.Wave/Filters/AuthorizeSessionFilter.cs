fe6892b2f16793094abd711e56efbe96aa5fa698
Dmitriy Khmaladze <d@zhabis.com>
2020-08-16 20:35:44 -0400

    WIP #343

--- src/Azos.Wave/Filters/AuthorizeSessionFilter.cs
+++ src/Azos.Wave/Filters/AuthorizeSessionFilter.cs
@@ -13,7 +13,8 @@ namespace Azos.Wave.Filters
 {
   /// <summary>
   /// Handles Authorization header with Basic and Bearer schemes creating IDPasswordCredentials or BearerCredentials respectively.
-  /// Optionally performs injection of session.DataContextName from "wv-data-ctx" header (if present)
+  /// Optionally performs injection of session.DataContextName from "wv-data-ctx" header (if present).
+  /// This filter is typically used for API server development
   /// </summary>
   public sealed class AuthorizeSessionFilter : SessionFilter
   {
@@ -37,6 +38,13 @@ namespace Azos.Wave.Filters
     /// </summary>
     [Config]
     public string AltAuthorizationHeader { get ; set;}
+
+
+    /// <summary>
+    /// When set will bump the named Gate var on every bad auth request which did not produce a Valid user
+    /// </summary>
+    [Config]
+    public string GateBadAuthVar{ get; set; }
 
 
     //disregard onlyExisting parameter, for APIs the session context is ephemeral
@@ -111,8 +119,23 @@ namespace Azos.Wave.Filters
       if (credentials==null)
         throw HTTPStatusException.BadRequest_400("Bad [Authorization] header");
 
-      session.User = App.SecurityManager.Authenticate(credentials);//authenticate the user
-      work.SetAuthenticated(session.User.IsAuthenticated);
+      var user = App.SecurityManager.Authenticate(credentials);//authenticate the user
+      session.User = user;
+      work.SetAuthenticated(user.IsAuthenticated);
+
+      //gate traffic
+      if (!user.IsAuthenticated && NetGate!=null && NetGate.Enabled)
+      {
+        var vn = GateBadAuthVar;
+        if (vn.IsNotNullOrWhiteSpace())
+        {
+          NetGate.IncreaseVariable(IO.Net.Gate.TrafficDirection.Incoming,
+                                     work.EffectiveCallerIPEndPoint.Address.ToString(),
+                                     vn,
+                                     1);
+        }
+      }
+
       return session;
     }
 
