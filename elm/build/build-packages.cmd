4ccc8bee235c601ebc18d94573273bc47f4f5ff8
Dmitriy Khmaladze <d@zhabis.com>
2020-10-21 12:41:59 -0400

    Nuget 1.5.1

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=1.5.0
-set HEADLINE=Optional DI, Configure await 
+set VER=1.5.1
+set HEADLINE=Service release 
 call vs2017-build-release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
