96bda735693ddf086540a3cb7d4d0c9df9f966be
Dmitriy Khmaladze <d@zhabis.com>
2021-02-08 15:16:59 -0500

    Nuget 16.1 push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=1.6.0
-set HEADLINE=Log archiving, DistributedCallFlow handling
+set VER=1.6.1
+set HEADLINE=Fixes, Log archiving, DistributedCallFlow handling
 call vs2017-build-release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
