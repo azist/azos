ed7f6626eb08e27383268629ad32fc897cf75a72
Dmitriy Khmaladze <d@zhabis.com>
2021-03-19 16:36:34 -0400

    NUget fix

--- src/Azos/Security/AuthenticationRequestContext.cs
+++ src/Azos/Security/AuthenticationRequestContext.cs
@@ -5,7 +5,10 @@
 </FILE_LICENSE>*/
 
 using System;
+using System.Collections.Generic;
 using Azos.Data;
+using Azos.Serialization.Bix;
+using Azos.Serialization.JSON;
 
 namespace Azos.Security
 {
@@ -16,6 +19,7 @@ namespace Azos.Security
   /// services may elect to interpret the supplied context object in various ways,
   /// for example: generate sys tokens with longer lifespan for OAuth requestors
   /// </summary>
+  [BixJsonHandler]
   public abstract class AuthenticationRequestContext : TypedDoc/*must be easily serializable hence DataDoc*/
   {
     /// <summary>
@@ -23,12 +27,23 @@ namespace Azos.Security
     /// </summary>
     [Field]
     public string RequestDescription { get; set; }
+
+    protected override void AddJsonSerializerField(Schema.FieldDef def, JsonWritingOptions options, Dictionary<string, object> jsonMap, string name, object value)
+    {
+      if (def?.Order == 0)
+      {
+        BixJsonHandler.EmitJsonBixDiscriminator(this, jsonMap);
+      }
+
+      base.AddJsonSerializerField(def, options, jsonMap, name, value);
+    }
   }
 
 
   /// <summary>
   /// Denotes requests related to OAuth subject (target impersonated user) authentication
   /// </summary>
+  [Bix("5ADDB87A-FB26-4843-97C9-8A549594DB36")]
   public class OAuthSubjectAuthenticationRequestContext : AuthenticationRequestContext
   {
     [Field]
