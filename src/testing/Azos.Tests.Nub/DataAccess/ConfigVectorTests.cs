1cd070ab8fe8c79a48faabc288c260bccdcdc934
Dmitriy Khmaladze <d@zhabis.com>
2021-03-29 20:12:24 -0400

    FIX #456

--- src/testing/Azos.Tests.Nub/DataAccess/ConfigVectorTests.cs
+++ src/testing/Azos.Tests.Nub/DataAccess/ConfigVectorTests.cs
@@ -30,7 +30,7 @@ namespace Azos.Tests.Nub.DataAccess
       [Field(required: true)]
       public ConfigVector C1{ get; set;}
 
-      [Field(maxLength: 1024)]
+      [Field(maxLength: 32)]
       public ConfigVector C2 { get; set; }
     }
 
@@ -73,6 +73,60 @@ namespace Azos.Tests.Nub.DataAccess
 
       got.C1.Node.Of("a");
     }
+
+
+    [Run]
+    public void Test_Long()
+    {
+      var json = "{C1: '{a: {}}', C2: '{r: {m: \"very long message which is longer than 32 characters\"}}'}";
+
+      var got = JsonReader.ToDoc<Tezt>(json);
+
+      Aver.AreEqual("very long message which is longer than 32 characters", got.C2.Node.ValOf("m"));
+
+      var ve = got.Validate() as FieldValidationException;
+      Aver.IsNotNull(ve);
+      Aver.AreEqual("C2", ve.FieldName);
+      Aver.IsTrue(ve.Message.Contains("exceeds"));
+      ve.See();
+    }
+
+    [Run]
+    public void Test_FullCycle()
+    {
+      var d = new Tezt
+      {
+        C1 = "{r: {a:1, b:2}}",
+        C2 = "{r: {a:-1, b:-2}}"
+      };
+
+      var json = JsonWriter.Write(d, JsonWritingOptions.PrettyPrintRowsAsMap);
+
+      json.See();
+
+      var got = JsonReader.ToDoc<Tezt>(json);
+
+      var n1 = got.C1.Node;
+      Aver.AreEqual(1, got.C1.Node.Of("a").ValueAsInt());
+      Aver.AreEqual(2, got.C1.Node.Of("b").ValueAsInt());
+      Aver.AreSameRef(n1, got.C1.Node);
+
+      got.C1.Content = "{r: {a:10, b:20}}";
+      var n2 = got.C1.Node;
+      Aver.AreNotSameRef(n1, n2);
+      Aver.AreSameRef(n2, got.C1.Node);
+
+      got.C1.Node = "r{z=900}".AsLaconicConfig();
+      var n3 = got.C1.Node;
+      Aver.AreNotSameRef(n2, n3);
+      Aver.AreEqual(900, got.C1.Node.Of("z").ValueAsInt());
+
+      Aver.AreEqual(-1, got.C2.Node.Of("a").ValueAsInt());
+      Aver.AreEqual(-2, got.C2.Node.Of("b").ValueAsInt());
+
+      got.See();
+    }
+
 
   }
 }
