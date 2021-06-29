967b33b7a46b07e8ae54eedcb8f996fe6a07e539
Dmitriy Khmaladze <d@zhabis.com>
2021-06-28 22:25:15 -0400

    Refactoring data access, add generic InstrumentedCrudQueryHandler

--- src/Azos/Data/Access/CRUDQueryHandler.cs
+++ src/Azos/Data/Access/CRUDQueryHandler.cs
@@ -4,12 +4,16 @@
  * See the LICENSE file in the project root for more information.
 </FILE_LICENSE>*/
 
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Azos.Instrumentation;
+using Azos.Time;
 
 namespace Azos.Data.Access
 {
   /// <summary>
-  /// Represents an entity that can execute a query. The implementation may be called by multiple threads and must be safe
+  /// Represents an entity that can execute a query. The implementation may be called by multiple threads and must be thread-safe
   /// </summary>
   public abstract class CRUDQueryHandler : Collections.INamed
   {
@@ -81,7 +85,6 @@ namespace Azos.Data.Access
     public abstract Task<int> ExecuteWithoutFetchAsync(ICRUDQueryExecutionContext context, Query query);
   }
 
-
   /// <summary>
   /// Represents an entity that can execute a query. The implementation may be called by multiple threads and must be safe
   /// </summary>
@@ -94,5 +97,124 @@ namespace Azos.Data.Access
     /// Store instance that handler is under
     /// </summary>
     public new TStore Store => (TStore)base.Store;
+  }
+
+  /// <summary>
+  /// Represents an entity that can execute a query. The implementation may be called by multiple threads and must be safe.
+  /// Adds instrumentation logic, generally providers should derive their implementations from this base,
+  /// </summary>
+  /// <remarks>
+  /// The handler is purposed for ASYNC-first use, having its sync methods short circuit to async versions.
+  /// Override async methods while implementing logic
+  /// </remarks>
+  public abstract class InstrumentedCrudQueryHandler<TStore, TStoreContext> : CRUDQueryHandler<TStore>
+                          where TStore : ICRUDDataStore
+                          where TStoreContext : ICRUDQueryExecutionContext
+  {
+    /// <summary>
+    /// Added to queries when the schema of supported AST (Abstract Syntax Tree) should be returned instead of schema of data itself.
+    /// Used for advanced filters with AST
+    /// </summary>
+    public const string AST_REFLECTION_QUERY_PARAM = "_____AST_REFLECTION_QUERY_PARAM";
+
+    public InstrumentedCrudQueryHandler(TStore store, string name) : base(store, name) { }
+
+    protected IInstrumentation Instruments => Store.App.Instrumentation;
+    protected bool Instrumented => Instruments.Enabled && ((Store as IInstrumentable)?.InstrumentationEnabled ?? false);
+
+    protected T Profile<T>(Func<T> body)
+    {
+      var sw = Timeter.StartNew();
+      var result = body();
+      if (Instrumented)
+      {
+        var el = sw.ElapsedMs;
+        Instrumentation.DataQueryLatency.Emit(Instruments, Store.TargetName, GetType().Name, el);
+      }
+      return result;
+    }
+
+    protected async Task<T> ProfileAsync<T>(Func<Task<T>> body)
+    {
+      var sw = Timeter.StartNew();
+      var result = await body();
+      if (Instrumented)
+      {
+        var el = sw.ElapsedMs;
+        Instrumentation.DataQueryLatency.Emit(Instruments, Store.TargetName, GetType().Name, el);
+      }
+      return result;
+    }
+
+    public override RowsetBase Execute(ICRUDQueryExecutionContext context, Query query, bool oneRow = false)
+      => Profile(() => ExecuteAsync(context, query, oneRow).GetAwaiter().GetResult());
+
+    public sealed override async Task<RowsetBase> ExecuteAsync(ICRUDQueryExecutionContext context, Query query, bool oneRow = false)
+      => await ProfileAsync(() => ExecuteAsync((TStoreContext)context, query, oneRow));
+
+    public virtual Task<RowsetBase> ExecuteAsync(TStoreContext context, Query query, bool oneRow = false)
+      => throw new NotImplementedException($"{this.GetType().FullName}.{nameof(ExecuteAsync)}");
+
+
+    public override Cursor OpenCursor(ICRUDQueryExecutionContext context, Query query)
+     => Profile(() => OpenCursorAsync(context, query).GetAwaiter().GetResult());
+
+    public sealed override async Task<Cursor> OpenCursorAsync(ICRUDQueryExecutionContext context, Query query)
+     => await ProfileAsync(() => OpenCursorAsync((TStoreContext)context, query));
+
+    public virtual Task<Cursor> OpenCursorAsync(TStoreContext context, Query query)
+      => throw new NotImplementedException($"{this.GetType().FullName}.{nameof(OpenCursorAsync)}");
+
+
+
+    public override int ExecuteWithoutFetch(ICRUDQueryExecutionContext context, Query query)
+      => Profile(() => ExecuteWithoutFetchAsync(context, query).GetAwaiter().GetResult());
+
+    public sealed override async Task<int> ExecuteWithoutFetchAsync(ICRUDQueryExecutionContext context, Query query)
+      => await ProfileAsync(() => ExecuteWithoutFetchAsync((TStoreContext)context, query));
+
+    public virtual Task<int> ExecuteWithoutFetchAsync(TStoreContext context, Query query)
+      => throw new NotImplementedException($"{this.GetType().FullName}.{nameof(ExecuteWithoutFetchAsync)}");
+
+
+
+    public override Schema GetSchema(ICRUDQueryExecutionContext context, Query query)
+      => Profile(() => GetSchemaAsync(context, query).GetAwaiter().GetResult());
+
+    public sealed override async Task<Schema> GetSchemaAsync(ICRUDQueryExecutionContext context, Query query)
+     => await ProfileAsync(() => GetSchemaAsync((TStoreContext)context, query));
+
+    /// <summary>
+    /// Default implementation looks for AST_REFLECTION_QUERY_PARAM, and if it is present, then fetches schema from
+    /// the AST not query, otherwise gets schema from query itself
+    /// </summary>
+    public virtual async Task<Schema> GetSchemaAsync(TStoreContext context, Query query)
+    {
+      if (query.Any(p => p.Name.EqualsOrdSenseCase(AST_REFLECTION_QUERY_PARAM)))
+      {
+        var astSchema = GetAstReflectionSchema(context, query);
+        return astSchema;
+      }
+
+      var result = await GetQuerySchemaAsync(context, query);
+      return result;
+    }
+
+    /// <summary>
+    /// Override to get schema from query. Default implementation throws "NotImplemented" exception
+    /// </summary>
+    public virtual Task<Schema> GetQuerySchemaAsync(TStoreContext context, Query query)
+    {
+      throw new NotSupportedException($"{this.GetType().FullName}.{nameof(GetSchemaAsync)}");
+    }
+
+    /// <summary>
+    /// Override to reflect on AST. ASTs are used for complex ad hoc expressions, so you can specify
+    /// what fields an AST supports
+    /// </summary>
+    protected virtual Schema GetAstReflectionSchema(TStoreContext context, Query query)
+    {
+      throw new NotSupportedException($"{this.GetType().FullName}.{nameof(GetAstReflectionSchema)}");
+    }
   }
 }
---
+++ src/Azos/Data/Access/Instrumentation/DataQueryLatency.cs
@@ -0,0 +1,42 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+
+using Azos.Instrumentation;
+using Azos.Serialization.Bix;
+
+namespace Azos.Data.Access.Instrumentation
+{
+  /// <summary>
+  /// Measures data query execution times
+  /// </summary>
+  [Serializable, Bix("5EF35033-44FE-4D7B-9F15-D479EE94D20C")]
+  public sealed class DataQueryLatency : LongGauge, INetInstrument, IDatabaseInstrument
+  {
+    /// <summary>
+    /// Emits the latency reading
+    /// </summary>
+    public static void Emit(IInstrumentation inst, string ctxName, string queryName, long msLatency, string queryParam = null)
+    {
+      if (inst == null || !inst.Enabled) return;
+      var src = $"{ctxName}::{queryName}::{queryParam}";
+
+      var datum = new DataQueryLatency(src, msLatency);
+      inst.Record(datum);
+
+      datum = new DataQueryLatency(UNSPECIFIED_SOURCE, msLatency);//general
+      inst.Record(datum);
+    }
+
+    internal DataQueryLatency(string src, long value) : base(src, value) { }
+
+    public override string Description => "Measures data query execution time";
+    public override string ValueUnitName => CoreConsts.UNIT_NAME_MSEC;
+
+    protected override Datum MakeAggregateInstance() => new DataQueryLatency(Source, 0);
+  }
+}
