a77e1de7bc89921d2e369be2614251f6e7aac1f6
Dmitriy Khmaladze <d@zhabis.com>
2021-07-25 14:25:42 -0400

    Nuget 1.20.5.1 Push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=1.20.4
-set HEADLINE=EventHub in progress
+set VER=1.20.5.1
+set HEADLINE=EventHub pre-release
 call vs2017-build-release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
