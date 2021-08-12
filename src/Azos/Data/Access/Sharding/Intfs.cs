56ae44f621895c81f6fff4d0734f9438c88aa1dc
Dmitriy Khmaladze <d@zhabis.com>
2021-08-12 19:38:04 -0400

    wip #535 - add Sharding namespace

---
+++ src/Azos/Data/Access/Sharding/Intfs.cs
@@ -0,0 +1,90 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using Azos.Conf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azos.Data.Access.Sharding
+{
+  /// <summary>
+  /// Represents data store which has a concept of data partitioning between shards.
+  /// The shards are routed-into by <see cref="ShardKey"/>
+  /// </summary>
+  public interface IShardedCrudDataStore : IDataStore
+  {
+    /// <summary>
+    /// Gets all shards in the current set
+    /// </summary>
+    IEnumerable<IShard> CurrentSet {  get; }
+
+    /// <summary>
+    /// Gets <see cref="CrudOperations"/> for the shard routed to
+    /// using the specified <see cref="ShardKey"/>
+    /// </summary>
+    CrudOperations GetOperationsFor(ShardKey key);
+  }
+
+  public interface IShardedCrudDataStoreImplementation : IShardedCrudDataStore
+  {
+    /// <summary>
+    /// Physical store which services the shard set
+    /// </summary>
+    ICrudDataStore PhysicalStore { get; }
+
+    /// <summary>
+    /// Factory method which makes appropriate instance of <see cref="CrudOperationCallContext"/>
+    /// or its derivative class, suitable for passing into <see cref="PhysicalStore"/> for the
+    /// specified shard instance
+    /// </summary>
+    CrudOperationCallContext MakeCallContext(IShard shard);
+  }
+
+  /// <summary>
+  /// Represents a shard (a partition) of the data in shard set
+  /// </summary>
+  public interface IShard
+  {
+    /// <summary>
+    /// Data store which this shard is a part of
+    /// </summary>
+    IShardedCrudDataStoreImplementation Store { get; }
+
+    /// <summary>
+    /// The immutable unique ID assigned to a shard node within a set.
+    /// The ID determines the consistent shard traffic routing, so be careful not to change
+    /// the id once assigned. It is recommended to use all 8 bytes of the ID for better probability
+    /// distribution
+    /// </summary>
+    ulong ShardId { get; }
+
+    /// <summary>
+    /// Shard weight controls relative weighting of this shard in a set.
+    /// Value of 1 represents the normal (default) weight multiplier.
+    /// Changing this parameters changes data distribution
+    /// </summary>
+    double ShardWeight { get; }
+
+    /// <summary>
+    /// Returns Route for the destination, such as database connect string, node config etc..
+    /// This is where the traffic gets routed into.
+    /// Note: depending on physical store, this can represent logical connection name (such as cluster db instance name)
+    /// which gets translated to physical database connection address later
+    /// </summary>
+    string RouteConnectString { get; }
+
+    /// <summary>
+    /// Used to override store's default database name - used by some stores, others take db name from the connect string
+    /// </summary>
+    string RouteDatabaseName { get; set; }
+
+    /// <summary>
+    /// Operations for this shard
+    /// </summary>
+    CrudOperations Operations {  get; }
+  }
+}
---
+++ src/Azos/Data/Access/Sharding/ShardedDataContext.cs
@@ -0,0 +1,138 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Azos.Apps;
+using Azos.Collections;
+using Azos.Conf;
+using Azos.Instrumentation;
+
+namespace Azos.Data.Access.Sharding
+{
+  /// <summary>
+  /// Implements IDataContext(IDataStore) with sharding
+  /// </summary>
+  public class ShardedDataContext : DaemonWithInstrumentation<IApplicationComponent>, IShardedCrudDataStoreImplementation, IDataContextImplementation
+  {
+    public const string CONFIG_STORE_SECTION = "store";
+
+    public ShardedDataContext(IApplication app) : base(app) {  }
+    public ShardedDataContext(IApplicationComponent director) : base(director) { }
+
+    protected override void Destructor()
+    {
+      cleanup();
+      base.Destructor();
+    }
+
+    private bool m_InstrumentationEnabled;
+    private ICrudDataStoreImplementation m_PhysicalStore;
+
+    /// <summary>
+    /// Underlying physical data store servicing requests
+    /// </summary>
+    public ICrudDataStore PhysicalStore => m_PhysicalStore.NonNull("configured `./store`");
+
+
+    [ExternalParameter(CoreConsts.EXT_PARAM_GROUP_LOG, CoreConsts.EXT_PARAM_GROUP_DATA)]
+    public StoreLogLevel DataLogLevel { get; set; }
+
+    public string TargetName => "*";
+    public int DefaultTimeoutMs { get => 0; set {} }
+
+    public override string ComponentLogTopic => CoreConsts.DATA_TOPIC;
+
+    /// <summary>
+    /// Implements IInstrumentable
+    /// </summary>
+    [Config(Default = false)]
+    [ExternalParameter(CoreConsts.EXT_PARAM_GROUP_DATA, CoreConsts.EXT_PARAM_GROUP_INSTRUMENTATION)]
+    public override bool InstrumentationEnabled
+    {
+      get { return m_InstrumentationEnabled; }
+      set { m_InstrumentationEnabled = value; }
+    }
+
+    public IEnumerable<IShard> CurrentSet => throw new NotImplementedException();
+
+
+    public void TestConnection()
+    {
+
+    }
+
+    public CrudOperations GetOperationsFor(ShardKey key)
+    {
+      throw new NotImplementedException();
+    }
+
+    CrudOperationCallContext IShardedCrudDataStoreImplementation.MakeCallContext(IShard shard)
+    {
+      throw new NotImplementedException();
+    }
+
+
+    protected override void DoConfigure(IConfigSectionNode node)
+    {
+      base.DoConfigure(node);
+      if (node == null) return;
+
+      cleanup();
+
+      //m_Contexts = new Registry<IDataContextImplementation>();
+      //foreach (var ndb in node.ChildrenNamed(CONFIG_CONTEXT_SECTION))
+      //{
+      //  var context = FactoryUtils.MakeAndConfigureDirectedComponent<IDataContextImplementation>(
+      //                                  this,
+      //                                  ndb);
+
+      //  if (!m_Contexts.Register(context))
+      //    throw new DataAccessException($"{nameof(DefaultDataContextHub)} config contains duplicate named section: ./context[name='{context.Name}']");
+      //}
+    }
+
+
+    protected override void DoStart()
+    {
+      m_PhysicalStore.NonNull(nameof(m_PhysicalStore));
+      if (m_PhysicalStore is Daemon d) d.Start();
+      base.DoStart();
+    }
+
+    protected override void DoSignalStop()
+    {
+      base.DoSignalStop();
+      if (m_PhysicalStore is Daemon d) d.SignalStop();
+    }
+
+    protected override void DoWaitForCompleteStop()
+    {
+      base.DoWaitForCompleteStop();
+      if (m_PhysicalStore is Daemon d) d.WaitForCompleteStop();
+    }
+
+    protected override void DoAcceptManagerVisit(object manager, DateTime managerNow)
+    {
+      base.DoAcceptManagerVisit(manager, managerNow);
+
+      if (m_PhysicalStore is Daemon d)
+      {
+        this.DontLeak(() => d.AcceptManagerVisit(this, managerNow));
+      }
+    }
+
+    private void cleanup()
+    {
+      this.DontLeak(() =>  DisposeAndNull(ref m_PhysicalStore));
+    }
+
+
+  }
+}
