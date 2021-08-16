1355cdfbacbc0ad69dec99dc4c3d6c1f056884ca
Dmitriy Khmaladze <d@zhabis.com>
2021-08-16 18:44:56 -0400

    WIP #537 CloudOrigin

--- src/Azos/Apps/NOPApplication.cs
+++ src/Azos/Apps/NOPApplication.cs
@@ -113,6 +113,8 @@ namespace Azos.Apps
 
     public Atom AppId => Atom.ZERO;
 
+    public Atom CloudOrigin => Atom.ZERO;
+
     public ushort NodeDiscriminator => 0;
 
     public Guid InstanceId => m_InstanceID;
