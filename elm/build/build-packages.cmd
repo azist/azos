008443d051e08ecdcbce2cb80fdb7efd434705dc
Dmitriy Khmaladze <d@zhabis.com>
2021-09-01 17:15:15 -0400

    Nuget 1.30.0 push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=1.20.5.12
-set HEADLINE=#550
+set VER=1.30.0
+set HEADLINE=#553 CRUD Execute doc, QueryHandler.ExecuteProcedure
 call vs2017-build-release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
