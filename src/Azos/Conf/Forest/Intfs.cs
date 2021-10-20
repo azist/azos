4e1d9d654939c0554572035d6d5d0a20afafa148
Dmitriy Khmaladze <d@zhabis.com>
2021-10-20 14:39:15 -0400

    Wip #562 Forest

--- src/Azos/Conf/Forest/Intfs.cs
+++ src/Azos/Conf/Forest/Intfs.cs
@@ -15,13 +15,18 @@ using Azos.Data.Business;
 namespace Azos.Conf.Forest
 {
   /// <summary>
-  /// Defines logic for consuming Forest services
+  /// Defines logic for consuming Forest services.
+  /// A forest is a system of named trees; each tree is a hierarchy (DAG) or tree nodes containing configuration
+  /// content. The strength of the system lies in inheritance of configuration vectors which cascade down from tree root level down
+  /// to individual node level respecting structured config override rules.
+  /// All data is a). version-controlled  b). has a temporal component - a point in time when a node becomes active, therefore the system
+  /// allows for back and forward dating node presence in a tree
   /// </summary>
   public interface IForestLogic : IBusinessLogic
   {
     /// <summary>
     /// Retrieves all versions of the specified tree node identified by GDID.
-    /// You must use <see cref="TreePath.SCH_GNODE"/> address schema
+    /// You must use <see cref="Constraints.SCH_GNODE"/> address schema
     /// </summary>
     /// <param name="id">Node Gdid is the only address schema supported</param>
     /// <returns>List of versions of the specified path or null if such id does not exist</returns>
@@ -29,31 +34,27 @@ namespace Azos.Conf.Forest
 
     /// <summary>
     /// Retrieves tree node information of the specified version.
-    /// Note: you must use <see cref="TreePath.SCH_GVER"/> address schema:  `region.gver@geo::0:0:345`
+    /// Note: you must use <see cref="Constraints.SCH_GVER"/> address schema:  `region.gver@geo::0:0:345`
     /// </summary>
     /// <param name="idVersion">EntityId of version containing (system, type, gver: gVersion)</param>
     /// <returns>NodeInfo or null if not found</returns>
     Task<TreeNodeInfo> GetNodeInfoVersionAsync(EntityId idVersion);
 
-//Delete this????
-/*
     /// <summary>
-    /// Retrieves a list of enterprise nodes which are top-level nodes of the corporate hierarchy
+    /// Retrieves a list trees in the specified forest
     /// </summary>
-    /// <param name="asOfUtc">As of which point in time to retrieve the state, if null passed then current timestamp assumed</param>
-    /// <param name="cache">Controls cache options used by the call, such as bypass cache etc.</param>
-    /// <returns>Enumerable of <see cref="ListItem"/> objects</returns>
-    Task<IEnumerable<(Atom, Atom)>> GetTreeRootListAsync(DateTime? asOfUtc = null, ICacheParams cache = null);
-*/
+    /// <param name="idForest">Id of a forest to get tree ids from. Forest id is encoded as `EntityId.System` field</param>
+    /// <returns>Enumerable of <see cref="Atom"/> objects identifying trees of the selected forest</returns>
+    Task<IEnumerable<Atom>> GetTreeListAsync(Atom idForest);
 
     /// <summary>
-    /// Retrieves a list of child nodes for the specified corporate hierarchy entity
+    /// Retrieves a list of child nodes headers (node info without config content) for the specified tree node
     /// </summary>
-    /// <param name="idParent">EntityId of a parent, e.g. an enterprise, company or division node</param>
-    /// <param name="asOfUtc">As of which point in time to retrive the state, if null passed then current timestamp assumed</param>
+    /// <param name="idParent">EntityId of a parent node</param>
+    /// <param name="asOfUtc">As of which point in time to retrieve the state, if null passed then current timestamp assumed</param>
     /// <param name="cache">Controls cache options used by the call, such as bypass cache etc.</param>
-    /// <returns>Enumerable of <see cref="ListItem"/> objects</returns>
-//    Task<IEnumerable<ListItem>> GetChildNodeListAsync(EntityId idParent, DateTime? asOfUtc = null, ICacheParams cache = null);
+    /// <returns>Enumerable of <see cref="TreeNodeHeader"/> objects</returns>
+    Task<IEnumerable<TreeNodeHeader>> GetChildNodeListAsync(EntityId idParent, DateTime? asOfUtc = null, ICacheParams cache = null);
 
     /// <summary>
     /// Retrieves a node of corporate hierarchy by its id as of certain point in time
@@ -63,20 +64,27 @@ namespace Azos.Conf.Forest
     /// <param name="cache">Controls cache options used by the call, such as bypass cache etc.</param>
     /// <returns>CorporateHierarchyNodeInfo-derived object for the requested level, null if such item is not found</returns>
     Task<TreeNodeInfo> GetNodeInfoAsync(EntityId id, DateTime? asOfUtc = null, ICacheParams cache = null);
+  }
 
-
+  /// <summary>
+  /// Defines setup operations for forest tree structures: Saving and Deleting tree nodes.
+  /// This interface provide writing functionality on top of reading functionality provided by `IForestLogic`
+  /// and the caller is required to have a additional permission grants to perform these operations
+  /// </summary>
+  public interface IForestSetupLogic : IForestLogic
+  {
     /// <summary>
     /// Performs complex validation steps on the supplied node before it gets saved.
-    /// This method is called by the CorporateNode instance as a part of save activity.
+    /// This method is called by the `TreeNode` instance as a part of save activity.
     /// For example, child nodes check that they are saved under the proper parent entity type etc.
     /// </summary>
-    /// <param name="node">Model node to validate</param>
+    /// <param name="node">Tree node to validate</param>
     /// <param name="state">Existing validState</param>
     /// <returns><see cref="ValidState"/> representative of validation outcome</returns>
     Task<ValidState> ValidateNodeAsync(TreeNode node, ValidState state);
 
     /// <summary>
-    /// Persists the representation of data supplied as `CorporateHierarchyNode` in the version-controlled
+    /// Persists the representation of data supplied as `TreeNode` in the version-controlled
     /// store. The deletion works via `node.FormMode = Delete` which effectively inactivates the record
     /// </summary>
     /// <param name="node">Persisted data model</param>
@@ -86,7 +94,7 @@ namespace Azos.Conf.Forest
     /// <summary>
     /// Logically deletes node by its ID returning <see cref="ChangeResult"/>.
     /// Deletion of node is logical(not-physical) and logically inactivates all child structures under it, for example
-    /// deletion of a company logically deletes all divisions and units under that company effectively making
+    /// deletion of `policies` node logically deletes all individual `policy` child nodes and their sub-nodes effectively making
     /// those entities not found
     /// </summary>
     /// <param name="id">Node id</param>
---
+++ src/Azos/Conf/Forest/TreeNodeHeader.cs
@@ -0,0 +1,36 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Azos.Data;
+using Azos.Data.Business;
+using Azos.Serialization.Bix;
+
+namespace Azos.Conf.Forest
+{
+  /// <summary>
+  /// Provides minimal tree node information used for listing
+  /// </summary>
+  [Bix("a897b1af-b2a9-463a-a361-c307ba10e779")]
+  [Schema(immutable: true, Description = "Provides minimal tree node information used for listing")]
+  public sealed class TreeNodeHeader : TransientModel
+  {
+    [Field(required: true, Description = "EntityId for listed node")]
+    public EntityId Id { get; set; }
+
+    [Field(required: true, Description = "Gdid of this data version")]
+    public GDID G_Version { get; set; }
+
+    [Field(required: true, Description = "Path segment of the parent")]
+    public string PathSegment { get; set; }
+
+    [Field(required: true, Description = "Timestamp as of which this tree node becomes effective")]
+    public DateTime StartUtc { get; set; }
+  }
+}
