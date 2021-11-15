492cb4aefe5171b6dda49bcbaf3b0ae0de77ebdc
Dmitriy Khmaladze <d@zhabis.com>
2021-11-14 22:24:28 -0500

    Implementation MsSqlRpcHandler #609 G8#142

--- src/providers/Azos.MsSql/Rpc/MsSqlRpcHandler.cs
+++ src/providers/Azos.MsSql/Rpc/MsSqlRpcHandler.cs
@@ -7,11 +7,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Text;
 using System.Threading.Tasks;
 
 using Azos.Apps;
-using Azos.Data;
 using Azos.Data.Access.Rpc;
 
 namespace Azos.Data.Access.MsSql
@@ -41,12 +39,12 @@ namespace Azos.Data.Access.MsSql
     }
 
     /// <summary>
-    /// Override to validate the read request
+    /// Must override to validate the read request
     /// </summary>
     protected abstract ValidState DoValidateReadRequest(ValidState state, ReadRequest request);
 
     /// <summary>
-    /// Override to validate the transact request
+    /// Must override to validate the transact request
     /// </summary>
     protected abstract ValidState DoValidateTransactRequest(ValidState state, TransactRequest request);
 
@@ -67,26 +65,27 @@ namespace Azos.Data.Access.MsSql
 
     public async Task<Rowset> ReadAsync(ReadRequest request)
     {
+#warning Finish this tomorrow
       //App.Authorize();
-      SqlConnection connection = null;//getConnection for context()
+      SqlConnection connection = null;//getConnection for context() <-- add as a virtual function to allow only certain context mappings
 
       var command = request.Command.NonNull(nameof(request.Command));
+      command.Text.NonBlank(nameof(command.Text));
       var isSql = true;
 
       if (command.Headers != null)
       {
         var tp = command.Headers[StandardHeaders.Sql.COMMAND_TYPE].AsString();
+
         if (tp.EqualsOrdIgnoreCase(StandardHeaders.Sql.COMMAND_TYPE_VALUE_TEXT)) isSql = true;
         else if (tp.EqualsOrdIgnoreCase(StandardHeaders.Sql.COMMAND_TYPE_VALUE_PROC)) isSql = false;
-        else if (tp.IsNotNullOrWhiteSpace()) throw new DocValidationException(nameof(ReadRequest.Command), "Invalid header `{0}` = `{1}`".Args(StandardHeaders.Sql.COMMAND_TYPE, tp));
+        else if (tp.IsNotNullOrWhiteSpace())
+        {
+          throw new DocValidationException(nameof(ReadRequest.Command), "Invalid header `{0}` = `{1}`".Args(StandardHeaders.Sql.COMMAND_TYPE, tp));
+        }
       }
 
-      Rowset rowset = null;
-      //if (isSql)
-      //  rowset = await executeReadSqlAsync(connection, command).ConfigureAwait(false);
-      //else
-      //  rowset = await executeReadStoredProcAsync(connection, command).ConfigureAwait(false);
-
+      var rowset = await readAsync(connection, command, isSql).ConfigureAwait(false);
       return rowset;
     }
 
@@ -96,5 +95,98 @@ namespace Azos.Data.Access.MsSql
     }
     #endregion
 
+    #region .pvt
+    private void bindParams(SqlCommand cmd, Command command)
+    {
+      if (command.Parameters == null) return;
+      var pName = "?";
+      try
+      {
+        foreach(var par in command.Parameters)
+        {
+          pName = "?";
+          var name = par.Name.NonBlank("parameter name");
+          pName = name;
+          var val = par.Value;
+
+          if (val != null)
+          {
+            var tp = Command.Param.TryMap(par.Type);
+            if (tp != null)
+            {
+              val = val.AsString().AsType(tp);
+            }
+          }
+          cmd.Parameters.AddWithValue(name, val);
+        }
+      }
+      catch(Exception error)
+      {
+        throw new ValidationException("Command parameter `{0}` cast error: {1}".Args(pName, error.ToMessageWithType()), error);
+      }
+    }
+
+    private Schema inferSchema(SqlDataReader reader, Command command)
+    {
+      var name = "{0}-{1}".Args(GetType().DisplayNameWithExpandedGenericArgs(), Guid.NewGuid());
+      var fdefs = new List<Schema.FieldDef>();
+
+      for (int i = 0; i < reader.FieldCount; i++)
+      {
+        var fname = reader.GetName(i);
+        var ftype = reader.GetFieldType(i);
+
+        var def = new Schema.FieldDef(fname, ftype, attr: null);
+        fdefs.Add(def);
+      }
+
+      return new Schema(name, readOnly: true, fieldDefs: fdefs);
+    }
+
+    private void populateDoc(Doc doc, SqlDataReader reader, Command command)
+    {
+      for (int i = 0; i < reader.FieldCount; i++)
+      {
+        var fdef = doc.Schema[i];
+        var val = reader.GetValue(i);
+
+        if (val == null || val is DBNull)
+        {
+          doc[fdef.Order] = null;
+          continue;
+        }
+
+        doc[fdef.Order] = val;
+      }
+    }
+
+    private async Task<Rowset> readAsync(SqlConnection connection, Command command, bool isSql)
+    {
+      Rowset result = null;
+
+      using(var cmd = connection.CreateCommand())
+      {
+        cmd.CommandType = isSql ? System.Data.CommandType.Text : System.Data.CommandType.StoredProcedure;
+        cmd.CommandText = command.Text;
+
+        bindParams(cmd, command);
+
+        using(var reader = await cmd.ExecuteReaderAsync().ConfigureAwait(false))
+        {
+          var schema = inferSchema(reader, command);
+          result = new Rowset(schema);
+          result.LogChanges = false;
+          while(await reader.ReadAsync().ConfigureAwait(false))
+          {
+            var doc = Doc.MakeDoc(result.Schema);
+            populateDoc(doc, reader, command);
+            result.Add(doc);
+          }
+        }
+      }
+
+      return result;
+    }
+    #endregion
   }
 }
