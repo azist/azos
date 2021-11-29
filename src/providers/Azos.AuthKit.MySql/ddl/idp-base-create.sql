69e14a6255f5997ec392708728cc41f3e5afeaf3
Dmitriy Khmaladze <d@zhabis.com>
2021-11-29 10:48:01 -0500

    #587 wip ddl

--- src/providers/Azos.AuthKit.MySql/ddl/idp-base-create.sql
+++ src/providers/Azos.AuthKit.MySql/ddl/idp-base-create.sql
@@ -8,16 +8,16 @@ create table tbl_user
   `REALM`      BIGINT UNSIGNED   not null comment 'Realm Atom',
 
   `NAME`        VARCHAR(64)      not null comment 'User name',
-  `LEVEL`       CHAR(1)                   comment 'User archetype access level: I|U|A|S',
-  `DESCRIPTION` VARCHAR(128)     not null comment 'User name',
+  `LEVEL`       CHAR(1)          not null comment 'User archetype access level: I|U|A|S',
+  `DESCRIPTION` VARCHAR(128)     not null comment 'User description',
 
   `START_UTC`  DATETIME       not null    comment 'When user privilege takes effect',
   `END_UTC`    DATETIME       not null    comment 'When user privilege stops',
   `ORG_UNIT`   VARCHAR(256)               comment 'Tree path for org unit. So the user list may be searched by it',
   `PROPS`      MEDIUMTEXT     not null    comment 'Properties such as tree connections (e.g. roles) and claims',
-  `RIGHTS`     MEDIUMTEXT          comment 'User-specific Rights override or null for default rights',
+  `RIGHTS`     MEDIUMTEXT                 comment 'User-specific Rights override or null for default rights',
 
-  `NOTE`       MEDIUMTEXT          comment 'Free form text notes associated with the account',
+  `NOTE`       MEDIUMTEXT                 comment 'Free form text notes associated with the account',
 
   `CREATE_UTC`    DATETIME         not null comment 'Creation UTC timestamp',
   `CREATE_ORIGIN` BIGINT UNSIGNED  not null comment 'Cloud partition where this record first originated from',
@@ -59,9 +59,9 @@ create table tbl_login
   `LEVEL_DOWN` CHAR(1)                    comment 'User level restriction: I|U|A',
 
   `ID`         VARCHAR(2048)     not null comment 'Login ID, or provider key',
-  `TID`        CHAR(1)           not null comment 'Login Type Atom',
+  `TID`        BIGINT UNSIGNED   not null comment 'Login Type Atom',
   `PROVIDER`   BIGINT UNSIGNED   not null comment 'Login provider, e.g.  AZOS, FBK, TWT, AD, SYSTEM X etc.. or Atom.ZERO for not eternal provider',
-  `PWD`        VARCHAR(2048)     not null comment 'Password vector, or empty for providers',
+  `PWD`        VARCHAR(2048)              comment 'Password vector, or NULL for providers who dont need it',
   `PROVIDER_DATA`     MEDIUMTEXT          comment 'Optional extra provider -specific JSON vector',
 
   `START_UTC`  DATETIME       not null    comment 'When login privilege takes effect',
@@ -69,12 +69,10 @@ create table tbl_login
   `PROPS`      MEDIUMTEXT                 comment 'Properties such as tree connections (e.g. roles) and claims',
   `RIGHTS`     MEDIUMTEXT                 comment 'Rights override or null',
 
-
   `LOCK_START_UTC` DATETIME           comment 'Lock timestamp, if set the account is inactive past that timestamp, until LOCK_END_UTC',
   `LOCK_END_UTC`   DATETIME           comment 'If present, resets lock after that point in time',
   `LOCK_ACTOR`     VARCHAR(256)       comment 'Who locked the user account',
   `LOCK_NOTE`      VARCHAR(128)       comment 'Short note explaining lock reason/status',
-
 
   `VERSION_UTC`    DATETIME       not null comment 'Version UTC timestamp',
   `VERSION_ORIGIN` BIGINT UNSIGNED  not null comment 'Cloud partition where this version originated from',
@@ -97,12 +95,14 @@ create table tbl_loginstatus
   `PWD_DATE_UTC`    DATETIME    not null comment 'When pwd was set last time',
   --pwd confirm data {date, method, history} etc.. used by Policy (loaded from tree)
 
-  `LAST_OK_UTC`    DATETIME     not null  comment 'Last correct login timestamp or null',
-  `LAST_OK_ADDR`   VARCHAR(32)  not null  comment 'Last correct login timestamp or null',
+  `LAST_OK_UTC`    DATETIME      comment 'Last correct login timestamp or null',
+  `LAST_OK_ADDR`   VARCHAR(32)   comment 'Last correct login timestamp or null',
+  `LAST_OK_AGENT`  VARCHAR(256)  comment 'Last correct login user agent or null',
 
-  `LAST_BAD_UTC`   DATETIME         comment 'Last incorrect login timestamp or null',
-  `LAST_BAD_ADDR`  VARCHAR(32)      comment 'Last correct login timestamp or null',
-  `BAD_COUNT`      INT              comment 'Consecutive incorrect login count',
+  `LAST_BAD_UTC`    DATETIME         comment 'Last bad login timestamp or null',
+  `LAST_BAD_ADDR`   VARCHAR(32)      comment 'Last bad login timestamp or null',
+  `LAST_BAD_AGENT`  VARCHAR(256)     comment 'Last bad login user agent or null',
+  `BAD_COUNT`       INT              comment 'Consecutive incorrect login count',
 )
    comment = 'LoginStatus keeps Login volatile information to lessen the replication load Login:LoginStatus = 1:1 (same GDID)'
 ;.
