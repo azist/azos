8b66165358453c039cda5905b5b105e32ecf734d
Dmitriy Khmaladze <d@zhabis.com>
2021-12-27 14:40:45 -0500

    #587 ParseId()

--- src/providers/Azos.AuthKit.Server/DefaultIdpHandlerLogic.cs
+++ src/providers/Azos.AuthKit.Server/DefaultIdpHandlerLogic.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 using Azos.Apps;
@@ -22,12 +23,15 @@ namespace Azos.AuthKit.Server
     public DefaultIdpHandlerLogic(IModule parent) : base(parent) { }
 
     Registry<LoginProvider> m_Providers;
+    Atom m_DefaultLoginProvider;
 
     public bool IsServerImplementation => true;
     public override bool IsHardcodedModule => false;
     public override string ComponentLogTopic => CoreConsts.SECURITY_TOPIC;
 
     public IRegistry<LoginProvider> Providers => m_Providers;
+
+    public Atom DefaultLoginProvider => m_DefaultLoginProvider;
 
     public string SysTokenCryptoAlgorithmName => throw new NotImplementedException();
 
@@ -77,9 +81,43 @@ namespace Azos.AuthKit.Server
       throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Parses the supplied login string expressed in EntityId format.
+    /// The string has to be formatted as EntityId or plain string which then assumes defaults.
+    /// The EntityId.System is Provider.Name, and EntityId.Type is login type.
+    /// Throws `DataValidationException/400` on wrong ID
+    /// </summary>
     public EntityId ParseId(string id)
     {
-      throw new NotImplementedException();
+      var isplain = id.NonBlank(nameof(id))
+                      .IndexOf(EntityId.SYS_PREFIX) == -1;
+
+      if (isplain)
+      {
+        var p = Providers[DefaultLoginProvider.Value].NonNull(nameof(DefaultLoginProvider));
+        return new EntityId(DefaultLoginProvider, p.DefaultLoginType, Atom.ZERO, id);
+
+      }
+
+      if (!EntityId.TryParse(id, out var result))
+      {
+        throw new ValidationException("Bad id format") { HttpStatusDescription = "The ID is not parable as EntityId"};
+      }
+
+      var provider = Providers[result.System.Value];
+      if (provider == null)
+      {
+        throw new ValidationException("Unknown provider") { HttpStatusDescription = "Login provider `{0}` is not known".Args(result.System) };
+      }
+
+      if (result.Type.IsZero) result = new EntityId(result.System, provider.DefaultLoginType, Atom.ZERO, result.Address);
+
+      if (!provider.SupportedLoginTypes.Any(t => t == result.Type))
+      {
+        throw new ValidationException("Bad login type") { HttpStatusDescription = "Login provider `{0}` does not support login type `{1}`".Args(result.System, result.Type) };
+      }
+
+      return result;
     }
 
     public EntityId ParseUri(string uri)
