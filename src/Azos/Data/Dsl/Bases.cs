8dd75f791d6860364046a5da755bc1b53430b142
Dmitriy Khmaladze <d@zhabis.com>
2022-04-20 15:17:30 -0400

    Add data loader DSL steps

---
+++ src/Azos/Data/Dsl/Bases.cs
@@ -0,0 +1,57 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Azos.Collections;
+using Azos.Conf;
+using Azos.Scripting.Dsl;
+using Azos.Serialization.JSON;
+
+namespace Azos.Data.Dsl
+{
+  /// <summary>
+  /// Facilitates the loading of TDataStream into a call frame
+  /// </summary>
+  public abstract class DataLoader<TData> : Step where TData : class, IDataSource, INamed
+  {
+    /// <summary>
+    /// Tries to find a data stream of a specified type with optional name on a call stack of frames.
+    /// Returns null if such data stream is not found
+    /// </summary>
+    public static TData TryGet(string name = null)
+     => StepRunner.Frame.Current?.All.FirstOrDefault(o => (o is TData d) && (name.IsNullOrWhiteSpace() || d.Name.EqualsOrdIgnoreCase(name))) as TData;
+
+
+    /// <summary>
+    /// Tries to find a data stream of a specified type with optional name on a call stack of frames.
+    /// Throws if such data stream is not found and dependency could not be satisfied
+    /// </summary>
+    public static TData Get(string name = null)
+     => TryGet(name).NonNull("Satisfied data stream dependency on `{0}('{1}')` loaded by `{2}` step".Args(
+                             typeof(TData).DisplayNameWithExpandedGenericArgs(),
+                             name.Default("<null>"),
+                             nameof(DataLoader<TData>)));
+
+
+    protected DataLoader(StepRunner runner, IConfigSectionNode cfg, int idx) : base(runner, cfg, idx) { }
+
+
+    protected abstract IDataSource MakeDataSource(JsonDataMap state);
+
+    protected override Task<string> DoRunAsync(JsonDataMap state)
+    {
+      var stream = MakeDataSource(state);
+      StepRunner.Frame.Current.Owned.Add(stream);
+      return Task.FromResult<string>(null);
+    }
+  }
+
+  public interface IDataSource : IDisposable, INamed { }
+
+}
---
+++ src/Azos/Data/Dsl/JsonLoaders.cs
@@ -0,0 +1,57 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using Azos.Conf;
+using Azos.Scripting.Dsl;
+using Azos.Serialization.JSON;
+
+namespace Azos.Data.Dsl
+{
+  public class JsonObjectLoader : DataLoader<JsonObjectDataSource>
+  {
+    public JsonObjectLoader(StepRunner runner, IConfigSectionNode cfg, int idx) : base(runner, cfg, idx) { }
+
+    [Config]
+    public string FileName { get; set; }
+
+    [Config]
+    public string Json { get; set; }
+
+    protected override IDataSource MakeDataSource(JsonDataMap state)
+    {
+      var fn = Eval(FileName, state);
+      var json = Eval(Json, state);
+
+      if (fn.IsNotNullOrWhiteSpace())
+        return JsonObjectDataSource.FromFile(Name, fn);
+      else
+        return JsonObjectDataSource.FromJson(Name, json);
+    }
+  }
+
+  public sealed class JsonObjectDataSource : DisposableObject, IDataSource
+  {
+    public static JsonObjectDataSource FromFile(string name, string fileName)
+     => new JsonObjectDataSource(name.NonBlank(nameof(name)), JsonReader.DeserializeDataObjectFromFile(fileName));
+
+    public static JsonObjectDataSource FromJson(string name, string json)
+     => new JsonObjectDataSource(name.NonBlank(nameof(name)), JsonReader.DeserializeDataObject(json));
+
+    private JsonObjectDataSource(string name, IJsonDataObject data)
+    {
+      m_Name = name;
+      m_Data = data;
+    }
+
+    private string m_Name;
+    private IJsonDataObject m_Data;
+
+    public string Name => m_Name;
+    public IJsonDataObject Data => m_Data;
+  }
+
+
+}
