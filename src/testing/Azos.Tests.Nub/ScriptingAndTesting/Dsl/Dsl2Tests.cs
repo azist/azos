bd00e7ead7f3363b5ccae3355ecce83d7a4b3561
JohnPKosh <devprohome@live.com>
2022-04-28 10:31:17 -0400

    add failing ReadJson tests with arg eval

--- src/testing/Azos.Tests.Nub/ScriptingAndTesting/Dsl/Dsl2Tests.cs
+++ src/testing/Azos.Tests.Nub/ScriptingAndTesting/Dsl/Dsl2Tests.cs
@@ -88,6 +88,41 @@ namespace Azos.Tests.Nub.ScriptingAndTesting.Dsl
       Aver.AreEqual("Rinaldo", runnable.GlobalState["server"].AsString());
     }
 
+    public const string JSON_STATE_LOADER_FROM_JSON_ARGS = @"
+      script
+      {
+        type-path='Azos.Scripting.Dsl, Azos;Azos.Data.Dsl, Azos'
+
+do{ type='Set' global='gname' to='Gurariy' }
+
+        do{ type='ReadJson' global='global_tezt' json='{""name"":""Gurariy""}'}
+        do{ type='ReadJson' local='local_tezt' json='{""name"":""~global.gname""}'}
+
+        do{ type='See' format='Global tezt name is: {~global.global_tezt.name}'}
+        do{ type='See' format='Local tezt name is: {~local.local_tezt.name}'}
+
+        do{ type='DumpGlobalState'}
+        do{ type='DumpLocalState'}
+
+      }
+    ";
+
+    [Run]
+    public async Task SetJsonStateLoader_FromJsonArgs()
+    {
+      var runnable = new StepRunner(NOPApplication.Instance, JSON_STATE_LOADER_FROM_JSON_ARGS.AsLaconicConfig(handling: Data.ConvertErrorHandling.Throw));
+      var state = await runnable.RunAsync();
+
+      var gotGlobal = runnable.GlobalState["global_tezt"] as JsonDataMap;
+      Aver.IsNotNull(gotGlobal);
+      Aver.AreEqual("Gurariy", gotGlobal["name"].AsString());
+
+      var gotLocal = state["local_tezt"] as JsonDataMap;
+      Aver.IsNotNull(gotLocal);
+      Aver.AreEqual("Gurariy", gotLocal["name"].AsString());
+    }
+
+
     public const string JSON_STATE_LOADER_FROM_JSON = @"
       script
       {
@@ -118,7 +153,6 @@ namespace Azos.Tests.Nub.ScriptingAndTesting.Dsl
       var gotLocal = state["local_tezt"] as JsonDataMap;
       Aver.IsNotNull(gotLocal);
       Aver.AreEqual("Gurariy", gotLocal["name"].AsString());
-
     }
 
 
@@ -149,6 +183,50 @@ namespace Azos.Tests.Nub.ScriptingAndTesting.Dsl
       saveJsonFile(fn, @"{""name"":""Gurariy""}"); // ********** SAVE FILE **********
 
       var runnable = new StepRunner(NOPApplication.Instance, JSON_STATE_LOADER_FROM_FILE.AsLaconicConfig(handling: Data.ConvertErrorHandling.Throw));
+      var state = await runnable.RunAsync();
+
+      delJsonFile(fn); // ********** DEL FILE **********
+
+      var gotGlobal = runnable.GlobalState["global_tezt"] as JsonDataMap;
+      Aver.IsNotNull(gotGlobal);
+      Aver.AreEqual("Gurariy", gotGlobal["name"].AsString());
+
+      var gotLocal = state["local_tezt"] as JsonDataMap;
+      Aver.IsNotNull(gotLocal);
+      Aver.AreEqual("Gurariy", gotLocal["name"].AsString());
+
+    }
+
+
+    public const string JSON_STATE_LOADER_FROM_FILE_ARGS = @"
+      script
+      {
+        type-path='Azos.Scripting.Dsl, Azos;Azos.Data.Dsl, Azos'
+
+        do{ type='Set' global='input_file_name' to='JSON_02' }
+
+do{ type='Set' global='gname' to='Gurariy' }
+
+        do{ type='ReadJson' global='global_tezt' local='local_tezt' fileName='{~global.input_file_name}.json'}
+
+        do{ type='See' format='Global tezt name is: {~global.global_tezt.name}'}
+        do{ type='See' format='Local tezt name is: {~local.local_tezt.name}'}
+
+        do{ type='See' format='File name is: {~global.input_file_name}'}
+
+        do{ type='DumpGlobalState'}
+        do{ type='DumpLocalState'}
+
+      }
+    ";
+
+    [Run]
+    public async Task SetJsonStateLoader_FromFileWithArgs()
+    {
+      var fn = "JSON_02.json";
+      saveJsonFile(fn, @"{""name"":""{~global.gname}""}"); // ********** SAVE FILE **********
+
+      var runnable = new StepRunner(NOPApplication.Instance, JSON_STATE_LOADER_FROM_FILE_ARGS.AsLaconicConfig(handling: Data.ConvertErrorHandling.Throw));
       var state = await runnable.RunAsync();
 
       delJsonFile(fn); // ********** DEL FILE **********
