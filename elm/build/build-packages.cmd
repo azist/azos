5f462d7149ace20479a5a535e5ec517fb34e4970
Dmitriy Khmaladze <d@zhabis.com>
2022-05-17 15:37:10 -0400

    Nuget push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=1.42.11.1
-set HEADLINE=#688 rtm ADLIB server
+set VER=1.42.12.1
+set HEADLINE=#695 Composite IDS
 call vs2017-build-release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
