aeea38fabe77f7d8830028281d93a7ab1c1f7091
Dmitriy Khmaladze <d@zhabis.com>
2022-05-19 11:44:16 -0400

    Nuget push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=1.42.12.1
-set HEADLINE=#695 Composite IDS
+set VER=1.42.12.2
+set HEADLINE=#696 Ability to pass pre-generated GDID on insert to Adlib item
 call vs2017-build-release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
