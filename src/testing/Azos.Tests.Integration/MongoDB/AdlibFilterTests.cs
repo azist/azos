b852f86c766e7364b38c87a0e75da19520681ad7
Dmitriy Khmaladze <d@zhabis.com>
2022-06-13 11:30:42 -0400

    Add skeleton for #702 bug Mongo adlib XLAT

---
+++ src/testing/Azos.Tests.Integration/MongoDB/AdlibFilterTests.cs
@@ -0,0 +1,34 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Azos.Scripting;
+using Azos.Data.Adlib;
+using Azos.Data.Adlib.Server;
+
+namespace Azos.Tests.Integration.MongoDb
+{
+  [Runnable]
+  public class AdlibFilterTests
+  {
+    [Run]
+    public void Test01()
+    {
+      var filter = new ItemFilter
+      {
+
+      };
+
+      var qry = BsonConvert.GetFilterQuery(filter);
+
+      qry.See();
+
+    }
+  }
+}
