4b0328e7985aae0dfb8cad88fc9801ad2c64fc7c
Dmitriy Khmaladze <d@zhabis.com>
2022-07-05 12:44:14 -0400

    #718 Aspect plumbing

--- src/Azos/Client/Intfs.Http.cs
+++ src/Azos/Client/Intfs.Http.cs
@@ -37,6 +37,43 @@ namespace Azos.Client
     /// When set, overrides the HTTP_HDR_DEFAULT_CALL_FLOW header name
     /// </summary>
     string DistributedCallFlowHeader { get; }
+
+    /// <summary> If True, automatically follows HTTP redirect </summary>
+    bool AutoRedirect { get; }
+
+    /// <summary> When set imposes maximum on the redirect count </summary>
+    int? AutoRedirectMax { get; }
+
+    /// <summary> If True, automatically decompresses traffic </summary>
+    bool AutoDecompress { get; }
+
+    string AuthScheme { get; }
+
+    string AuthHeader { get; }
+
+    /// <summary>
+    /// When set to true, attaches Authorization header with `SysToken` scheme and sysAuthToken content, overriding
+    /// the AuthHeader value (if any)
+    /// </summary>
+    bool AuthImpersonate { get; }
+
+    /// <summary>
+    /// When set, overrides the standard HTTP `Authorization` header name when impersonation is used
+    /// </summary>
+    string AuthImpersonateHeader { get; }
+
+    bool AcceptJson { get; }
+
+    bool UseCookies { get; }
+
+    /// <summary> When set imposes maximum content buffer size limit in bytes </summary>
+    int? MaxRequestContentBufferSize { get; }
+
+    /// <summary> When set imposes maximum on the response headers length sent back from server </summary>
+    int? MaxResponseHeadersLength { get; }
+
+    /// <summary> When set imposes maximum on connection count </summary>
+    int? MaxConnections { get; }
   }
 
 
