c99b2b827550df8b377a47aa69917f3a4e4dff34
Dmitriy Khmaladze <d@zhabis.com>
2022-07-13 11:25:45 -0400

    #721 DSL dir and file exists, Nuget

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=1.43.0.1
-set HEADLINE=#718 http client aspects
+set VER=1.43.0.2
+set HEADLINE=#721 dsl paths
 call vs2017-build-release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
