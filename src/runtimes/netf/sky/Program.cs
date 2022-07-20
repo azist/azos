d5f69f0bd621e5511ce1f50f4a893a73716103ab
Dmitriy Khmaladze <d@zhabis.com>
2022-07-20 11:14:11 -0400

    #624 help text

--- src/runtimes/netf/sky/Program.cs
+++ src/runtimes/netf/sky/Program.cs
@@ -27,12 +27,16 @@ namespace sky
       }
       catch(ProgramBodyActivator.EMissingArgs notfound)
       {
+        Console.WriteLine();
         Console.WriteLine(notfound.ToMessageWithType());
+        Console.WriteLine();
         generalSyntax();
       }
       catch(ProgramBodyActivator.ENotFound notfound) when (activator != null)
       {
+        Console.WriteLine();
         Console.WriteLine(notfound.ToMessageWithType());
+        Console.WriteLine();
         dumpPrograms(activator);
       }
       catch(Exception error)
@@ -47,10 +51,17 @@ namespace sky
 
     private static void generalSyntax()
     {
-      Console.WriteLine("Process activator general syntax:");
-      Console.WriteLine("  > sky $app_process_name [app-args]");
-      Console.WriteLine("");
+      Console.WriteLine("SKY Process Activator v2022.07");
+      Console.WriteLine();
+      Console.WriteLine("Command syntax:");
+      Console.WriteLine();
+      Console.WriteLine("  > sky [@cfg] $app_process_name [app-args]");
+      Console.WriteLine();
+      Console.WriteLine("  @cfg - optional path to config file, e.g. `/opt/sky/toolset1.laconf`");
+      Console.WriteLine("         if not specified system looks for co-located `sky.laconf`");
+      Console.WriteLine();
       Console.WriteLine("  $app_process_name - is the name of target process, e.g. `gluec`");
+      Console.WriteLine();
     }
 
     private static void dumpPrograms(ProgramBodyActivator activator)
