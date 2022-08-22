485fa05f785cff562c71b709b112c060cb9afd54
Dmitriy Khmaladze <d@zhabis.com>
2022-08-22 12:10:36 -0400

    Nuget 3.0.0.3 push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.0.0.2
-set HEADLINE=Fix net6.0 packaging
+set VER=3.0.0.3
+set HEADLINE=HttpStatusException and Bixas
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
