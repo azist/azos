13ce54252056d47afcc5b6100fd8dd4b38298901
Dmitriy Khmaladze <d@zhabis.com>
2022-08-31 16:37:49 -0400

    #757 add SkyodDaemon

--- src/Azos.Sky.Server/Apps/Hosting/Skyod/SkyodDaemon.cs
+++ src/Azos.Sky.Server/Apps/Hosting/Skyod/SkyodDaemon.cs
@@ -30,15 +30,111 @@ namespace Azos.Sky.Server.Apps.Hosting.Skyod
     protected override void Destructor()
     {
       base.Destructor();
- //     DisposeAndNull(ref m_Chain);
+      cleanup();
+    }
+
+    private void cleanup()
+    {
+      DisposeAndNull(ref m_Chain);
+      m_Sets.ForEach(c => this.DontLeak(() => c.Dispose()));
+      m_Sets.Clear();
     }
 
     private Registry<SoftwareSet> m_Sets;
     private Thread m_Thread;
     private AutoResetEvent m_Wait;
+    private Daemon m_Chain;
 
     public override string ComponentLogTopic => SysConsts.LOG_TOPIC_SKYOD;
-    public override bool InstrumentationEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
+    [Config, ExternalParameter(CoreConsts.EXT_PARAM_GROUP_INSTRUMENTATION)]
+    public override bool InstrumentationEnabled { get; set; }
+
+    protected override void DoConfigure(IConfigSectionNode node)
+    {
+      base.DoConfigure(node);
+      cleanup();
+      if (node == null) return;
+
+      foreach (var nset in node.ChildrenNamed(SoftwareSet.CONFIG_SOFTWARE_SET_SECTION))
+      {
+        var set = FactoryUtils.MakeDirectedComponent<SoftwareSet>(this, nset, typeof(SoftwareSet), new[] { nset });
+        m_Sets.Register(set).IsTrue("Unique software set name `{0}`".Args(set.Name));
+      }
+    }
+
+    protected override void DoStart()
+    {
+      base.DoStart();
+
+      (m_Sets.Count > 0).IsTrue("Configured software sets");
+
+      if (m_Chain != null)
+      {
+        WriteLogFromHere(MessageType.Trace, "Starting: {0}/{1}".Args(m_Chain.ServiceDescription, m_Chain.StatusDescription));
+        m_Chain.Start();
+        WriteLogFromHere(MessageType.Trace, "Started: {0}/{1}".Args(m_Chain.ServiceDescription, m_Chain.StatusDescription));
+      }
+
+      m_Wait = new AutoResetEvent(false);
+
+      m_Thread = new Thread(threadBody);
+      m_Thread.IsBackground = false;
+      m_Thread.Name = nameof(SkyodDaemon);
+      m_Thread.Start();
+    }
+
+    protected override void DoSignalStop()
+    {
+      if (m_Chain != null)
+      {
+        m_Chain.SignalStop();
+      }
+
+      m_Wait.Set();
+      base.DoSignalStop();
+    }
+
+    protected override void DoWaitForCompleteStop()
+    {
+      if (m_Thread != null)
+      {
+        m_Thread.Join();
+        m_Thread = null;
+      }
+
+      DisposeAndNull(ref m_Wait);
+
+      if (m_Chain != null)
+      {
+        m_Chain.WaitForCompleteStop();
+      }
+
+      base.DoWaitForCompleteStop();
+    }
+
+
+    private void threadBody()
+    {
+      const int SLICE_MS_LOW = 150;
+      const int SLICE_MS_HIGH = 250;
+
+      var rel = Guid.NewGuid();
+
+      while (Running) //<--- LOOP ---
+      {
+        try
+        {
+         // doWorkOnce(rel);
+        }
+        catch (Exception error)
+        {
+          WriteLogFromHere(MessageType.CatastrophicError, "..scanAllOnce() leaked: " + error.ToMessageWithType(), error, related: rel);
+        }
+
+        m_Wait.WaitOne(Ambient.Random.NextScaledRandomInteger(SLICE_MS_LOW, SLICE_MS_HIGH));
+      }//while
+
+    }
   }
 }
