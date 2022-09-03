6e98c681304c9a60fd40cc9e6eb9c0922f1c60a2
Dmitriy Khmaladze <d@zhabis.com>
2022-09-03 17:10:02 -0400

    #757 Build nuget

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.0.0.3
-set HEADLINE=HttpStatusException and Bixas
+set VER=3.0.0.4
+set HEADLINE=HGov Wip
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
@@ -33,7 +33,7 @@ nuget pack Azos.AuthKit.nuspec -Version %VER% %WARNING% -OutputDirectory "%OUT%"
 nuget pack Azos.AuthKit.Server.nuspec -Version %VER% %WARNING% -OutputDirectory "%OUT%" -Properties icon="%ICON%";headline="%HEADLINE%"
 nuget pack Azos.AuthKit.Server.MySql.nuspec -Version %VER% %WARNING% -OutputDirectory "%OUT%" -Properties icon="%ICON%";headline="%HEADLINE%"
 
-rem goto :FINISH
+goto :FINISH
 rem --------------------------
 
 nuget push "%OUT%\Azos.%VER%.nupkg" %AZIST_NUGET_API_KEY% 
