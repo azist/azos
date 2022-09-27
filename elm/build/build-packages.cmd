c006a7d3e5e548e548ac2f2abe93ed7d5721bf48
Dmitriy Khmaladze <d@zhabis.com>
2022-09-27 11:37:47 -0400

    Nuget

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.0.0.26
-set HEADLINE=#776 WebConsole wmbedded resource paths
+set VER=3.0.0.27
+set HEADLINE=#778, #779 Config enhancements
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
