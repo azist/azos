41c88aa7fc2fe63f04ec32e633a1ec9b4d09848b
Dmitriy Khmaladze <d@zhabis.com>
2022-10-03 12:42:34 -0400

    #780 add gov cmdlets to induce gc

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.0.0.27
-set HEADLINE=#778, #779 Config enhancements
+set VER=3.0.0.28
+set HEADLINE=#780 daemon cmdlets and activation vars
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
