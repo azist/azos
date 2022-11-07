e8f6feb009e07b895046c72573c070e0d34f914e
Dmitriy Khmaladze <d@zhabis.com>
2022-11-07 12:03:14 -0500

    #800 Gdid module added dyn resolution and Nuget push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.0.0.49
-set HEADLINE=#799 Config.TreeBuilder
+set VER=3.0.0.50
+set HEADLINE=#800 DynamicFlowScope for modules added to Gdid resolver
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
