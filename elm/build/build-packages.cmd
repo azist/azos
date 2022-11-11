54df6b2f8ec7c4e24e19e77bc669086f48d76e6c
Dmitriy Khmaladze <d@zhabis.com>
2022-11-11 12:19:24 -0500

    #807 AsString

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.0.0.61
-set HEADLINE=#807 OrgUnit EntityId
+set VER=3.0.0.62
+set HEADLINE=#807 OrgUnit EntityId.AsString
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
