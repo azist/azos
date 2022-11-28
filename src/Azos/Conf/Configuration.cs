47a91b664f42444b7aa7267e3dda4dd1fd969338
Dmitriy Khmaladze <d@zhabis.com>
2022-11-28 15:58:26 -0500

    #814 Add  pragma section

--- src/Azos/Conf/Configuration.cs
+++ src/Azos/Conf/Configuration.cs
@@ -37,6 +37,7 @@ namespace Azos.Conf
     public const string CONFIG_INCLUDE_PRAGMA_COPY_ATTR = "copy";
     public const string CONFIG_INCLUDE_PRAGMA_OVERRIDE_ATTR = "override";
     public const string CONFIG_INCLUDE_PRAGMA_PREPROCESS_ALL_INCLUDES_ATTR = "pre-process-all-includes";
+    public const string CONFIG_INCLUDE_PRAGMA_WITH_SECTION = "with";
 
     public const string DEFAULT_VAR_ESCAPE = "$(###)";
     public const string DEFAULT_VAR_START = "$(";
