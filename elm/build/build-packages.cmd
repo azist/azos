1fcee07e00012d845462b78d2a9e24b4515fd318
Dmitriy Khmaladze <d@zhabis.com>
2022-11-28 15:59:21 -0500

    #814 Nuget push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.0.0.66
-set HEADLINE=#801 #812 Basic auth with entityid
+set VER=3.0.0.67
+set HEADLINE=#814 with in includes
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
