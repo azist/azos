6fb1a7b231c9121796947bd29eaa4ddcffb960ab
Dmitriy Khmaladze <d@zhabis.com>
2022-12-23 12:12:24 -0500

    #817 CMS sec

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.0.0.68
-set HEADLINE=#815 PreProcessSubjectIdCredentials hook
+set VER=3.0.0.69
+set HEADLINE=#817 Cms security
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
