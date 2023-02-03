1b6b267ef5ec5006e8e2a5a0e63f0efc4a4f23a2
Dmitriy Khmaladze <d@zhabis.com>
2023-02-03 17:12:04 -0500

    #828 Not needed (at least for now)

--- src/Azos.Sky/Fabric/Patterns/Workflow.cs
+++ src/Azos.Sky/Fabric/Patterns/Workflow.cs
@@ -1,109 +1,120 @@
-﻿/*<FILE_LICENSE>
- * Azos (A to Z Application Operating System) Framework
- * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
- * See the LICENSE file in the project root for more information.
-</FILE_LICENSE>*/
+﻿/////////////////////*<FILE_LICENSE>
+//////////////////// * Azos (A to Z Application Operating System) Framework
+//////////////////// * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+//////////////////// * See the LICENSE file in the project root for more information.
+////////////////////</FILE_LICENSE>*/
 
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+////////////////////using System;
+////////////////////using System.Collections.Generic;
+////////////////////using System.Reflection;
+////////////////////using System.Text;
+////////////////////using System.Threading;
+////////////////////using System.Threading.Tasks;
 
-using Azos.Apps;
-using Azos.Apps.Injection;
-using Azos.Data;
-using Azos.Security;
-using Azos.Serialization.Bix;
-using Azos.Serialization.JSON;
-using Azos.Serialization.Slim;
+////////////////////using Azos.Apps;
+////////////////////using Azos.Apps.Injection;
+////////////////////using Azos.Data;
+////////////////////using Azos.Security;
+////////////////////using Azos.Serialization.Bix;
+////////////////////using Azos.Serialization.JSON;
+////////////////////using Azos.Serialization.Slim;
 
-namespace Azos.Sky.Fabric.Patterns
-{
-  /// <summary>
-  /// Sagas are multi-part processing chains with possibly compensating transactions.
-  /// They represent a mixture of "Saga" design pattern with "supervisor" actor model pattern
-  /// </summary>
-  public abstract class Workflow<TParameters, TState> : Fiber<TParameters, TState> where TParameters : FiberParameters
-                                                                                   where TState : WorkflowState
-  {
-    //public abstract StartPhase{ get;}
+////////////////////namespace Azos.Sky.Fabric.Patterns
+////////////////////{
+////////////////////  /// <summary>
+////////////////////  /// Sagas are multi-part processing chains with possibly compensating transactions.
+////////////////////  /// They represent a mixture of "Saga" design pattern with "supervisor" actor model pattern
+////////////////////  /// </summary>
+////////////////////  public abstract class Workflow<TParameters, TState> : Fiber<TParameters, TState> where TParameters : FiberParameters
+////////////////////                                                                                   where TState : WorkflowState
+////////////////////  {
+////////////////////    //public abstract StartPhase{ get;}
 
-    //public FiberStep Loop()
-    //{
-    //
-    //}
+////////////////////    //public WorkflowPhase CurrentPhase();
 
-    //public FiberStep CurrentPhase();
-    //public FiberStep NextPhase(Atom phase);
-  }
+////////////////////    //public WorkflowPhase ExecutePhaseSlice()
+////////////////////    //{
+////////////////////    //
+////////////////////    //}
 
-
-  /// <summary>
-  /// EXAMPLE ONLY
-  /// </summary>
-  public abstract class WorkflowState : FiberState
-  {
-    //Only one phase can be active
-    protected abstract class Current : Slot
-    {
-      [Field(Required = true, Description = "Current Phase")]
-      public Atom CurrentPhase { get; set; }
-    }
+////////////////////    //public FiberStep NextPhase(Atom phase);
+////////////////////  }
 
 
-    //Only one phase can be active
-    protected abstract class Phase : Slot
-    {
-      [Field(Required = true, Description = "When Phase started")]
-      public DateTime StartUtc { get;  set; }
+////////////////////  /// <summary>
+////////////////////  /// EXAMPLE ONLY
+////////////////////  /// </summary>
+////////////////////  public abstract class WorkflowState : FiberState
+////////////////////  {
+////////////////////    //Only one phase can be active at a time
+////////////////////    [Bix("a084e6a9-d6c9-4c10-857c-dbd345038462")]
+////////////////////    protected sealed class current : Slot
+////////////////////    {
+////////////////////      [Field(Required = true)]
+////////////////////      public Atom CurrentPhase { get; set; }
 
-      [Field(Required = true, Description = "Collection of work items")]
-      public List<Item> Items {  get; set; }
-    }
-
-    /// <summary>
-    /// Items can happen simultaneously within phase
-    /// </summary>
-    [BixJsonHandler(ThrowOnUnresolvedType = true)]
-    protected abstract class Item : AmorphousTypedDoc
-    {
-      [Field(Required = true, Description = "A part which this chapter is in")]
-      public Atom Phase { get; set; }
-
-      protected override void AddJsonSerializerField(Schema.FieldDef def, JsonWritingOptions options, Dictionary<string, object> jsonMap, string name, object value)
-      {
-        if (def?.Order == 0)
-        {
-          BixJsonHandler.EmitJsonBixDiscriminator(this, jsonMap);
-        }
-
-        base.AddJsonSerializerField(def, options, jsonMap, name, value);
-      }
-    }
-
-    private static readonly Atom SLOT_DATA = Atom.Encode("d");
+////////////////////      [Field]
+////////////////////      public JsonDataMap Data{ get; set; }
+////////////////////    }
 
 
-    public Atom CurrentPhase => Get<Current>(SLOT_DATA)?.CurrentPhase ?? Atom.ZERO;
+////////////////////    //Only one phase can be active
+////////////////////    protected sealed class phase : Slot
+////////////////////    {
+////////////////////      [Field(Required = true, Description = "When Phase started")]
+////////////////////      public DateTime StartUtc { get;  set; }
+
+////////////////////      [Field(Required = true, Description = "Collection of work items")]
+////////////////////      public List<Item> Items {  get; set; }
+////////////////////    }
+
+////////////////////    /// <summary>
+////////////////////    /// Items can happen simultaneously within phase
+////////////////////    /// </summary>
+////////////////////    [BixJsonHandler(ThrowOnUnresolvedType = true)]
+////////////////////    protected abstract class Item : AmorphousTypedDoc
+////////////////////    {
+////////////////////      [Field(Required = true, Description = "A part which this chapter is in")]
+////////////////////      public Atom Phase { get; set; }
+
+////////////////////      protected override void AddJsonSerializerField(Schema.FieldDef def, JsonWritingOptions options, Dictionary<string, object> jsonMap, string name, object value)
+////////////////////      {
+////////////////////        if (def?.Order == 0)
+////////////////////        {
+////////////////////          BixJsonHandler.EmitJsonBixDiscriminator(this, jsonMap);
+////////////////////        }
+
+////////////////////        base.AddJsonSerializerField(def, options, jsonMap, name, value);
+////////////////////      }
+////////////////////    }
+
+////////////////////    private static readonly Atom SLOT_CURRENT = Atom.Encode("_______c");
 
 
-    //public int DonutCount
-    //{
-    //  get => Get<counts>(SLOT_DATA)?.DonutCount ?? 0;
-    //  set => Set<counts>(SLOT_DATA, data => data.DonutCount = value);
-    //}
+////////////////////    public Atom CurrentPhase => Get<current>(SLOT_CURRENT)?.CurrentPhase ?? Atom.ZERO;
 
-    //public int CakeCount
-    //{
-    //  get => Get<counts>(SLOT_DATA)?.CakeCount ?? 0;
-    //  set => Set<counts>(SLOT_DATA, data => data.CakeCount = value);
-    //}
+////////////////////    public void SetCurrentPhase(Atom phase)
+////////////////////    {
+////////////////////      phase.IsValidNonZero(nameof(phase));
+////////////////////      Set()
+////////////////////      Set<Current>(SLOT_DATA, data => data.CurrentPhase = phase);
+////////////////////    }
 
-    //  public byte[] GetImage(runtime) => Get<cakeimg>(SLOT_IMG)?.EnsureLoaded(runtime) ?? null;
+////////////////////    //public int DonutCount
+////////////////////    //{
+////////////////////    //  get => Get<counts>(SLOT_DATA)?.DonutCount ?? 0;
+////////////////////    //  set => Set<counts>(SLOT_DATA, data => data.DonutCount = value);
+////////////////////    //}
 
-  }
+////////////////////    //public int CakeCount
+////////////////////    //{
+////////////////////    //  get => Get<counts>(SLOT_DATA)?.CakeCount ?? 0;
+////////////////////    //  set => Set<counts>(SLOT_DATA, data => data.CakeCount = value);
+////////////////////    //}
+
+////////////////////    //  public byte[] GetImage(runtime) => Get<cakeimg>(SLOT_IMG)?.EnsureLoaded(runtime) ?? null;
+
+////////////////////  }
 
 
-}
+////////////////////}
