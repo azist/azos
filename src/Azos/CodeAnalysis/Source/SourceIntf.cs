be215c7900341b61fba13edbcee737c90cb360be
Dmitriy Khmaladze <d@zhabis.com>
2023-03-17 14:52:21 -0400

    #731 ISourceText refactoring started

--- src/Azos/CodeAnalysis/Source/SourceIntf.cs
+++ src/Azos/CodeAnalysis/Source/SourceIntf.cs
@@ -5,20 +5,25 @@
 </FILE_LICENSE>*/
 
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Azos.CodeAnalysis.Source
 {
-
 #warning AZ #731 rewrite async deserializer core - add CHUNKING ASYNC/SYNC block handling with "ChunkEof" which triggers next async fetch in chunks
   /// <summary>
-  /// Represents source code input text (usually text from file)
+  /// Represents an abstraction of a source code text which comes from various sources such as strings, streams or files
   /// </summary>
-  public interface ISourceText
+  public interface ISourceText : Collections.INamed//Provides a meaningful name to a source code
   {
     /// <summary>
-    /// Resets source to beginning
+    /// Indicates what language this source is supplied in
     /// </summary>
-    void Reset();
+    Language Language { get; }
+
+    /// <summary>
+    /// Resets source to the very beginning
+    /// </summary>
+  //  void Reset();
 
     /// <summary>
     /// Indicates whether last character has been read
@@ -36,14 +41,45 @@ namespace Azos.CodeAnalysis.Source
     char PeekChar();
 
     /// <summary>
-    /// Indicates what language this source is supplied in
+    /// How much memory (in characters) is used for buffering reads from source.
+    /// At first the system reads the source Segment asynchronously into the allocated buffer.
+    /// Then, the system uses sync functions <see cref="ReadChar"/> and <see cref="PeekChar"/>
+    /// to get character efficiently from memory.
     /// </summary>
-    Language Language { get; }
+    int BufferSize { get; }
 
     /// <summary>
-    /// Provides a meaningful name to a source code
+    /// Character length of a pre-fetched character segment which was read from source into memory buffer,
+    /// possibly asynchronously
     /// </summary>
-    string Name { get; }
+    int SegmentLength { get; }
+
+    /// <summary>
+    /// Character position within a Segment which was read from source, possibly asynchronously.
+    /// The caller may trigger an async fetch to get more data from the underlying source asynchronously
+    /// when this number approaches <see cref="SegmentLength"/>.
+    /// When this property goes past <see cref="SegmentLength"/> the system calls <see cref="FetchBufferAsync"/>
+    /// automatically, however you can check these properties yourself and trigger a fully async fetch yourself
+    /// </summary>
+    int SegmentPosition { get; }
+
+    /// <summary>
+    /// Return true when implementation deems <see cref="SegmentPosition"/> get close to <see cref="SegmentLength"/>.
+    /// Inspect this property to trigger async call to <see cref="FetchSegmentAsync"/>
+    /// </summary>
+    bool NearEndOfSegment { get; }
+
+    /// <summary>
+    /// True, if the fetched segment is the last one as the source has been read to the very end.
+    /// The call to <see cref="FetchSegmentAsync"/> is not needed/does nothing
+    /// </summary>
+    bool IsLastSegment { get; }
+
+    /// <summary>
+    /// Fetches more - the next character segment into buffer asynchronously, then use synchronous function to efficiently get data
+    /// using sync <see cref="ReadChar"/> and <see cref="PeekChar"/>
+    /// </summary>
+    ValueTask FetchSegmentAsync();
   }
 
 
