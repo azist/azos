109ea9a75ed94ecd77352670c9efaef57e2f8a1d
Dmitriy Khmaladze <d@zhabis.com>
2023-03-18 16:17:16 -0400

    #731 Implement StringSource

--- src/Azos/CodeAnalysis/Source/XStringSource.cs
+++ src/Azos/CodeAnalysis/Source/XStringSource.cs
@@ -4,15 +4,14 @@
  * See the LICENSE file in the project root for more information.
 </FILE_LICENSE>*/
 
-using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Azos.CodeAnalysis.Source
 {
   /// <summary>
-  /// Provides source code from a fully materialized string value
+  /// Provides source code from string
   /// </summary>
-  public sealed class StringSource : ISourceText
+  public class XStringSource : ISourceText
   {
     private Language m_Language;
     private string m_Name;
@@ -22,25 +21,40 @@ namespace Azos.CodeAnalysis.Source
 
     public StringSource(string source, Language language = null, string name = null)
     {
-      m_Source = source.NonNull(nameof(source));
-      m_Language = language ?? UnspecifiedLanguage.Instance;
+      m_Language = language;
+      m_Source = source;
       m_Length = source.Length;
-      m_Name = name ?? Guid.NewGuid().ToString();
+      m_Name = name;
     }
 
     #region ISourceText Members
-    public string Name => m_Name;
-    public Language Language => m_Language;
+
+    public void Reset() => m_Position = 0;
+
     public bool EOF => m_Position >= m_Length;
+
     public char ReadChar() => m_Position >= m_Length ? (char)0 : m_Source[m_Position++];
+
     public char PeekChar() => m_Position >= m_Length ? (char)0 : m_Source[m_Position];
-    public Task FetchSegmentAsync() => Task.CompletedTask;
-    public int BufferSize => m_Source.Length;
-    public int SegmentLength => m_Source.Length;
-    public int SegmentPosition => m_Position;
-    public bool NearEndOfSegment => false;
-    public bool IsLastSegment => true;
+
+    public Language Language => m_Language ?? UnspecifiedLanguage.Instance;
+
+    /// <summary>
+    /// Provides a handy way to name an otherwise-anonymous string source code,
+    /// This property is like a "file name" only data is kept in a string
+    /// </summary>
+    public string Name => m_Name ?? string.Empty;
+
     #endregion
+  }
+
+
+  /// <summary>
+  /// Represents a list of strings used as source text
+  /// </summary>
+  public class StringSourceList : List<StringSource>
+  {
+
   }
 }
 
