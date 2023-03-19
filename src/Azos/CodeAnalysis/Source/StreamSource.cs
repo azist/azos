8acd7ce017975cc9419da98c468f11a0e1cf4350
Dmitriy Khmaladze <d@zhabis.com>
2023-03-18 21:50:16 -0400

    #731 Shelve StreamSource implementation changes

--- src/Azos/CodeAnalysis/Source/StreamSource.cs
+++ src/Azos/CodeAnalysis/Source/StreamSource.cs
@@ -4,58 +4,97 @@
  * See the LICENSE file in the project root for more information.
 </FILE_LICENSE>*/
 
+using System;
 using System.Text;
 using System.IO;
+using System.Buffers;
+using System.Threading.Tasks;
 
 namespace Azos.CodeAnalysis.Source
 {
-#warning AZ #731 Needs `ChunkEof` for async processing so we can `await streamSource.GetNextChunkAsync(1024)`
   /// <summary>
-  /// Represents source code stored in a stream
+  /// Represents source code stored in a stream which can be asynchronously read in segments
   /// </summary>
-  public class StreamSource : StreamReader, ISourceText
+  public class StreamSource : DisposableObject, ISourceText
   {
-    /// <summary>
-    /// Constructs stream source with specified language and default encoding
-    /// </summary>
-    public StreamSource(Stream stream, Language language, string name = null)
-      : base(stream, Encoding.UTF8, true, 1024, leaveOpen: true)//#837
-    {
-      m_Language = language;
-      m_Name = name;
-    }
-
     /// <summary>
     /// Constructs stream source with specified language and encoding
     /// </summary>
     public StreamSource(Stream stream, Encoding encoding, Language language, string name = null)
-      : base(stream, encoding, true, 1024, leaveOpen: true)//#837
     {
-      m_Language = language;
-      m_Name = name;
+      m_Stream = stream.NonDisposed(nameof(stream));
+      m_Language = language ?? UnspecifiedLanguage.Instance;
+      m_Encoding = encoding ?? Encoding.UTF8;
+      m_Name = name ?? Guid.NewGuid().ToString();
+
+      m_Buffer = System.Buffers.MemoryPool<char>.Shared.Rent(1024).Memory;
     }
 
+    private Stream m_Stream;
+    private Encoding m_Encoding;
     private Language m_Language;
     private string m_Name;
 
-    public void Reset()
+    private int m_SegmentLength;
+    private int m_SegmentPosition;
+    private int m_SegmentTailThreshold;
+    private Memory<char> m_Buffer;
+    private bool m_StreamEof;
+
+    #region ISourceText Members
+    public string Name => m_Name;
+    public Language Language => m_Language;
+    public bool EOF => m_StreamEof && m_SegmentPosition >= m_SegmentLength;
+    public int BufferSize => m_Buffer.Length;
+    public int SegmentLength => m_SegmentLength;
+    public int SegmentPosition => m_SegmentPosition;
+    public bool NearEndOfSegment => (m_SegmentLength - m_SegmentPosition) < m_SegmentTailThreshold;
+    public bool IsLastSegment => m_StreamEof;
+
+
+    public char ReadChar()
     {
-      BaseStream.Position = 0;
-      DiscardBufferedData();
+      if (!prepData()) return (char)0;
+      return m_Buffer.Span[m_SegmentPosition++];
     }
 
-    /// <summary>
-    /// Returns source's name
-    /// </summary>
-    public string Name => m_Name ?? string.Empty;
+    public char PeekChar()
+    {
+      if (!prepData()) return (char)0;
+      return m_Buffer[m_SegmentPosition];
+    }
 
-    public bool EOF => EndOfStream;
+    public async Task FetchSegmentAsync()
+    {
+      using var bin = MemoryPool<byte>.Shared.Rent(1024);
+      var got = await m_Stream.ReadAsync(bin.Memory).ConfigureAwait(false);
 
-    public char ReadChar() => (char)Read();
+      if (got==0)//eof
+      {
+        m_StreamEof = true;
+        return;
+      }
 
-    public char PeekChar() => (char)Peek();
+      var gotc = m_Encoding.GetChars(bin.Memory.Slice(0, got).Span, new Span<char>());
 
-    public Language Language => m_Language ?? UnspecifiedLanguage.Instance;
+     // m_Encoding.GetMaxByteCount(charCount);
 
+    }
+    #endregion
+
+    #region .pvt
+    private bool prepData()
+    {
+      var segmentEof = m_SegmentPosition >= m_SegmentLength;
+      if (segmentEof)
+      {
+        if (m_StreamEof) return false;//real EOF
+        FetchSegmentAsync().Await();//try to fetch more from the underlying stream
+        segmentEof = m_SegmentPosition >= m_SegmentLength;
+        if (segmentEof) return false;//nothing extra was fetched
+      }
+      return true;
+    }
+    #endregion
   }
 }
