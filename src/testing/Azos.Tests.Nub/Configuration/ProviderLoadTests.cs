1492fd82a640268bbb1e95a42afd552b7c3e60ec
Dmitriy Khmaladze <d@zhabis.com>
2023-03-25 18:33:59 -0400

    #845 Add test for Utf16 BE LE encoding load from file

--- src/testing/Azos.Tests.Nub/Configuration/ProviderLoadTests.cs
+++ src/testing/Azos.Tests.Nub/Configuration/ProviderLoadTests.cs
@@ -1,4 +1,4 @@
-/*<FILE_LICENSE>
+﻿/*<FILE_LICENSE>
  * Azos (A to Z Application Operating System) Framework
  * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
  * See the LICENSE file in the project root for more information.
@@ -19,18 +19,48 @@ namespace Azos.Tests.Nub.Configuration
   public class ProviderLoadTests
   {
     [Run]
-    public void LaconicCtorFileName()
+    public void LaconicCtorFileName_UTF8()
     {
       var sut = new LaconicConfiguration("nub-test.laconf");
-      sut.See();
+      Aver.AreEqual("utf8", sut.Root.ValOf("encoding"));
+      check(sut);
+    }
+
+    [Run]
+    public void LaconicCtorFileName_UTF16BE()
+    {
+      var sut = new LaconicConfiguration("nub-test-utf16-be.laconf");
+      Aver.AreEqual("utf16be", sut.Root.ValOf("encoding"));
+      check(sut);
+    }
+
+    [Run]
+    public void LaconicCtorFileName_UTF16LE()
+    {
+      var sut = new LaconicConfiguration("nub-test-utf16-le.laconf");
+      Aver.AreEqual("utf16le", sut.Root.ValOf("encoding"));
+      check(sut);
     }
 
 
     [Run]
-    public void ProviderLoadFromFile_Laconic()
+    public void ProviderLoadFromFile_Laconic_UTF8()
     {
       var sut = Conf.Configuration.ProviderLoadFromFile("nub-test.laconf");
-      sut.See();
+      Aver.AreEqual("utf8", sut.Root.ValOf("encoding"));
+      check(sut);
     }
+
+    private void check(Conf.Configuration sut)
+    {
+      sut.See();
+      Aver.AreEqual("𝄞𝄢doremi", sut.Root.ValOf("music"));
+      Aver.AreEqual("我能吞下玻璃而不傷身體", sut.Root.ValOf("chinese"));
+      Aver.AreEqual("ვეპხის ტყაოსანი შოთა რუსთაველი", sut.Root.ValOf("rustaveli"));
+      Aver.AreEqual("ЭЮЯ?", sut.Root.ValOf("russian"));
+      Aver.AreEqual("simpletext", sut.Root.ValOf("english"));
+
+    }
+
   }
 }
