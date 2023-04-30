9a358cf0c49f2d761fc44e08c3c2c4239b2330ee
Dmitriy Khmaladze <d@zhabis.com>
2023-04-29 20:51:04 -0400

    #859 refactoring

---
+++ src/Azos/Web/WebCallExtensions.Aspects.cs
@@ -0,0 +1,91 @@
+/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Azos.Apps;
+using Azos.Serialization.JSON;
+
+namespace Azos.Web
+{
+  public static partial class WebCallExtensions
+  {
+    /// <summary> Marker interface for traits applicable to making web calls</summary>
+    public interface ICallerAspect { }
+
+    /// <summary> Indicates that the caller is capable of providing DistributedCallFlow context </summary>
+    public interface IDistributedCallFlowAspect : ICallerAspect
+    {
+      /// <summary> If non-empty provides header name to be used for sending DistributedCallFlow object, otherwise a default name is used</summary>
+      string DistributedCallFlowHeader {  get; }
+
+      /// <summary>
+      /// Gets the distributed call flow or NULL if not present or not enabled
+      /// </summary>
+      DistributedCallFlow GetDistributedCallFlow();
+    }
+
+    /// <summary>
+    /// Designates the caller as authentication provider for remote impersonation - provides
+    /// header name and header value if caller impersonation is used
+    /// </summary>
+    public interface IAuthImpersonationAspect : ICallerAspect
+    {
+      /// <summary>
+      /// When non empty provides header name used for impersonation, otherwise standard HTTP `Authorization` header is used
+      /// </summary>
+      string AuthImpersonationHeader {  get; }
+
+      /// <summary>
+      /// Gets auth header value if impersonation is used or null.
+      /// Optional functor to get identity context of the caller
+      /// </summary>
+      Task<string> GetAuthImpersonationHeaderAsync(Func<object> fGetIdentityContext);
+    }
+
+    /// <summary>
+    /// Designates the caller as an entity reacting to request body errors detected by server.
+    /// Typically an aspect will log the body that could not be processed.
+    /// Body errors arise when client-supplied body is not processable e.g. when requested JSON can not be parsed
+    /// </summary>
+    public interface IRequestBodyErrorAspect : ICallerAspect
+    {
+      /// <summary>
+      /// When non empty provides header name used for body error detection, otherwise request body errors are not detected
+      /// </summary>
+      string BodyErrorHeader { get; }
+
+      /// <summary>
+      /// Called after client receives a named header signaling a presence of body processing problem.
+      /// The implementor typically captures the original requested object by logging its contents elsewhere
+      /// </summary>
+      /// <param name="uri">Originally called URI</param>
+      /// <param name="method">Original HTTP method</param>
+      /// <param name="body">The original body object that was supplied by client caller and caused server error</param>
+      /// <param name="contentType">The original content type passed by the caller</param>
+      /// <param name="options">The original JsonWritingOptions supplied by the caller</param>
+      /// <param name="request">Request sent to server</param>
+      /// <param name="response">Response gotten from server</param>
+      /// <param name="isSuccess">True if server responded with 200</param>
+      /// <param name="rawResponseContent">Fetched raw response content (such as error) or null</param>
+      /// <param name="bodyErrorValues">Collection of values for header named by `BodyErrorHeader`</param>
+      Task ProcessBodyErrorAsync(string uri,
+                        HttpMethod method,
+                        object body,
+                        string contentType,
+                        JsonWritingOptions options,
+                        HttpRequestMessage request,
+                        HttpResponseMessage response,
+                        bool isSuccess,
+                        string rawResponseContent,
+                        IEnumerable<string> bodyErrorValues);
+    }
+
+  }
+}
---
+++ src/Azos/Web/WebCallExtensions.Call.cs
@@ -0,0 +1,137 @@
+/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+using Azos.Apps;
+using Azos.Data;
+using Azos.Serialization.JSON;
+
+namespace Azos.Web
+{
+  public static partial class WebCallExtensions
+  {
+    /// <summary>
+    /// Calls an arbitrary HttpMethod with the specified entity body on a remote endpoint returning a JsonDataMap result on success.
+    /// A body is a string, a binary blob or object converted to json using JsonWritingOptions.
+    /// Optional fGetIdentityContext is used to create impersonation auth tokens - defines what identity is for the call.
+    /// </summary>
+    public static async Task<JsonDataMap> CallAndGetJsonMapAsync(this HttpClient client,
+                                                                      string uri,
+                                                                      HttpMethod method,
+                                                                      object body,
+                                                                      string contentType = null,
+                                                                      JsonWritingOptions options = null,
+                                                                      bool fetchErrorContent = true,
+                                                                      IEnumerable<KeyValuePair<string, string>> requestHeaders = null,
+                                                                      Func<object> fGetIdentityContext = null,
+                                                                      JsonReadingOptions ropt = null)
+    {
+      client.NonNull(nameof(client));
+      method.NonNull(nameof(method));
+      uri.NonNull(nameof(uri));
+
+      HttpContent content = null;
+
+      if (body != null)
+      {
+        if (body is string strbody)
+        {
+          content = new StringContent(strbody, Encoding.UTF8, contentType.Default(ContentType.TEXT));
+        }
+        else if (body is byte[] binbody)
+        {
+          content = new ByteArrayContent(binbody);
+          var ctp = new MediaTypeHeaderValue(contentType.Default(ContentType.BINARY))
+          {
+            CharSet = Encoding.UTF8.WebName
+          };
+          content.Headers.ContentType = ctp;
+        }
+        else
+        {
+          var jsonToSend = body.ToJson(options ?? JsonWritingOptions.CompactRowsAsMap);
+          content = new StringContent(jsonToSend, Encoding.UTF8, ContentType.JSON);
+        }
+      }
+
+      using (var request = new HttpRequestMessage(method, uri))
+      {
+        if (content != null)
+          request.Content = content;
+
+        if (requestHeaders != null)
+        {
+          foreach(var pair in requestHeaders)
+            request.Headers.Add(pair.Key, pair.Value);
+        }
+
+        if (client is IDistributedCallFlowAspect dca)
+        {
+          var dcf = dca.GetDistributedCallFlow();
+          if (dcf != null)
+          {
+            var hdr = dca.DistributedCallFlowHeader.Default(CoreConsts.HTTP_HDR_DEFAULT_CALL_FLOW);
+            request.Headers.Add(hdr, dcf.ToHeaderValue());
+          }
+        }
+
+        if (client is IAuthImpersonationAspect aia)
+        {
+          var token = await aia.GetAuthImpersonationHeaderAsync(fGetIdentityContext).ConfigureAwait(false);
+          if (token.IsNotNullOrWhiteSpace())
+          {
+            var hdr = aia.AuthImpersonationHeader.Default(WebConsts.HTTP_HDR_AUTHORIZATION);
+            request.Headers.Add(hdr, token);
+          }
+        }
+
+        using (var response = await client.SendAsync(request, fetchErrorContent ? HttpCompletionOption.ResponseContentRead
+                                                                                : HttpCompletionOption.ResponseHeadersRead)
+                                          .ConfigureAwait(false))
+        {
+
+          var isSuccess = response.IsSuccessStatusCode;
+          string raw = string.Empty;
+          if (isSuccess || fetchErrorContent)
+          {
+            raw = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+          }
+
+          //#834 ------------------------------------
+          if (client is IRequestBodyErrorAspect bea)
+          {
+            var hdrn = bea.BodyErrorHeader;
+            if (hdrn.IsNotNullOrWhiteSpace())
+            {
+              if (response.Headers.TryGetValues(hdrn, out var hdrValues))
+              {
+                await bea.ProcessBodyErrorAsync(uri, method, body, contentType, options, request, response, isSuccess, raw, hdrValues).ConfigureAwait(false);
+              }
+            }
+          }//#834 -----------------------------------
+
+          if (!isSuccess)
+            throw new WebCallException(StringConsts.WEB_CALL_UNSUCCESSFUL_ERROR.Args(uri.SplitKVP('?').Key.TakeLastChars(64),
+                                                                                    (int)response.StatusCode,
+                                                                                    response.StatusCode),
+                                       uri,
+                                       method.Method,
+                                       (int)response.StatusCode,
+                                       response.ReasonPhrase,
+                                       raw.TakeFirstChars(CALL_ERROR_CONTENT_MAX_LENGTH, "..."));
+
+          return (raw.JsonToDataObject(ropt: ropt ?? JsonReadingOptions.NoLimits) as JsonDataMap).NonNull(StringConsts.WEB_CALL_RETURN_JSONMAP_ERROR.Args(raw.TakeFirstChars(48)));
+        }//using response
+      }//using request
+    }
+  }
+}
