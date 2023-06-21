f5f5760e0316a9e8eb7ced9701a1a3c758839ef5
Dmitriy Khmaladze <d@zhabis.com>
2023-06-21 11:12:24 -0400

    #877 Nuget 3.5.0.6 push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.5.0.5
-set HEADLINE=#878 Chronicle shard coloring
+set VER=3.5.0.6
+set HEADLINE=#877 Bixon IEnumerable support
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
