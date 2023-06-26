b860066e86ff5b1a6f74686826fd8942adffd56f
Dmitriy Khmaladze <d@zhabis.com>
2023-06-26 14:37:41 -0400

    #883 Intfs

---
+++ src/Azos.Sky/FileGateway/IFileGateway.cs
@@ -0,0 +1,105 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azos.Apps;
+using Azos.Data;
+using Azos.Log;
+
+
+namespace Azos.Sky.FileGateway
+{
+  /// <summary>
+  /// Specifies the file creation mode
+  /// </summary>
+  public enum CreateMode
+  {
+    /// <summary>
+    /// If file exists it throws. The file may not exist at the time of the call
+    /// </summary>
+    Create,
+
+    /// <summary>
+    /// If file exists, it first truncates it to zero bytes, then post new content which replaces the existing file
+    /// </summary>
+    Replace,
+
+    /// <summary>
+    /// If file does not exist, it gets created, if it exists, it is kept as is and written-to at the specified chunk preserving other chunks
+    /// </summary>
+    Update,
+  }
+
+
+  /// <summary>
+  /// Provides file upload/download services.
+  /// The server exposes "systems" which contain named "volumes", then a file/dir is identified by
+  /// a string path rooted at volumes. The system uses <see cref="EntityId"/> as full item path in a form: `volume@system::/path/dir/file`
+  /// </summary>
+  public interface IFileGateway
+  {
+    /// <summary>
+    /// Retrieves a list of server systems which in turn contain named volumes
+    /// </summary>
+    Task<IEnumerable<Atom>> GetSystemsAsync();
+
+    /// <summary>
+    /// Retrieves a list of server volumes - named "drives" where the path is rooted at, per specific system
+    /// </summary>
+    Task<IEnumerable<Atom>> GetVolumesAsync(Atom system);
+
+    /// <summary>
+    /// Gets a listing of items at the specified path level
+    /// </summary>
+    Task<IEnumerable<ItemInfo>> GetItemListAsync(EntityId path, int recurseLevels = 0);
+
+    /// <summary>
+    /// Gets item info for a specific item or null if it does not exist
+    /// </summary>
+    Task<IEnumerable<ItemInfo>> GetItemInfoAsync(EntityId path);
+
+    /// <summary>
+    /// Create a directory. If it exists, then does nothing
+    /// </summary>
+    Task<ItemInfo> CreateDirectory(EntityId path);
+
+    /// <summary>
+    /// Creates a file and sets its content chunk at the specified offset
+    /// </summary>
+    Task<ItemInfo> CreateFile(EntityId path, CreateMode mode, long offset, byte[] content);
+
+    /// <summary>
+    /// Uploads file chunk at the specified offset. Path must exist and be a file (not a directory)
+    /// </summary>
+    Task<ItemInfo> UploadFileChunk(EntityId path, long offset, byte[] content);
+
+    /// <summary>
+    /// Downloads file chunk at the specified offset of specified size. Path must exist and be a file (not a directory)
+    /// </summary>
+    Task<byte[]> DownloadFileChunk(EntityId path, long offset, int size);
+
+    /// <summary>
+    /// Deletes an item by path
+    /// </summary>
+    Task<bool> DeleteItem(EntityId path);
+
+    /// <summary>
+    ///Renames item in-place, this is an atomic operation which can be used for "remote transactions"
+    ///when large file needs to be uploaded in chunks, you can then rename the file to a name which the
+    ///other party cal "see at once" after the upload has been finalized
+    /// </summary>
+    Task<bool> RenameItem(EntityId path, EntityId newPath);
+  }
+
+  /// <summary>
+  /// Outlines a contract for implementing logic of IFileGateway
+  /// </summary>
+  public interface IFileGatewayLogic : IFileGateway, IModule
+  {
+  }
+}
---
+++ src/Azos.Sky/FileGateway/ItemInfo.cs
@@ -0,0 +1,42 @@
+﻿/*<FILE_LICENSE>
+ * Azos (A to Z Application Operating System) Framework
+ * The A to Z Foundation (a.k.a. Azist) licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+</FILE_LICENSE>*/
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azos.Apps;
+using Azos.Data;
+using Azos.Data.Business;
+using Azos.Log;
+using Azos.Serialization.Bix;
+
+namespace Azos.Sky.FileGateway
+{
+  /// <summary>
+  /// File or Directory
+  /// </summary>
+  public enum ItemType{ File = 1, Directory = 2 };
+
+  [Bix("595824e8-da4a-4117-b0d8-e88b8a5b6e59")]
+  [Schema(Description = "Provides information of files or directories")]
+  public sealed class ItemInfo : TransientModel
+  {
+    [Field(Required = true, Description = "Full item path")]
+    public EntityId Path{ get; set; }
+
+    [Field(Required = true, Description = "A file or directory")]
+    public ItemType Type { get; set; }
+
+    [Field(Required = true, Description = "When item was created")]
+    public DateTime CreateUtc{ get; set; }
+
+    [Field(Required = true, Description = "When item was changed for the last time")]
+    public DateTime LastChangeUtc { get; set; }
+
+    [Field(Required = true, Description = "Items size in bytes or item numbers (such as directories)")]
+    public long Size { get; set; }
+  }
+}
