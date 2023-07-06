e1226574c00aa39574d4d792275cc8ba6df047f8
Dmitriy Khmaladze <d@zhabis.com>
2023-07-06 16:09:25 -0400

    #884 Nuget

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.5.0.8
-set HEADLINE=Chronicle shardding
+set VER=3.5.0.9
+set HEADLINE=#884 Xml encoding FileGateway WIP
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
