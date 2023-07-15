3b6daf31efeeecf7aa7987247f13b5710edc4dd5
Dmitriy Khmaladze <d@zhabis.com>
2023-07-15 18:19:45 -0400

    #870 interactive password entry with confirmation

--- src/Azos.Tools/Phash/SafeLogic.cs
+++ src/Azos.Tools/Phash/SafeLogic.cs
@@ -19,10 +19,42 @@ namespace Azos.Tools.Phash
 {
   public static class SafeLogic
   {
+    public static string GetPassword(IConfigSectionNode argsSafe, bool doConfirm)
+    {
+      var result = argsSafe.ValOf("pwd");
+      Console.WriteLine();
+      while(result.IsNullOrWhiteSpace())
+      {
+        Console.WriteLine("Type-in secure password: ");
+        result = ConsoleUtils.ReadPassword('*');
+      }
+      Console.WriteLine();
+
+      if (doConfirm)
+      {
+        ConsoleUtils.Warning("Make sure you remember the password and confirm it!");
+        ConsoleUtils.Warning("Otherwise your content will not be deciphered back if you delete the original unprotected files");
+        Console.WriteLine();
+        ConsoleUtils.Info("You can hit CTRL+C now to abort if needed");
+        Console.WriteLine();
+        string confirm;
+        do
+        {
+          Console.WriteLine("Re type secure password to confirm: ");
+          confirm = ConsoleUtils.ReadPassword('#');
+        }
+        while(!result.EqualsOrdSenseCase(confirm));
+      }
+
+      Console.WriteLine();
+      return result;
+    }
+
     public static void Protect(IApplication app, IConfigSectionNode argsSafe)
     {
+      var pwd = GetPassword(argsSafe, true);
       TheSafe.ProtectDirectory(argsSafe.AttrByIndex(0).Value,
-                               argsSafe.ValOf("pwd"),
+                               pwd,
                                argsSafe.ValOf("pfx").Default(TheSafe.FILE_PREFIX_SAFE),
                                argsSafe.ValOf("ext").Default(TheSafe.FILE_EXTENSION_SAFE),
                                argsSafe.Of("recurse").ValueAsBool(false),
@@ -42,8 +74,9 @@ namespace Azos.Tools.Phash
 
     public static void Unprotect(IApplication app, IConfigSectionNode argsSafe)
     {
+      var pwd = GetPassword(argsSafe, false);
       TheSafe.UnprotectDirectory(argsSafe.AttrByIndex(0).Value,
-                                argsSafe.ValOf("pwd"),
+                                pwd,
                                 argsSafe.ValOf("pfx").Default(TheSafe.FILE_PREFIX_SAFE),
                                 argsSafe.ValOf("ext").Default(TheSafe.FILE_EXTENSION_SAFE),
                                 argsSafe.Of("recurse").ValueAsBool(false),
