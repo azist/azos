c39dda76535441c8b01e5256630354ea1f2115b9
Dmitriy Khmaladze <d@zhabis.com>
2023-08-18 21:33:48 -0400

    #890

--- src/providers/Azos.AuthKit/Tools/idp/UserListVerb.cs
+++ src/providers/Azos.AuthKit/Tools/idp/UserListVerb.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Azos.Apps;
+using Azos.Data;
 using Azos.Serialization.JSON;
 
 namespace Azos.AuthKit.Tools.idp
@@ -22,7 +23,7 @@ namespace Azos.AuthKit.Tools.idp
     public override void Run()
     {
       Console.WriteLine("Come on Starbear!");
-      var filter = new UserListFilter{ };
+      var filter = new UserListFilter{};// Name = "dkh" };
 
       //SET data CONTEXT session
       var session = Ambient.CurrentCallSession;
@@ -50,17 +51,52 @@ namespace Azos.AuthKit.Tools.idp
 
     public static string BuildUserListMarkup(IEnumerable<UserInfo> data)
     {
+      var now = Ambient.UTCNow;
       var sb = new StringBuilder();
       foreach(var one in data)
       {
-        sb.Append($"<J width=10 dir=right text='{one.Gdid}'>");
-        sb.Append($"<J width=8 dir=right text='.{one.Guid.ToString().TakeLastChars(5)}'>");
-        sb.Append($"<J width=25 dir=right text='{one.Name}'>");
-        sb.Append($"<J width=10 dir=right text='{one.Level}'>");
+        sb.Append("<push>");
+        sb.Append("<f color=Cyan>" + one.Gdid.ToString().PadLeft(16));
+        sb.Append("<f color=Gray>");
+        sb.Append(" ");
+        sb.Append(one.Guid.ToString("N").TakeLastChars(5).PadLeft(6, '.'));
+        sb.Append("<f color=DarkGray> ");
+        var cc = one.Level >= Security.UserStatus.System ? "Magenta" :
+                 one.Level == Security.UserStatus.User ? "Green" :
+                 one.Level == Security.UserStatus.Admin ? "Yellow" : "Red";
+        sb.Append($"[<f color={cc}>" + one.Level.ToString().TakeFirstChars(3).ToUpperInvariant() + "<f color=DarkGray>]");
+        sb.Append(" ");
+        sb.Append("<f color=White>" + one.Name.Default("?").PadRight(32));
+        sb.Append("<f color=Gray>");
+        sb.Append(one.Description.Default(" ").TakeFirstChars(12, "..").PadRight(14));
+        sb.Append(" ");
+        sb.Append((one.OrgUnit?.AsString).Default(" ").TakeLastChars(12, "..").PadRight(14));
+        sb.Append(" ");
+        sb.Append("<f color=DarkGray>" + dt(one.CreateVersion.Utc));
+        sb.Append(" ");
+        sb.Append($"<f color={(one.ValidSpanUtc.Contains(now) ? "DarkGray" : "DarkRed")}>(" + dt(one.ValidSpanUtc.Start) + ".." + dt(one.ValidSpanUtc.End)+ ")");
+        sb.Append(" ");
+        if (one.LockSpanUtc.HasValue)
+        {
+          sb.Append($"<f color={(one.LockSpanUtc.Value.Contains(now) ? "Red" : "DarkYellow")}>(" + dt(one.LockSpanUtc.Value.Start) + ".." + dt(one.LockSpanUtc.Value.End) + ")");
+        }
+        else
+        {
+          sb.Append(" ".PadLeft(10, '·'));
+        }
+        sb.Append("<f color=DarkCyan>");
+        sb.Append(" ");
+        sb.Append(one.Note.Default(" ").TakeFirstChars(31, "..").PadRight(32, '·'));
+
+        sb.Append("<pop>");
         sb.AppendLine();
+
       }
       return sb.ToString();
     }
+
+    private static string dt(DateTime d) => d.ToString("MM/dd/yyyy");
+    private static string dt(DateTime? d) => d.HasValue ? dt(d.Value) : "";
 
   }
 }
