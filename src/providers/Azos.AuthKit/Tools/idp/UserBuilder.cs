9801bbb5687f1b581c18cd3073dc42b63966363d
Dmitriy Khmaladze <d@zhabis.com>
2023-08-21 21:02:28 -0400

    #890 Add user entity wizard

--- src/providers/Azos.AuthKit/Tools/idp/UserBuilder.cs
+++ src/providers/Azos.AuthKit/Tools/idp/UserBuilder.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Azos.Apps;
+using Azos.Conf;
 using Azos.Data;
 using Azos.IO.Console;
 using Azos.Security;
@@ -23,24 +24,38 @@ namespace Azos.AuthKit.Tools.idp
     {
       var result = new UserEntity();
 
-      string role = "";
+      ConfigSectionNode prop = Configuration.NewEmptyRoot(Constraints.CONFIG_PROP_ROOT_SECTION);
+      ConfigSectionNode claims = prop.AddChildNode(Constraints.CONFIG_CLAIMS_SECTION);
+      result.Props = new ConfigVector(prop);
+
       while(true)
       {
         OneField("Name", () => result.Name, v => result.Name = v);
-        OneField("Level", () => result.Level.ToString(), v => result.Level = v.AsEnum(UserStatus.Invalid, ConvertErrorHandling.Throw));
+        OneField("Level (<push><f color=darkred>Usr|Adm|Sys<pop>)", () => result.Level.ToString(), v => result.Level = v.AsEnum(UserStatus.Invalid, ConvertErrorHandling.Throw));
         OneField("Description", () => result.Description, v => result.Description = v);
-        OneField("Valid UTC Start", () => result.ValidSpanUtc?.Start?.ToString(), v => result.ValidSpanUtc = new DateRange(v.AsDateTime(CoreConsts.UTC_TIMESTAMP_STYLES), result.ValidSpanUtc?.End));
-        OneField("Valid UTC End", () => result.ValidSpanUtc?.End?.ToString(), v => result.ValidSpanUtc = new DateRange(result.ValidSpanUtc?.Start, v.AsDateTime(CoreConsts.UTC_TIMESTAMP_STYLES)));
+        OneField("Validity Utc span start (mm/dd/yyyy hh:mm)", () => result.ValidSpanUtc?.Start?.ToString(), v => result.ValidSpanUtc = new DateRange(v.AsDateTime(CoreConsts.UTC_TIMESTAMP_STYLES), result.ValidSpanUtc?.End));
+        OneField("Validity Utc span end (mm/dd/yyyy hh: mm)", () => result.ValidSpanUtc?.End?.ToString(), v => result.ValidSpanUtc = new DateRange(result.ValidSpanUtc?.Start, v.AsDateTime(CoreConsts.UTC_TIMESTAMP_STYLES)));
         OneField("Org Unit", () => result.OrgUnit?.AsString, v => result.OrgUnit = v.AsEntityId(EntityId.EMPTY, ConvertErrorHandling.Throw));
-        OneField("Role", () => role, v => role = v);
-        //todo check role
-        result.Props = new ConfigVector("prop{  role='"+role+"'}");
-
+        OneField("Role", () => prop.ValOf(Constraints.CONFIG_ROLE_ATTR), v => prop.AttrByName(Constraints.CONFIG_ROLE_ATTR, autoCreate: true).Value = v.AsNullableEntityId(null, ConvertErrorHandling.Throw));
         OneField("Note", () => result.Note, v => result.Note = v);
 
         var ve = result.Validate();
         if (ve == null)
         {
+          ConsoleUtils.WriteMarkupContent(@$"
+  <f color=darkgray>Name: <f color=white>{result.Name}
+  <f color=darkgray>Level: <f color=white>{result.Level}
+  <f color=darkgray>Descr: <f color=white>{result.Description}
+  <f color=darkgray>Valid span: <f color=white>{result.ValidSpanUtc}
+  <f color=darkgray>Org unit: <f color=white>{result.OrgUnit}
+  <f color=darkgray>Props: <f color=cyan>
+     {result.Props.Content}
+  <f color=darkgray>Rights: <f color=cyan>
+     {result.Rights?.Content}
+  <f color=darkgray>Note: <f color=white>
+     {result.Note}
+
+          ");
           if (OK())
           {
             return result;
@@ -55,7 +70,7 @@ namespace Azos.AuthKit.Tools.idp
 
     public static void OneField(string fn,  Func<string> getter, Action<string> setter)
     {
-      ConsoleUtils.WriteMarkupContent($"<f color=gray>Please enter <f color=white>{fn}<f color=gray>: <f color=darkcyan>{getter()}  <f color=gray>");
+      ConsoleUtils.WriteMarkupContent($"<f color=darkgray>Please enter <f color=white>{fn}<f color=gray>: <f color=darkcyan>{getter()}  <f color=gray>");
 
       while(true)
       {
