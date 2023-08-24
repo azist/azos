a5917e1cf2c9aa06a5122e048a6df5b00a5bac61
Dmitriy Khmaladze <d@zhabis.com>
2023-08-24 12:55:03 -0400

    Nuget 3.6.1.1 #891 #890

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.6.0.12
-set HEADLINE=FileGatewayClient async
+set VER=3.6.1.1
+set HEADLINE=Fix #891 json reader nullable
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
