0bee49a51c06ce8f6beb012b35ecc242c2714311
Dmitriy Khmaladze <d@zhabis.com>
2023-09-07 14:42:39 -0400

    #894 #893 Nuget push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.6.1.1
-set HEADLINE=Fix #891 json reader nullable
+set VER=3.6.2.1
+set HEADLINE=#894 #893 DCF/ICallFlow and ChangeResult json data
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
