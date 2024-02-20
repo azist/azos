a097e8619f8ab552ea90a14c52a892a907a81903
Dmitriy Khmaladze <d@zhabis.com>
2024-02-20 13:12:52 -0500

    #909 Nuget

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.8.0.2
-set HEADLINE=#908 buffer overflow fix
+set VER=3.8.0.3
+set HEADLINE=#909 WebCallExtensions.IJsonReadingOptionsAspect
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
