2cf72d909d09e86a63fc09aaba39db00cde67e9e
Dmitriy Khmaladze <d@zhabis.com>
2024-02-26 12:33:57 -0500

    #904 Multifield operators

--- src/Azos/Scripting/Expressions/Data/DataDocs.cs
+++ src/Azos/Scripting/Expressions/Data/DataDocs.cs
@@ -5,6 +5,8 @@
 </FILE_LICENSE>*/
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Azos.Conf;
 using Azos.Data;
 
@@ -44,6 +46,7 @@ namespace Azos.Scripting.Expressions.Data
   public class ByName : Expression<ScriptCtx, object>
   {
     [Config] public string Field { get; set; }
+    [Config] public bool Lax { get; set; }
 
     public sealed override object Evaluate(ScriptCtx context)
     {
@@ -54,7 +57,11 @@ namespace Azos.Scripting.Expressions.Data
       object result = null;
       foreach(var fn in fns)
       {
-        if (ds == null) throw new ScriptingException("Bad field by name navigation: `{0}`".Args(Field));
+        if (ds == null)
+        {
+          if (Lax) return null;
+          throw new ScriptingException("Bad field by name navigation: `{0}`".Args(Field));
+        }
         result = ds[fn.Replace("%2e", ".")];
         ds = result as Doc;
       }
@@ -62,5 +69,119 @@ namespace Azos.Scripting.Expressions.Data
       return result;
     }
   }
+
+
+  /// <summary>
+  /// Abstract accessor which enumerates fields by name. Multiple field must be separated by semicolon.
+  /// Each field name property may have a single field or a path: "Patient.LastName"
+  /// in which case "Patient" is a field of primary data context of type "Doc" having a field name "LastName".
+  /// You can escape field names with dots with "%2e" for example: "Geometry.%2eX" = "Geometry[dot].X"
+  /// </summary>
+  public abstract class MultifieldByName<TResult> : Expression<ScriptCtx, TResult>
+  {
+    [Config]
+    public string Fields { get; set; }
+
+    [Config(Default = true)]
+    public bool Lax { get; set; } = true;
+
+    protected IEnumerable<(Doc doc, Schema.FieldDef fld, object val)> GetFields(ScriptCtx context)
+    {
+      context.NonNull(nameof(context));
+      var paths = Fields.NonBlank(nameof(Fields)).Split(';');
+      foreach(var path in paths.Where(p => p.IsNotNullOrWhiteSpace()))
+      {
+        var fns = path.NonBlank(nameof(path)).Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+        var ds = context.Data;
+        Doc doc = ds;
+        Schema.FieldDef fd = null;
+        object fv = null;
+
+        foreach (var fn in fns)
+        {
+          if (ds == null)
+          {
+            if (Lax) break;
+            throw new ScriptingException("Bad field by name navigation: `{0}`".Args(path));
+          }
+          doc = ds;
+          var dsfn = fn.Replace("%2e", ".");
+          fd = ds.Schema[dsfn];
+          if (fd == null)
+            throw new ScriptingException("Bad field by name navigation: `{0}`. Unknown field: `{1}`".Args(path, dsfn));
+
+          fv = ds.GetFieldValue(fd);
+          ds = fv as Doc;
+        }
+
+        if (fd != null) yield return (doc, fd, fv);
+      }//foreach
+    }
+  }
+
+  /// <summary>
+  /// Bool expression which is true if all fields are null
+  /// </summary>
+  public sealed class AllNull : MultifieldByName<bool>
+  {
+    public override bool Evaluate(ScriptCtx context)
+     => GetFields(context).All(one => one.val == null);
+  }
+
+  /// <summary>
+  /// Bool expression which is true if all fields are non-null
+  /// </summary>
+  public sealed class AllNonNull : MultifieldByName<bool>
+  {
+    public override bool Evaluate(ScriptCtx context)
+      => GetFields(context).All(one => one.val != null);
+  }
+
+  /// <summary>
+  /// Bool expression which is true if any of the fields are null
+  /// </summary>
+  public sealed class AnyNull : MultifieldByName<bool>
+  {
+    public sealed override bool Evaluate(ScriptCtx context)
+      => GetFields(context).Any(one => one.val == null);
+  }
+
+  /// <summary>
+  /// Bool expression which is true if any of the fields are not null
+  /// </summary>
+  public sealed class AnyNonNull : MultifieldByName<bool>
+  {
+    public sealed override bool Evaluate(ScriptCtx context)
+      => GetFields(context).Any(one => one.val != null);
+  }
+
+  /// <summary>
+  /// Returns the value of the first non-null field or null.
+  /// You can optionally type cast the value to `CastTypeName` and
+  /// apply optional `NullDefault`
+  /// </summary>
+  public sealed class FirstNonNull : MultifieldByName<object>
+  {
+    [Config] public string CastTypeName { get; set; }
+    [Config] public string NullDefault { get; set; }
+
+    public sealed override object Evaluate(ScriptCtx context)
+    {
+      var got = GetFields(context).FirstOrDefault(one => one.val != null).val;
+
+      if (CastTypeName.IsNotNullOrWhiteSpace())
+      {
+        var tp = Type.GetType(CastTypeName).NonNull($"Existing type `{CastTypeName}`");
+        if (got == null) return NullDefault.AsType(tp, false);
+        return Convert.ChangeType(got, tp);
+      }
+      else
+      {
+        return got ?? NullDefault;
+      }
+    }
+  }
+
 
 }
