8dc46915acf7570258820c8c06ad413cf7de2873
Dmitriy Khmaladze <d@zhabis.com>
2024-05-15 12:45:32 -0400

    #914 Nuget push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.9.0.3
-set HEADLINE=Add trim to MultifieldScalarPredicate
+set VER=3.9.0.4
+set HEADLINE=Add TimeZoneManager
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
