3ef874719926c7f19436a2bf7c9485c49ea5a87f
Dmitriy Khmaladze <d@zhabis.com>
2024-06-10 14:58:06 -0400

    #915 HourList.Exclude hangs in infinite loop

--- src/testing/Azos.Tests.Nub/Time/HourListIntersectionTests.cs
+++ src/testing/Azos.Tests.Nub/Time/HourListIntersectionTests.cs
@@ -20,6 +20,12 @@ namespace Azos.Tests.Nub.Time
     [Run("inc=true  a='1am-2am'  b='2am-3pm'  c='1am-2am,2am-3pm'")]
     [Run("inc=true  a='1am-2am'  b='1:15am-1:25am'  c='1am-2am'")]
     [Run("inc=true  a='1am-2am'  b='1:15am-2:25am'  c='1am-2:25am'")]
+
+    [Run("inc=true  a='0-0.75,13:00-14:45'  b='8pm-9pm'  c='0am-0:45am,1pm-2:45pm,8pm-9pm'")]
+    [Run("inc=true  a='12am-0.75,13:00-14:45'  b='8pm-9pm'  c='12am-0:45am,1pm-2:45pm,8pm-9pm'")]
+
+    //Exclude hangs in infinite loop
+   // [Run("inc=false  a='0-13:00,14-18:00,8pm-3am'  b='0:15-0:30,14:30-15:00'  c='0am-0:15am,0:30am-13:00,14-14:30,15:00-18:00,8pm-3am'")]
     public void Include(bool inc, string a, string b, string c)
     {
       var ha = new HourList(a);
