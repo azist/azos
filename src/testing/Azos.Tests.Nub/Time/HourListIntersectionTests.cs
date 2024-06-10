404a8150f8c7b8dade9bad784d9dfa1aaa313386
Dmitriy Khmaladze <d@zhabis.com>
2024-06-10 15:57:15 -0400

    #915 HourList with fallover to next day does not work: 1pm-2pm,1am-1pm

--- src/testing/Azos.Tests.Nub/Time/HourListIntersectionTests.cs
+++ src/testing/Azos.Tests.Nub/Time/HourListIntersectionTests.cs
@@ -24,8 +24,10 @@ namespace Azos.Tests.Nub.Time
     [Run("inc=true  a='0-0.75,13:00-14:45'  b='8pm-9pm'  c='0am-0:45am,1pm-2:45pm,8pm-9pm'")]
     [Run("inc=true  a='12am-0.75,13:00-14:45'  b='8pm-9pm'  c='12am-0:45am,1pm-2:45pm,8pm-9pm'")]
 
-    //Exclude hangs in infinite loop
+
+    [Run("inc=false  a='0-18:00,21:00-6:00'  b='19:00-19:15,1am-1:30am'  c='0-6pm,21:00-1am,1:30am-6am'")]
     [Run("inc=false  a='0-13:00,14-18:00,8pm-3am'  b='0:15-0:30,14:30-15:00'  c='0am-0:15am,0:30am-13:00,14-14:30,15:00-18:00,8pm-3am'")]
+
     public void IncludeOrExclude(bool inc, string a, string b, string c)
     {
       var ha = new HourList(a);
