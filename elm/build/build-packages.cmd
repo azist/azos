5985844bff86342d1e15e1c5c415b17aee555535
Dmitriy Khmaladze <d@zhabis.com>
2024-09-10 15:58:59 -0400

    Nuget 3.9.2.1 push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.9.1.4
-set HEADLINE=Add UTC_MASQUERADE SQL rpc header for transmission
+set VER=3.9.2.1
+set HEADLINE=#917 #918
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
