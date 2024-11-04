b41b60131a80b3147eb12f0a994018901983f2a3
Dmitriy Khmaladze <d@zhabis.com>
2024-11-04 18:42:14 -0500

    Nuget push 3.10.0.1

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.9.4.1
-set HEADLINE=#924 Slim EntityId serialization bug fix
+set VER=3.10.0.1
+set HEADLINE=#926 Adlib CreateUtc range queries
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
