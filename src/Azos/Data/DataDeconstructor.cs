03fa52e57960265ff72a8b7df1aee62cae5ce90f
Dmitriy Khmaladze <d@zhabis.com>
2024-11-22 14:50:50 -0500

    #930 wip

--- src/Azos/Data/DataDeconstructor.cs
+++ src/Azos/Data/DataDeconstructor.cs
@@ -5,9 +5,8 @@
 </FILE_LICENSE>*/
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+
 using Azos.CodeAnalysis.Laconfig;
 using Azos.CodeAnalysis.Source;
 using Azos.Text;
@@ -15,84 +14,74 @@ using Azos.Text;
 namespace Azos.Data
 {
   /// <summary>
-  /// Facilitates creation of classes which deconstruct complex data vectors, such as a data filter vectors,
-  /// from their simplified convoluted representations, such as a strings or key/value pairs.
-  /// For example, given a specific deconstructor implementation for a `PersonFilter` object,
-  /// an implementation will deconstruct as string like "Johnson 90210" into a `PersonFilter` object having its field values set to "LastName='Johnson' and AddressZip='90210'"
-  /// as decosntructed from a flat string. Deconstruction is usually based on simple decision-tree based heuristics process.
-  /// Server side deconstruction is used to process simple filter strings which look like Google queries - a single line of text coming from clients.
-  /// Decosntruction applies heuristics to compose a more complex filter objects based on a simple assumptions,
-  ///  such as "a string value which matches Email pattern is most likely an email address"
-  /// </summary>
-  public abstract class DataDeconstructor<TDoc, TValue> where TDoc : Doc
-  {
-    protected DataDeconstructor(TDoc doc)
-    {
-      m_Doc = doc.NonNull(nameof(doc));
-    }
-
-    protected readonly TDoc m_Doc;
-
-    public TDoc DataDoc => m_Doc;
-
-    /// <summary>
-    /// Deconstructs a value into this document. Returns true if value was deconstructed, or false when nothing was decosntructed
-    /// e.g., when the data was logically empty/blank.
-    /// Errors are thrown wrapped in <see cref="ValidationException"/> (e.g. invalid syntax)
-    /// </summary>
-    public abstract bool Deconstruct(TValue value);
-  }
-
-  /// <summary>
-  /// Deconstructs complex data convoluted into a string using Laconic lexer tokenization rules.
-  /// <inheritdoc/>
-  /// </summary>
-  public abstract class StringDataDeconstructor<TDoc> : DataDeconstructor<TDoc, string> where TDoc : Doc
-  {
-    public StringDataDeconstructor(TDoc doc) : base(doc)
-    {
-    }
-
-
-    /// <inheritdoc/>
-    public override bool Deconstruct(string value)
-    {
-      if (value.IsNullOrWhiteSpace()) return false;
-      try
-      {
-        var src = new StringSource(value);
-        var lxr = new LaconfigLexer(src, throwErrors: true);
-        var tokens = lxr.ToArray();
-        return DoDeconstructFromTokens(tokens);
-      }
-      catch (Exception cause)
-      {
-        throw new ValidationException($"Bad `{this.GetType().DisplayNameWithExpandedGenericArgs()}` clause: {cause.ToMessageWithType()}", cause);
-      }
-    }
-
-    protected abstract bool DoDeconstructFromTokens(LaconfigToken[] tokens);
-  }
-
-
-  /// <summary>
   /// Provides handy extension methods for writing data deconstruction heuristics
   /// </summary>
   public static class DataDeconstructionExtensions
   {
     public const string DECONSTRUCT_PROPERTY = "__deconstruct";
 
-    public static bool IsPhone(this string v) => DataEntryUtils.CheckTelephone(v);
-    public static bool IsEmail(this string v) => DataEntryUtils.CheckEMail(v);
-    public static bool IsScreenName(this string v) => DataEntryUtils.CheckScreenName(v);
+    /// <summary> Returns true if a string value could be deemed as a telephone number </summary>
+    public static bool IsPhone(this object v) => v is string sv && DataEntryUtils.CheckTelephone(sv);
+    /// <summary> Returns true if a token value could be deemed as a telephone number </summary>
+    public static bool IsPhone(this LaconfigToken t) => t != null && (t.IsLiteral || t.IsIdentifier) && t.Value.IsPhone();
 
-    public static bool IsUsZip(this string v) => v.IsNotNullOrWhiteSpace() && v.Length == 5 && int.TryParse(v, out var iv) && iv < 99999;
-    public static bool IsInteger(this string v) => v.IsNotNullOrWhiteSpace() && int.TryParse(v, out var _);
-    public static bool IsReal(this string v) => v.IsNotNullOrWhiteSpace() && decimal.TryParse(v, out var _);
+
+    /// <summary> Returns true if a string value could be deemed as an email </summary>
+    public static bool IsEmail(this object v) => v is string sv && DataEntryUtils.CheckEMail(sv);
+    /// <summary> Returns true if a token value could be deemed as a telephone number </summary>
+    public static bool IsEmail(this LaconfigToken t) => t != null && (t.IsLiteral || t.IsIdentifier) && t.Value.IsEmail();
+
+    /// <summary> Returns true if a string value could be deemed as a screen name/id </summary>
+    public static bool IsScreenName(this object v) => v is string sv && DataEntryUtils.CheckScreenName(sv);
+    /// <summary> Returns true if a token value could be deemed as a screen name </summary>
+    public static bool IsScreenName(this LaconfigToken t) => t != null && (t.IsLiteral || t.IsIdentifier) && t.Value.IsScreenName();
+
+
+    /// <summary> Returns true if a string value could be deemed as a zip code </summary>
+    public static bool IsUsZip(this object v)
+      => (v is string sv && sv.IsNotNullOrWhiteSpace() && sv.Length == 5 && int.TryParse(sv, out var iv) && iv < 99999) ||
+         (v is int iv2 && iv2 > 0 && iv2 < 99999) ||
+         (v is long lv && lv > 0 && lv < 99999);
+
+    /// <summary> Returns true if a long value could be deemed as a zip code </summary>
+    public static bool IsUsZip(this LaconfigToken t) => t != null && (t.IsLiteral || t.IsIdentifier) && t.Value.IsUsZip();
+
+
+
+    /// <summary> Returns true if a value could be deemed as a real number </summary>
+    public static bool IsInteger(this object v)
+      => v is string sv ? sv.IsNotNullOrWhiteSpace() && int.TryParse(sv, out var _) :
+         v is int ? true :
+         v is long ? true :
+         v is uint ? true :
+         v is ulong ? true : false;
+
+    /// <summary> Returns true if a token value could be deemed as a real number </summary>
+    public static bool IsInteger(this LaconfigToken t) => t != null && (t.IsNumericLiteral || t.IsLiteral || t.IsIdentifier) && t.Value.IsInteger();
+
+    /// <summary> Returns true if a value could be deemed as a real number </summary>
+    public static bool IsReal(this object v)
+      => v is string sv ? sv.IsNotNullOrWhiteSpace() && decimal.TryParse(sv, out var _) :
+         v is double ? true :
+         v is float ? true :
+         v is decimal ? true : false;
+
+    /// <summary> Returns true if a token value could be deemed as a real number </summary>
+    public static bool IsReal(this LaconfigToken t) => t != null && (t.IsNumericLiteral || t.IsLiteral || t.IsIdentifier) && t.Value.IsReal();
 
     public static string NormalizePhone(this string v) => DataEntryUtils.NormalizeUSPhone(v);
+    public static string NormalizePhone(this LaconfigToken t) => t != null && (t.IsLiteral || t.IsIdentifier) ?  DataEntryUtils.NormalizeUSPhone(t.Value.AsString()) : null;
 
-
+    /// <summary>
+    /// Deconstructs complex data vectors, such as a data filter vectors, from their simplified convoluted representations,
+    /// such as a strings or key/value pairs.
+    /// For example, given a specific deconstructor implementation passed as `Func` for a `PersonFilter` object,
+    /// an implementation will deconstruct as string like "Johnson 90210" into a `PersonFilter` object having its field values set to "LastName='Johnson' and AddressZip='90210'"
+    /// as decosntructed from a flat string. Deconstruction is usually based on simple decision-tree based heuristics process.
+    /// Server side deconstruction is used to process simple filter strings which look like Google queries - a single line of text coming from clients.
+    /// Decosntruction applies heuristics to compose a more complex filter objects based on a simple assumptions,
+    ///  such as "a string value which matches Email pattern is most likely an email address"
+    /// </summary>
     public static bool DeconstructStringData<TDoc>(this TDoc doc, string value, Func<TDoc, LaconfigToken[], bool> fBody) where TDoc : Doc
     {
       fBody.NonNull(nameof(fBody));
@@ -107,11 +96,20 @@ namespace Azos.Data
       }
       catch (Exception cause)
       {
-        throw new ValidationException($"Bad deconstruction clause: {cause.ToMessageWithType()}", cause);
+        throw new ValidationException($"Bad deconstruction string clause: {cause.ToMessageWithType()}", cause);
       }
     }
 
-
+    /// <summary>
+    /// Deconstructs complex data vectors, such as a data filter vectors, from their simplified convoluted representations,
+    /// such as a strings or key/value pairs.
+    /// For example, given a specific deconstructor implementation passed as `Func` for a `PersonFilter` object,
+    /// an implementation will deconstruct as string like "Johnson 90210" into a `PersonFilter` object having its field values set to "LastName='Johnson' and AddressZip='90210'"
+    /// as decosntructed from a flat string. Deconstruction is usually based on simple decision-tree based heuristics process.
+    /// Server side deconstruction is used to process simple filter strings which look like Google queries - a single line of text coming from clients.
+    /// Decosntruction applies heuristics to compose a more complex filter objects based on a simple assumptions,
+    ///  such as "a string value which matches Email pattern is most likely an email address"
+    /// </summary>
     public static bool DeconstructAmorphousData<TDoc>(this TDoc doc, Func<TDoc, object, bool> fBody, string amorphousPropertyName = null) where TDoc : Doc, IAmorphousData
     {
       fBody.NonNull(nameof(fBody));
@@ -123,6 +121,17 @@ namespace Azos.Data
       return fBody(doc, value);
     }
 
+    /// <summary>
+    /// Deconstructs complex data vectors, such as a data filter vectors, from their simplified convoluted string representations, which are extracted from an amorphous
+    /// data named key (See <see cref="DECONSTRUCT_PROPERTY"/> used by default).
+    /// For example, given a specific deconstructor implementation passed as `Func` for a `PersonFilter` object,
+    /// an implementation will deconstruct as string like "Johnson 90210" into a `PersonFilter` object having its field values set to "LastName='Johnson' and AddressZip='90210'"
+    /// as decosntructed from a flat string using Laconfig lexer (tokenizer).
+    /// Deconstruction is usually based on simple decision-tree based heuristics process.
+    /// Server side deconstruction is used to process simple filter strings which look like Google queries - a single line of text coming from clients.
+    /// Decosntruction applies heuristics to compose a more complex filter objects based on a simple assumptions,
+    ///  such as "a string value which matches Email pattern is most likely an email address"
+    /// </summary>
     public static bool DeconstructAmorphousStringData<TDoc>(this TDoc doc, Func<TDoc, LaconfigToken[], bool> fBody, string amorphousPropertyName = null) where TDoc : Doc, IAmorphousData
     {
       fBody.NonNull(nameof(fBody));
