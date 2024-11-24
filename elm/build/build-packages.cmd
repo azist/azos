edd7da417238109db1b90edbe6d4a9ce75dbae44
Dmitriy Khmaladze <d@zhabis.com>
2024-11-24 16:44:22 -0500

    #930 and SendGrid Nuget 3.10.0.4

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.10.0.3
-set HEADLINE=#929 Web Call Extensions no content fix
+set VER=3.10.0.4
+set HEADLINE=#930 Deconstructor, SendGrid Sink
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
