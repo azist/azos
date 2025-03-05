83f8401612c0bbc5c8b740445599f2774af6bf92
Dmitriy Khmaladze <d@zhabis.com>
2025-03-05 17:43:02 -0500

    NUget push

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.10.0.11
-set HEADLINE=IDP Identity cfg conversion
+set VER=3.10.1.1
+set HEADLINE=OAuth tracing
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
