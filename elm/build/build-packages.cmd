93f317efe49d43efda2a33bac80076a5630121ae
Dmitriy Khmaladze <d@zhabis.com>
2025-07-03 16:15:57 -0400

    #954 Nuget 3.2.1.1

--- elm/build/build-packages.cmd
+++ elm/build/build-packages.cmd
@@ -1,7 +1,7 @@
 @echo on
 
-set VER=3.11.1.3
-set HEADLINE=LatLng refactoring, precision
+set VER=3.2.1.1
+set HEADLINE=#954 Daemon.CancellationToken
 call build-all Release %VER%
 
 if errorlevel 1 goto BUILD_ERROR
